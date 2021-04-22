30e466694efe8becc8e0a78334a9011dbd936e9a
Michael Ashton <michael.ashton@caktusgroup.com>
2021-04-22 09:27:42 -0400

    [NRH-13] remove taggit app

--- revengine/settings/base.py
+++ revengine/settings/base.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
     "apps.users",
     "apps.organizations",
     "apps.pages",
-    "taggit",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

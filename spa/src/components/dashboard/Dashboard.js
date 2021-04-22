5ac58bfc79200760d6823cfb5cf577b8a35565f8
Michael Ashton <michael.ashton@caktusgroup.com>
2021-04-22 17:20:18 -0400

    [NRH-19] create ProtectedRoute, add shadows

---
+++ spa/src/components/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import * as S from './Dashboard.styled';
+
+function Dashboard(props) {
+  return (
+    <S.Dashboard>
+      <p>Dashboard</p>
+    </S.Dashboard>
+  );
+}
+
+export default Dashboard;
---
+++ spa/src/constants/authConstants.js
@@ -0,0 +1,1 @@
+export const LS_USER = 'REVENGINE_USER';

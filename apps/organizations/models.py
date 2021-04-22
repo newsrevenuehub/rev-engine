e888857d4637f675acf7095bff7a27ca4b62f469
Jeremy Gibson <jgibson@caktusgroup.com>
2021-04-22 14:12:35 -0400

    [NRH-12] WIP

--- apps/organizations/models.py
+++ apps/organizations/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+from apps.users.models import User
+
 
 class Organization(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField()
+    non_profit = models.BooleanField(default=True)
+    project_manager = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -13,6 +17,13 @@ class RevenueProgram(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField()
     organization = models.ForeignKey("organizations.Organization", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Plan(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name

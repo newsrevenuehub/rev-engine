c02017acebe42e096c5c4bfcdef3c78e7b47a850
Michael Ashton <michael.ashton@caktusgroup.com>
2021-04-22 16:39:29 -0400

    [NRH-19] very basic styling assumptions

--- spa/src/styles/createGlobalStyles.js
+++ spa/src/styles/createGlobalStyles.js
@@ -3,4 +3,24 @@ import { normalize } from 'styled-normalize'
  
 export default createGlobalStyle`
   ${normalize};
+
+  body {
+    color: ${(p) => p.theme.colors.black};
+    font-family: ${(p) => p.theme.fonts.body};
+  }
+
+  p {
+    color: ${(p) => p.theme.colors.black};
+    font-family: ${(p) => p.theme.fonts.body};
+  }
+
+  h1, h2 {
+    color: ${(p) => p.theme.colors.black};
+    font-family: ${(p) => p.theme.fonts.heading};
+  }
+
+  h3, h4, h5, h6 {
+    color: ${(p) => p.theme.colors.black};
+    font-family: ${(p) => p.theme.fonts.subheading};
+  }
 `;

d1238a6d07c2e4c8b8acd8241d7bf10c36a54bb6
Michael Ashton <michael.ashton@caktusgroup.com>
2021-04-22 16:14:38 -0400

    [NRH-19] remove unused package, sort absolut imports issue

--- spa/src/styles/createGlobalStyles.js
+++ spa/src/styles/createGlobalStyles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 import { normalize } from 'styled-normalize'
  
-export const GlobalStyle = createGlobalStyle`
+export default createGlobalStyle`
   ${normalize};
 `;

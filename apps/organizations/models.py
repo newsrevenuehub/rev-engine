f2b8ac565d84c149770af99e694e85b7239879df
Michael Ashton <michael.ashton@caktusgroup.com>
2021-04-22 08:57:42 -0400

    [NRH-13] add organizations app, stub models for relations

---
+++ apps/organizations/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+
+class RevenueProgram(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    organization = models.ForeignKey("organizations.Organization", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name

c6b50f02ea85ee50e2b797c766097f36391321cb
Michael Ashton <michael.ashton@caktusgroup.com>
2021-04-22 14:41:54 -0400

    [NRH-13] Change django admin text to refer to RevEngine

---
+++ apps/api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+
+admin.site.site_header = "RevEngine Admin"
+admin.site.site_title = "RevEngine Admin Portal"
+admin.site.index_title = "Welcome to the RevEngine Adminstrator Portal"

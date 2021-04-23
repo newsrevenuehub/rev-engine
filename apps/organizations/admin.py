670edab930f50a72467dcf30fc7645449d4e5d21
Jeremy Gibson <jgibson@caktusgroup.com>
2021-04-23 13:35:10 -0400

    [NRH-12] WIP Modeling

--- apps/organizations/admin.py
+++ apps/organizations/admin.py
@@ -1,15 +1,38 @@
 from django.contrib import admin
 
-from apps.organizations.models import Organization, RevenueProgram
+from apps.organizations.models import Feature, Organization, Plan, RevenueProgram
 
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Organization
+    organization_fieldset = (
+        ("Organization", {"fields": ("name", "slug", "project_manager")}),
+        (
+            "Address",
+            {
+                "fields": (
+                    "org_addr1",
+                    "org_addr2",
+                    ("org_city", "org_state", "org_zip"),
+                )
+            },
+        ),
+        ("Plan", {"fields": ("non_profit", "plan", "stripe_account", "salesforce_id")}),
+    )
+
+    fieldsets = organization_fieldset
 
 
 @admin.register(RevenueProgram)
 class RevenueProgramAdmin(admin.ModelAdmin):
-    class Meta:
-        model = RevenueProgram
+    pass
+
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Feature)
+class FeatureAdmin(admin.ModelAdmin):
+    pass

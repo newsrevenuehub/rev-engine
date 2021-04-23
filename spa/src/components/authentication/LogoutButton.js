ea95eaee76db1d58ae02e4609b3a8f81f70dbb32
Michael Ashton <michael.ashton@caktusgroup.com>
2021-04-23 12:50:23 -0400

    [NRH-19] set up frontend testing, basic tests, fix logout

--- spa/src/components/authentication/LogoutButton.js
+++ spa/src/components/authentication/LogoutButton.js
@@ -1,8 +1,16 @@
 import * as S from './LogoutButton.styled';
 
-import { handleLogout } from 'components/authentication/util';
+import axios from 'ajax/axios';
+import { TOKEN } from 'ajax/endpoints';
+import { handleLogoutSuccess } from 'components/authentication/util';
 
 function LogoutButton() {
+
+  const handleLogout = () => {
+    axios.delete(TOKEN);
+    handleLogoutSuccess()
+  }
+
   return (
     <S.LogoutButton onClick={handleLogout}>Sign out</S.LogoutButton>
   ) 

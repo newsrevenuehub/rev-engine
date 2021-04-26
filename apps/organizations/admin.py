ffc6d5cf594f5c421708d62add41ce2bbb9bada0
Michael Ashton <michael.ashton@caktusgroup.com>
2021-04-26 09:59:28 -0400

    [NRH-13] add display, search, list, properties to modeladmins, fix tests

--- apps/organizations/admin.py
+++ apps/organizations/admin.py
@@ -3,13 +3,6 @@ from django.contrib import admin
 from apps.organizations.models import Organization, RevenueProgram
 
 
-@admin.register(Organization)
-class OrganizationAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Organization
+admin.site.register(Organization)
 
-
-@admin.register(RevenueProgram)
-class RevenueProgramAdmin(admin.ModelAdmin):
-    class Meta:
-        model = RevenueProgram
+admin.site.register(RevenueProgram)

2cf0d0c729b8fa7cdfbe5b382c4931c910d43004
Michael Ashton <michael.ashton@caktusgroup.com>
2021-05-04 09:56:16 -0400

    [NRH-15] add stripe urls

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 
 from apps.api.views import TokenObtainPairCookieView
+from apps.contributions.urls import urlpatterns as contributions_urlpatterns
 from apps.pages.urls import urlpatterns as pages_urlpatterns
 
 
 urlpatterns = [
     path("v1/", include(pages_urlpatterns)),
+    path("v1/", include(contributions_urlpatterns)),
     path("v1/token/", TokenObtainPairCookieView.as_view(), name="token-obtain-pair"),
 ]

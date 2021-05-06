983f219a1318f6ff37f62700a2d7200274b81ff8
Michael Ashton <michael.ashton@caktusgroup.com>
2021-05-06 12:15:44 -0400

    [NRH-15] add working stripe elements checkout widget to frontend

--- spa/src/components/dashboard/Dashboard.js
+++ spa/src/components/dashboard/Dashboard.js
@@ -1,25 +1,15 @@
-import { useRef } from "react";
 import * as S from "./Dashboard.styled";
 import LogoutButton from "components/authentication/LogoutButton";
 
-// Deps
-import { loadStripe } from "@stripe/stripe-js";
-
 // TEMP
-import TemporaryStripeCheckoutTest from "./TemporaryStripeCheckoutTest";
+import { Link } from "react-router-dom";
 
 function Dashboard(props) {
-  const stripeRef = useRef(
-    loadStripe(
-      "pk_test_51InNlfFhyXrQfcca6Sfm65ldMRYwBGPyNRRR6GUkpeOpnFPyJCKw3nMjG51gOrVy2eYcz4ej44uMAgJC0FR9K8nN00pEZYwGum"
-    )
-  );
-
   return (
     <S.Dashboard>
       <p>Dashboard</p>
       <LogoutButton />
-      <TemporaryStripeCheckoutTest stripe={stripeRef.current} />
+      <Link to="/temp-pretend-payment">Make pretend payment</Link>
     </S.Dashboard>
   );
 }
--- spa/src/components/dashboard/TemporaryStripeCheckoutTest.styled.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const TemporaryStripeCheckoutTest = styled.div`
-  width: 500px;
-`;

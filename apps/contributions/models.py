31f36e0c6a029570ed70cdb3d101373ba6a4768c
Michael Ashton <michael.ashton@caktusgroup.com>
2021-05-06 12:02:49 -0400

    [NRH-15] create contributions models, update existing models with relations

--- apps/contributions/models.py
+++ apps/contributions/models.py
@@ -1,5 +1,70 @@
 from django.db import models
 
+from apps.common.models import IndexedTimeStampedModel
 
-class Contributor(models.Model):
+
+class Contributor(IndexedTimeStampedModel):
     email = models.EmailField()
+
+    @property
+    def contributions_count(self):
+        return self.contribution_set.count()
+
+    @property
+    def quarantined_contributions_count(self):
+        return self.contribution_set.filter(is_quarantined=True).count()
+
+    @property
+    def most_recent_contribution(self):
+        return self.contribution_set.filter(payment_state="paid").latest()
+
+    @property
+    def most_recent_contribution_date(self):
+        return self.contribution_set.filter(payment_state="paid").latest().modified
+
+    def __str__(self):
+        return self.email
+
+
+class Contribution(IndexedTimeStampedModel):
+    amount = models.IntegerField(help_text="Stored in cents")
+    currency = models.CharField(max_length=3, default="usd")
+    is_quarantined = models.BooleanField(default=False)
+    payment_provider_data = models.JSONField(null=True)
+    provider_reference_id = models.CharField(max_length=255)
+    contributor = models.ForeignKey(
+        "contributions.Contributor", on_delete=models.SET_NULL, null=True
+    )
+    donation_page = models.ForeignKey("pages.DonationPage", on_delete=models.SET_NULL, null=True)
+    organization = models.ForeignKey(
+        "organizations.Organization", on_delete=models.SET_NULL, null=True
+    )
+
+    PROCESSING = (
+        "processing",
+        "processing",
+    )
+    PAID = (
+        "paid",
+        "paid",
+    )
+    CANCELED = (
+        "canceled",
+        "canceled",
+    )
+    FAILED = (
+        "failed",
+        "failed",
+    )
+    PAYMENT_STATES = [PROCESSING, PAID, CANCELED, FAILED]
+    payment_state = models.CharField(max_length=10, choices=PAYMENT_STATES)
+
+    def __str__(self):
+        return f"{self.formatted_amount}, {self.organization}"
+
+    @property
+    def formatted_amount(self):
+        return f"{float(self.amount / 100)} {self.currency.upper()}"
+
+    class Meta:
+        get_latest_by = "modified"

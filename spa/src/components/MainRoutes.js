53ff91cb86e3595f5dddf9633d69d6ad4c68698c
Michael Ashton <michael.ashton@caktusgroup.com>
2021-05-06 12:15:44 -0400

    [NRH-15] add working stripe elements checkout widget to frontend

--- spa/src/components/MainRoutes.js
+++ spa/src/components/MainRoutes.js
@@ -1,17 +1,23 @@
-import { BrowserRouter } from 'react-router-dom';
-import ProtectedRoute from 'components/authentication/ProtectedRoute';
+import { BrowserRouter } from "react-router-dom";
+import ProtectedRoute from "components/authentication/ProtectedRoute";
 
 // Slugs
-import { DASHBOARD_SLUG } from 'routes';
+import { DASHBOARD_SLUG } from "routes";
 
 // Children
-import Dashboard from 'components/dashboard/Dashboard';
+import Dashboard from "components/dashboard/Dashboard";
+
+// TEMP
+import TemporaryStripeCheckoutTest from "components/TEMP/TemporaryStripeCheckoutTest";
 
 function MainRoutes() {
   return (
     <BrowserRouter>
-      <ProtectedRoute path={DASHBOARD_SLUG}>
+      <ProtectedRoute exact path={DASHBOARD_SLUG}>
         <Dashboard />
+      </ProtectedRoute>
+      <ProtectedRoute path="/temp-pretend-payment">
+        <TemporaryStripeCheckoutTest />
       </ProtectedRoute>
     </BrowserRouter>
   );
--- spa/src/components/authentication/util.js
+++ spa/src/components/authentication/util.js
@@ -1,6 +1,7 @@
 import { LS_USER, LS_CSRF_TOKEN } from "constants/authConstants";
 
 export function handleLoginSuccess(data) {
+  console.log("user org: ", data.user.organization);
   localStorage.setItem(LS_USER, JSON.stringify(data.user));
   localStorage.setItem(LS_CSRF_TOKEN, data.csrftoken);
 }

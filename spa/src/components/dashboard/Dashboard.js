8fa6ebda3cf006542e55aee15d64d047f74ffd2b
Michael Ashton <michael.ashton@caktusgroup.com>
2021-05-07 11:43:44 -0400

    [NRH-30] begin dashboard proper

--- spa/src/components/dashboard/Dashboard.js
+++ spa/src/components/dashboard/Dashboard.js
@@ -1,10 +1,26 @@
+import { useState, useEffect } from 'react';
 import * as S from './Dashboard.styled';
+
+// Children
 import LogoutButton from 'components/authentication/LogoutButton';
+
+// Hooks
+import useUser from 'hooks/useUser';
+
+// Utils
+import userHasPaymentProvider from 'utilities/userHasPaymentProvider';
 
 // TEMP
 import { Link } from 'react-router-dom';
 
-function Dashboard(props) {
+function Dashboard() {
+  const user = useUser();
+  const [hasPaymentProvider, setHasPaymentProvider] = useState(false);
+
+  useEffect(() => {
+    setHasPaymentProvider(userHasPaymentProvider(user));
+  }, [user]);
+
   return (
     <S.Dashboard>
       <p>Dashboard</p>

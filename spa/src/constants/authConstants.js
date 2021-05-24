1c339fa04fdc306a89c9b6ead6ac5704abcf19b7
Benjamin White <960351+benjaminEwhite@users.noreply.github.com>
2021-05-24 15:03:38 -0400

    update constant for PASWORD_RESET_URL

--- spa/src/constants/authConstants.js
+++ spa/src/constants/authConstants.js
@@ -1,4 +1,4 @@
 export const LS_USER = 'REVENGINE_USER';
 export const LS_CSRF_TOKEN = 'CSRF_TOKEN';
 export const CSRF_HEADER = 'X-CSRFTOKEN';
-export const PASSWORD_RESET_URL = '/accounts/reset-password/';
+export const PASSWORD_RESET_URL = '/users/password-reset/';

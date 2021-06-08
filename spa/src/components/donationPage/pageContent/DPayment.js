91fc35f568bced063c9f768a4ca3e1292a3a7092
Michael Ashton <michael.ashton@caktusgroup.com>
2021-06-08 14:29:17 -0400

    [NRH-37] WIP begin dynamic page

---
+++ spa/src/components/donationPage/pageContent/DPayment.js
@@ -0,0 +1,11 @@
+import * as S from './DPayment.styled';
+
+function DPayment(props) {
+  return (
+    <S.DPayment>
+      <p>DPayment</p>
+    </S.DPayment>
+  );
+}
+
+export default DPayment;
---
+++ spa/src/components/donationPage/pageContent/DPayment.styled.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+import ContentBase from './ContentBase';
+
+export const DPayment = styled(ContentBase)``;

21441d621c6c9ef3851fc9eb37e462b48059e169
Michael Ashton <michael.ashton@caktusgroup.com>
2021-06-12 09:21:59 -0400

    [NRH-109] amount responds to frequency, tests

--- spa/src/components/donationPage/DonationPage.js
+++ spa/src/components/donationPage/DonationPage.js
@@ -2,39 +2,33 @@ import { useState, createContext, useContext } from 'react';
 import * as S from './DonationPage.styled';
 import * as getters from 'components/donationPage/pageGetters';
 
-// TEMP
-// import TemporaryStripeCheckoutTest from 'components/TEMP/TemporaryStripeCheckoutTest';
-
 const DonationPageContext = createContext({});
-const ElementsContext = createContext([]);
 
 function DonationPage({ page, live = false }) {
-  const [elements, setElements] = useState(page?.elements || []);
-
+  const [frequency, setFrequency] = useState('one_time');
   return (
-    <DonationPageContext.Provider value={{ page, live }}>
-      <ElementsContext.Provider value={[elements, setElements]}>
-        <S.DonationPage data-testid="donation-page">
-          {getters.getHeaderBarElement()}
-          <S.PageMain>
-            <S.SideOuter>
-              <S.SideInner>
-                {getters.getPageHeadingElement()}
-                <S.DonationContent>
-                  {getters.getGraphicElement()}
-                  <S.PageElements>{elements.map((element) => getters.getDynamicElement(element, live))}</S.PageElements>
-                </S.DonationContent>
-              </S.SideInner>
-            </S.SideOuter>
-            {page.show_benefits && getters.getBenefitsElement()}
-          </S.PageMain>
-        </S.DonationPage>
-      </ElementsContext.Provider>
+    <DonationPageContext.Provider value={{ page, frequency, setFrequency }}>
+      <S.DonationPage data-testid="donation-page">
+        {getters.getHeaderBarElement()}
+        <S.PageMain>
+          <S.SideOuter>
+            <S.SideInner>
+              {getters.getPageHeadingElement()}
+              <S.DonationContent>
+                {getters.getGraphicElement()}
+                <S.PageElements>
+                  {page?.elements.map((element) => getters.getDynamicElement(element, live))}
+                </S.PageElements>
+              </S.DonationContent>
+            </S.SideInner>
+          </S.SideOuter>
+          {page.show_benefits && getters.getBenefitsElement()}
+        </S.PageMain>
+      </S.DonationPage>
     </DonationPageContext.Provider>
   );
 }
 
 export const usePage = () => useContext(DonationPageContext);
-export const useElements = () => useContext(ElementsContext);
 
 export default DonationPage;

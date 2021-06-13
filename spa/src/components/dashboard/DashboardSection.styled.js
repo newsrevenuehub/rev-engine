0a2728ff6e0cf281bd8bb85dfd537b2ed4a9d327
Michael Ashton <michael.ashton@caktusgroup.com>
2021-06-13 10:12:46 -0400

    restyle and reconfigure dashboard to match fundjournalism

--- spa/src/components/dashboard/DashboardSection.styled.js
+++ spa/src/components/dashboard/DashboardSection.styled.js
@@ -1,9 +1,22 @@
 import styled from 'styled-components';
 
 export const DashboardSection = styled.section`
-  border-radius: ${(props) => props.theme.radii[1]};
-  box-shadow: ${(props) => props.theme.shadows[1]};
+  border-radius: ${(props) => props.theme.radii[0]};
+  box-shadow: ${(props) => props.theme.shadows[0]};
   background: ${(props) => props.theme.colors.paneBackground};
   min-height: 200px;
+`;
+
+export const SectionHeading = styled.h2`
+  text-align: center;
+  font-size: ${(props) => props.theme.fontSizes[1]};
+  background: ${(props) => props.theme.colors.primary};
+  color: ${(props) => props.theme.colors.white};
+  border-top-right-radius: ${(props) => props.theme.radii[0]};
+  border-top-left-radius: ${(props) => props.theme.radii[0]};
+  padding: 0.5rem;
+`;
+
+export const SectionContent = styled.div`
   padding: 2rem;
 `;
---
+++ spa/src/components/dashboard/sidebar/DashboardSidebar.js
@@ -0,0 +1,44 @@
+import * as S from './DashboardSidebar.styled';
+import { DONATIONS_SLUG, PAGES_SLUG, MAIN_CONTENT_SLUG, ORGANIZATION_SLUG } from 'routes';
+import { ICONS } from 'assets/icons/SvgIcon';
+
+// Hooks
+import useUser from 'hooks/useUser';
+
+// Util
+import logout from 'components/authentication/logout';
+
+function DashboardSidebar({ shouldAllowDashboard }) {
+  const user = useUser();
+
+  const handleClick = (e) => {
+    if (!shouldAllowDashboard) e.preventDefault();
+  };
+
+  return (
+    <S.DashboardSidebar>
+      <S.NavList>
+        <S.NavItem exact to={MAIN_CONTENT_SLUG} onClick={handleClick} disabled={!shouldAllowDashboard}>
+          Overview
+        </S.NavItem>
+        <S.NavItem to={PAGES_SLUG} onClick={handleClick} disabled={!shouldAllowDashboard}>
+          Pages
+        </S.NavItem>
+        <S.NavItem to={DONATIONS_SLUG} onClick={handleClick} disabled={!shouldAllowDashboard}>
+          Donations
+        </S.NavItem>
+        <S.NavItem to={ORGANIZATION_SLUG} onClick={handleClick} disabled={!shouldAllowDashboard}>
+          {user?.organization?.name}
+        </S.NavItem>
+      </S.NavList>
+      <S.OtherContent>
+        <S.Logout onClick={logout} whileHover={{ scale: 1.05, x: -3 }} whileTap={{ scale: 1, x: 0 }}>
+          <S.LogoutIcon icon={ICONS.LOGOUT} />
+          Sign out
+        </S.Logout>
+      </S.OtherContent>
+    </S.DashboardSidebar>
+  );
+}
+
+export default DashboardSidebar;

b459dcbba313dda72afe0a1cd8bc89628dfe060d
Michael Ashton <michael.ashton@caktusgroup.com>
2021-06-15 17:36:24 -0400

    [NRH-118] donorinfo and payment edits not needed

---
+++ spa/src/components/pageEditor/elementEditors/donorInfo/DonorInfoEditor.styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const DonorInfoEditor = styled.h4`
+  margin: 3rem 0 3rem 6rem;
+`;
--- spa/src/components/pageEditor/elementEditors/index.js
+++ spa/src/components/pageEditor/elementEditors/index.js
@@ -1,3 +1,5 @@
 export { default as RichTextEditor } from './richText/RichTextEditor';
 export { default as FrequencyEditor } from './frequency/FrequencyEditor';
 export { default as AmountEditor } from './amount/AmountEditor';
+export { default as DonorInfoEditor } from './donorInfo/DonorInfoEditor';
+export { default as PaymentEditor } from './payment/PaymentEditor';

8b1de2cefbe0ba0364f1f21eb0b282da8f481c73
Michael Ashton <michael.ashton@caktusgroup.com>
2021-06-15 15:55:02 -0400

    [NRH-117] editable amounts

--- spa/src/components/pageEditor/elementEditors/index.js
+++ spa/src/components/pageEditor/elementEditors/index.js
@@ -1,2 +1,3 @@
 export { default as RichTextEditor } from './richText/RichTextEditor';
 export { default as FrequencyEditor } from './frequency/FrequencyEditor';
+export { default as AmountEditor } from './amount/AmountEditor';

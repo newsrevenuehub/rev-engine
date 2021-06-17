6d0231338592ab6669541548eb22bbf978380d39
Jeremy Gibson <jgibson@caktusgroup.com>
2021-06-17 12:34:43 -0400

    refs #NRH-51 Makes templates readonly, and adds some logging

--- apps/pages/admin.py
+++ apps/pages/admin.py
@@ -1,8 +1,14 @@
+import logging
+
+from django.conf import settings
 from django.contrib import admin, messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from apps.pages.models import Benefit, BenefitTier, DonationPage, DonorBenefit, Style, Template
+
+
+logger = logging.getLogger(f"{settings.DEFAULT_LOGGER}.{__name__}")
 
 
 class DonationPageAdminAbstract(admin.ModelAdmin):
@@ -54,9 +60,24 @@ class TemplateAdmin(DonationPageAdminAbstract):
 
     change_form_template = "pages/templates_changeform.html"
 
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        return [x for x in obj.field_names()]
+
     def response_change(self, request, obj):
         if "_page-from-template" in request.POST:
-            new_page = obj.make_page_from_template()
+            try:
+                new_page = obj.make_page_from_template()
+            except Template.TemplateError as e:
+                logger.error(e)
+                self.message_user(
+                    request,
+                    "Something went wrong. The page this template was created from cannot be found.",
+                    messages.ERROR,
+                )
+                return HttpResponseRedirect(reverse("admin:pages_template_change", kwargs={"object_id": obj.id}))
             return HttpResponseRedirect(reverse("admin:pages_donationpage_change", kwargs={"object_id": new_page.id}))
         return super().response_change(request, obj)
 

5f3f3ad3fccb5170fcbe4a8fb4a9afe0cd3a0583
Michael Ashton <michael.ashton@caktusgroup.com>
2021-06-30 15:29:47 -0400

    [NRH-151] cypress tests

--- spa/cypress/integration/contributor.spec.js
+++ spa/cypress/integration/contributor.spec.js
@@ -7,13 +7,29 @@ describe('Contributor portal', () => {
     cy.visit(CONTRIBUTOR_ENTRY);
   });
 
-  describe('Contributor magic link authentication', () => {
+  describe('Contributor magic link request', () => {
     it('should send request with provided email', () => {
-      cy.intercept('POST', getEndpoint(GET_MAGIC_LINK), (req) => {
-        console.log('request', req);
-      });
-      cy.getByTestId('magic-link-email-input').type('test@testing.com');
+      const email = 'test@testing.com';
+      cy.intercept('POST', getEndpoint(GET_MAGIC_LINK)).as('getMagicLink');
+      cy.getByTestId('magic-link-email-input').type(email);
       cy.getByTestId('magic-link-email-button').click();
+      return cy.wait('@getMagicLink').then((intercept) => {
+        expect(intercept.request.body.email).equal(email);
+      });
+    });
+
+    it('should display server generated error on email if non-200 status', () => {
+      const emailError = ['email error'];
+      cy.intercept(
+        { method: 'POST', url: getEndpoint(GET_MAGIC_LINK) },
+        { body: { email: emailError }, statusCode: 400 }
+      ).as('getMagicLink');
+      cy.getByTestId('magic-link-email-button').click();
+    });
+    it('should display generic success message if status 200', () => {
+      cy.intercept({ method: 'POST', url: getEndpoint(GET_MAGIC_LINK) }, { statusCode: 200 }).as('getMagicLink');
+      cy.getByTestId('magic-link-email-button').click();
+      cy.contains("If you're in our system, an email has been sent to you containing your magic link");
     });
   });
 });

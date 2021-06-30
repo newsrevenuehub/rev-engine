518d0207b44d3f349cb4a1d484115d1a7b7047b3
Michael Ashton <michael.ashton@caktusgroup.com>
2021-06-30 10:44:05 -0400

    add additional task logging to auto_accept_flagged_con

--- apps/contributions/tasks.py
+++ apps/contributions/tasks.py
@@ -1,4 +1,3 @@
-import logging
 import time
 
 from django.conf import settings
@@ -6,26 +5,27 @@ from django.utils import timezone
 
 import requests
 from celery import shared_task
+from celery.utils.log import get_task_logger
 from requests.exceptions import RequestException
 
 from apps.contributions.models import Contribution, ContributionStatus
 from apps.contributions.payment_managers import PaymentProviderError
 
 
-logger = logging.getLogger(f"{settings.DEFAULT_LOGGER}.{__name__}")
+logger = get_task_logger(f"{settings.DEFAULT_LOGGER}.{__name__}")
 
 
 def ping_healthchecks(check_name, healthcheck_url):  # pragma: no cover
     """Attempt to ping a healthchecks.io to enable monitoring of tasks"""
     if not healthcheck_url:
-        logger.warn(f"URL for {check_name} not available in this environment")
+        logger.warning(f"URL for {check_name} not available in this environment")
         return
     for attempt in range(1, 4):
         try:
             requests.get(healthcheck_url, timeout=1)
             break
         except RequestException:
-            logger.warn(f"Request {attempt} for {check_name} healthcheck failed")
+            logger.warning(f"Request {attempt} for {check_name} healthcheck failed")
         time.sleep(1)
 
 
@@ -42,7 +42,11 @@ def auto_accept_flagged_contributions():
     eligible_flagged_contributions = Contribution.objects.filter(
         status=ContributionStatus.FLAGGED, flagged_date__lte=now - settings.FLAGGED_PAYMENT_AUTO_ACCEPT_DELTA
     )
+    logger.info(f"Found {len(eligible_flagged_contributions)} eligible flagged contributions")
+
     for contribution in eligible_flagged_contributions:
+        logger.info(f"contribution.status {contribution.status}")
+        logger.info(f"contribution.flagged_data {contribution.flagged_data}")
         payment_intent = contribution.get_payment_manager_instance()
         try:
             payment_intent.complete_payment(reject=False)

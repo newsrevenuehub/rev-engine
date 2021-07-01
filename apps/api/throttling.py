ba55f800105639a77ebdc6aee66cc1fa2dc7d0a9
Michael Ashton <michael.ashton@caktusgroup.com>
2021-07-01 13:59:55 -0400

    [NRH-149] implement rate throttling

--- apps/api/throttling.py
+++ apps/api/throttling.py
@@ -1,11 +1,14 @@
+from django.conf import settings
+
 from rest_framework.throttling import SimpleRateThrottle
 
 
 class ContributorRateThrottle(SimpleRateThrottle):
+    rate = settings.CONTRIBUTOR_MAGIC_LINK_REQUEST_THROTTLE_RATE
+
     def get_cache_key(self, request, view):
         if email := request.data.get("email", None):
             ident = email
         else:
             ident = self.get_ident(request)
-
         return self.cache_format % {"scope": self.scope, "ident": ident}

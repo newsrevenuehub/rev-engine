edf3140e7b87bbcd820f9c69c06c2021fe3e3b9a
Benjamin White <bwhite@caktusgroup.com>
2021-07-13 14:38:20 -0400

    management command to seed dev db

--- apps/common/management/commands/seed_dev_db.py
+++ apps/common/management/commands/seed_dev_db.py
@@ -1,73 +1,88 @@
-from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+
 from faker import Faker
 
 import apps.contributions.tests.factories as contrib_factories
 import apps.organizations.tests.factories as org_factories
+import apps.users.tests.factories as user_factories
+
 
 faker = Faker()
-
-
-def _what_happened(
-    instances,
-    display_name,
-    instance_attr="pk",
-    child_additions_zip_key=None,
-    child_display_name=None,
-    child_instance_attr=None,
-):
-    if not child_additions_zip_key:
-        return [f"{len(instances)} of {display_name} are available"] + [
-            f"*   {getattr(instance, instance_attr)}" for instance in instances
-        ]
-    if child_additions_zip_key:
-        happenings = []
-        for instance in instances:
-            for child_added in getattr(instance, child_additions_zip_key):
-                happenings.append(
-                    f"Added a {child_display_name} to {display_name} whose attribute "
-                    f"`{child_instance_attr}` value is {getattr(child_added, child_instance_attr)}"
-                )
-        return happenings
 
 
 def create_n_instances(n, model_factory, **model_factory_kwargs):
     return [model_factory(**model_factory_kwargs) for n in range(n)]
 
 
-def create_n_users_for_org(org, n=1, password="tHiSiStHePaSwOrD"):
-    """Create n number of users. This uses get_or_creat"""
-    for _ in range(n):
-        org.users.add(create_n_instances(n, get_user_model(), password=password))
-    org.save()
-    return org.refresh_from_db()
+def create_org_users(orgs, n=3, password="tHiSiStHePaSwOrD"):
+    org_users = []
+    for org in orgs:
+        users = create_n_instances(n, user_factories.OrganizationUserFactory, user_password=password, organization=org)
+        org_users.extend(users)
+    return org_users
 
 
 def create_orgs(n=3):
     return create_n_instances(n, org_factories.OrganizationFactory)
 
+
+def create_contributors(n=3):
+    return create_n_instances(n, contrib_factories.ContributorFactory)
+
+
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
-            "n-base",
+            "--n-orgs",
             type=int,
             default=3,
-            help=("How many of each entity to make, within its particular place in model graph",),
+            help=("How many orgs to create",),
+        )
+        parser.add_argument(
+            "--n-org-users",
+            type=int,
+            default=3,
+            help=("How many org users to create per org",),
+        )
+        parser.add_argument(
+            "--n-contributors",
+            type=int,
+            default=3,
+            help=("How contributors to create",),
+        )
+        parser.add_argument(
+            "--n-contributions",
+            type=int,
+            default=3,
+            help=("How many contributions to create per contributor per org",),
         )
 
     def handle(self, *args, **options):
-        n_base = options["n_base"]
+        n_orgs = options["n_orgs"]
+        n_org_users = options["n_org_users"]
+        n_contributors = options["n_contributors"]
+        n_contributions = options["n_contributions"]
 
-        org_users = create_n_users_for_org(n=n_base)
-        self.stdout.write(_what_happened(org_users, "Organization users", "email"))
+        orgs = create_orgs(n=n_orgs)
+        msg = f"Created (or retrieved) {len(orgs)} organizations"
+        for org in orgs:
+            msg += f"/n/t -{org.name} | ID: {org.pk}"
+        self.stdout.write(msg)
 
-        orgs = create_orgs(n=n_base)
-        self.stdout.write(_what_happened(orgs, "Organizations", "name"))
+        org_users = create_org_users(orgs, n=n_org_users)
+        msg = f"Created (or retrieved) {len(org_users)} organization users"
+        for org_user in org_users:
+            msg += f"/n/t -{org_user.user.email} | ID: {org_user.pk} | Org: {org_user.organization.name}"
+        self.stdout.write(msg)
 
-        for index, item in orgs:
-            user in org_users:
+        contributors = create_contributors(n=n_contributors)
+        self.stdout.write(f"Created (or retrieved) {len(contributors)} contributors")
 
-            orgs,
-            org_user_transforms=None,
-        )
-        pass
+        contributions = []
+        for contributor in contributors:
+            for org in orgs:
+                for n in range(n_contributions):
+                    contributions.append(
+                        contrib_factories.ContributionFactory(organization=org, contributor=contributor)
+                    )
+        msg = f"Created {len(contributions)} contributions"
--- apps/contributions/tests/factories.py
+++ apps/contributions/tests/factories.py
@@ -1,23 +1,67 @@
+import datetime
+import random
+
+from django.conf import settings
+
 import factory
+import pytz
 from factory.django import DjangoModelFactory
+from factory.helpers import create
 from faker import Faker
 
+from apps.common.tests.test_utils import generate_random_datetime
 from apps.contributions import models
 
 
-faker = Faker()
+fake = Faker()
 
 
 class ContributorFactory(DjangoModelFactory):
     class Meta:
         model = models.Contributor
+        django_get_or_create = ("email",)
 
-    email = faker.email()
+    email = factory.Sequence(lambda n: f"{fake.user_name()}-{n}@{fake.domain_name()}")
+
+
+def _get_flagged_date(bad_actor_score, created_at):
+    if bad_actor_score >= settings.BAD_ACTOR_FAIL_ABOVE:
+        return created_at + datetime.timedelta(hours=1)
+    return None
+
+
+def _get_status(bad_actor_score):
+    if bad_actor_score >= settings.BAD_ACTOR_FAIL_ABOVE:
+        return models.ContributionStatus.FLAGGED[0]
+    return random.choice(
+        [
+            models.ContributionStatus.PROCESSING[0],
+            models.ContributionStatus.PAID[0],
+            models.ContributionStatus.CANCELED[0],
+        ]
+    )
+
+
+def _get_last_payment_date(created_date, bad_actor_score):
+    if bad_actor_score >= settings.BAD_ACTOR_FAIL_ABOVE:
+        return None
+    return created_date + datetime.timedelta(hours=1)
+
+
+NOW = datetime.datetime.now(tz=pytz.UTC) - datetime.timedelta(days=1)
+THEN = NOW - datetime.timedelta(weeks=52)
 
 
 class ContributionFactory(DjangoModelFactory):
     class Meta:
         model = models.Contribution
 
-    amount = faker.random_digit()
+    created = factory.LazyFunction(lambda: generate_random_datetime(THEN, NOW))
+    amount = factory.LazyFunction(lambda: random.randrange(1, 10000) * 100)
+    reason = factory.LazyFunction(lambda: fake.paragraph())
+    interval = factory.LazyFunction(lambda: random.choice(models.ContributionInterval.choices)[0])
+    bad_actor_score = factory.LazyFunction(lambda: random.choice([0, 1, 2, 3, 4]))
+    last_payment_date = factory.LazyAttribute(lambda o: _get_last_payment_date(o.created, o.bad_actor_score))
+    flagged_date = factory.LazyAttribute(lambda o: _get_flagged_date(o.bad_actor_score, o.created))
+    status = factory.LazyAttribute(lambda o: _get_status(o.bad_actor_score))
     payment_provider_used = "Stripe"

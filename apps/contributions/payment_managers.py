29963bc0ba7d14b79daffa1bd648e8353430de7b
Michael Ashton <michael.ashton@caktusgroup.com>
2021-07-16 16:05:44 -0400

    [NRH-196] logs error and skips slack message if no integration

--- apps/contributions/payment_managers.py
+++ apps/contributions/payment_managers.py
@@ -151,7 +151,6 @@ class PaymentManager:
 
         return Contribution.objects.create(
             amount=self.validated_data["amount"],
-            reason=self.validated_data["reason"],
             interval=self.validated_data["interval"],
             status=status,
             donation_page=donation_page,

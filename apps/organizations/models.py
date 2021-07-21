f2f0c81f556a757c8f7a583281821d1b839e3b88
Jeremy Gibson <jgibson@caktusgroup.com>
2021-07-21 15:07:32 -0400

    refs #NRH-179 Adds a model field. Tests and guard based on field.

--- apps/organizations/models.py
+++ apps/organizations/models.py
@@ -87,6 +87,7 @@ class Organization(IndexedTimeStampedModel):
     )
     stripe_product_id = models.CharField(max_length=255, blank=True)
     domain_apple_verified_date = models.DateTimeField(null=True)
+    uses_email_templates = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name

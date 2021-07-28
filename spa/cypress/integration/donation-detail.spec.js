0cde6589a30940343a34194d86a4e0c6c5df04c3
Michael Ashton <michael.ashton@caktusgroup.com>
2021-07-28 14:33:48 -0400

    [NRH-152] add cypress tests

--- spa/cypress/integration/donation-detail.spec.js
+++ spa/cypress/integration/donation-detail.spec.js
@@ -2,8 +2,10 @@ import unflaggedContributionDetailData from '../fixtures/donations/donation-not-
 import prevFlaggedContributionDetailData from '../fixtures/donations/donation-prev-flagged.json';
 import flaggedContributionDetailData from '../fixtures/donations/donation-flagged.json';
 
-import { CONTRIBUTIONS } from 'ajax/endpoints';
+import { DONATIONS_SLUG } from 'routes';
+import { CONTRIBUTIONS, PROCESS_FLAGGED } from 'ajax/endpoints';
 import { getEndpoint } from '../support/util';
+import { GENERIC_ERROR } from 'constants/textConstants';
 
 const CONTRIBUTION_PK = 123;
 
@@ -13,11 +15,11 @@ describe('Donation detail', () => {
       cy.login('user/stripe-verified.json');
       cy.intercept('GET', getEndpoint(`${CONTRIBUTIONS}/${CONTRIBUTION_PK}/`), {
         body: unflaggedContributionDetailData
-      }).as('getDonation');
+      }).as('getUnflaggedDonation');
       cy.visit(`/dashboard/donations/${CONTRIBUTION_PK}`);
     });
     it('should display donation details', () => {
-      cy.wait('@getDonation');
+      cy.wait('@getUnflaggedDonation');
       cy.getByTestId('donation-detail');
       const expectedAttrs = [
         'donorEmail',
@@ -33,17 +35,17 @@ describe('Donation detail', () => {
       expectedAttrs.forEach((attr) => cy.getByTestId(attr).invoke('text').should('have.length.gte', 1));
     });
   });
-  describe('Previously but not-longer-flagged donation', () => {
+  describe('Previously but no-longer-flagged donation', () => {
     beforeEach(() => {
       cy.login('user/stripe-verified.json');
       cy.intercept('GET', getEndpoint(`${CONTRIBUTIONS}/${CONTRIBUTION_PK}/`), {
         body: prevFlaggedContributionDetailData
-      }).as('getDonation');
+      }).as('getNoLongerFlaggedDonation');
       cy.visit(`/dashboard/donations/${CONTRIBUTION_PK}`);
     });
 
     it('should display flagged details in addition to donation details', () => {
-      cy.wait('@getDonation');
+      cy.wait('@getNoLongerFlaggedDonation');
       cy.getByTestId('donation-detail');
       const expectedAttrs = [
         'donorEmail',
@@ -64,9 +66,120 @@ describe('Donation detail', () => {
   });
 
   describe('Flagged donation', () => {
-    cy.intercept('GET', getEndpoint(`${CONTRIBUTIONS}/${CONTRIBUTION_PK}/`), {
-      body: flaggedContributionDetailData
-    }).as('getDonation');
-    cy.visit(`/dashboard/donations/${CONTRIBUTION_PK}`);
+    before(() => {
+      cy.login('user/stripe-verified.json');
+      cy.intercept('GET', getEndpoint(`${CONTRIBUTIONS}${CONTRIBUTION_PK}/`), {
+        body: flaggedContributionDetailData
+      }).as('getFlaggedDonation');
+      cy.visit(`${DONATIONS_SLUG}/${CONTRIBUTION_PK}`);
+    });
+
+    it('should show flagged details with accept/reject buttons', () => {
+      cy.wait('@getFlaggedDonation');
+      cy.getByTestId('flaggedDate').should('exist');
+      cy.getByTestId('accept-flagged-button').should('exist');
+      cy.getByTestId('reject-flagged-button').should('exist');
+    });
+
+    it('should show comfirmation modal when reject is clicked', () => {
+      cy.getByTestId('reject-flagged-button').click();
+      cy.getByTestId('confirmation-modal').should('exist');
+    });
+
+    it('should do nothing if cancel is clicked', () => {
+      cy.getByTestId('cancel-button').click();
+      // How on earth do we test "does nothing" in cypress?
+    });
+
+    it('should make request with proper body if continue is clicked', () => {
+      // There's a frustrating issue with cypress and the way we're utilizing React.createPortal for the confirmation modal.
+      // For whatever reason, cypress returns to the login screen here.
+      const contributionId = flaggedContributionDetailData.id;
+      cy.login('user/stripe-verified.json');
+      cy.intercept('GET', getEndpoint(`${CONTRIBUTIONS}${contributionId}/`), {
+        body: flaggedContributionDetailData
+      }).as('getFlaggedDonation');
+      cy.visit(`${DONATIONS_SLUG}/${contributionId}`);
+
+      // The proper test
+      cy.getByTestId('reject-flagged-button').click();
+      cy.intercept('POST', getEndpoint(`${CONTRIBUTIONS}${contributionId}/${PROCESS_FLAGGED}`), {
+        body: { detail: 'rejected' }
+      }).as('rejectedSuccess');
+      cy.getByTestId('continue-button').click();
+      return cy.wait('@rejectedSuccess').then((interception) => {
+        expect(interception.request.body.reject).to.equal(true);
+      });
+    });
+
+    it('should show error message if reject fails', () => {
+      const contributionId = flaggedContributionDetailData.id;
+      cy.getByTestId('reject-flagged-button').click();
+      cy.intercept('POST', getEndpoint(`${CONTRIBUTIONS}${contributionId}/${PROCESS_FLAGGED}`), {
+        statusCode: 400
+      }).as('rejectFailed');
+      cy.getByTestId('continue-button').click();
+      cy.wait('@rejectFailed');
+      cy.getByTestId('alert').contains(GENERIC_ERROR);
+    });
+
+    it('should show success message if reject succeeds', () => {
+      // There's a frustrating issue with cypress and the way we're utilizing React.createPortal for the confirmation modal.
+      // For whatever reason, cypress returns to the login screen here.
+      const contributionId = flaggedContributionDetailData.id;
+      cy.login('user/stripe-verified.json');
+      cy.intercept('GET', getEndpoint(`${CONTRIBUTIONS}${contributionId}/`), {
+        body: flaggedContributionDetailData
+      }).as('getFlaggedDonation');
+      cy.visit(`${DONATIONS_SLUG}/${contributionId}`);
+
+      cy.getByTestId('reject-flagged-button').click();
+      cy.intercept('POST', getEndpoint(`${CONTRIBUTIONS}${contributionId}/${PROCESS_FLAGGED}`), {
+        body: { detail: 'rejected' }
+      }).as('rejectedSuccess');
+      cy.getByTestId('continue-button').click();
+      cy.wait('@rejectedSuccess');
+      cy.getByTestId('alert').contains('Donation successfully rejected');
+    });
+
+    it('should make request with proper body if accept is clicked', () => {
+      // There's a frustrating issue with cypress and the way we're utilizing React.createPortal for the confirmation modal.
+      // For whatever reason, cypress returns to the login screen here.
+      const contributionId = flaggedContributionDetailData.id;
+      cy.login('user/stripe-verified.json');
+      cy.intercept('GET', getEndpoint(`${CONTRIBUTIONS}${contributionId}/`), {
+        body: flaggedContributionDetailData
+      }).as('getFlaggedDonation');
+      cy.visit(`${DONATIONS_SLUG}/${contributionId}`);
+
+      cy.intercept('POST', getEndpoint(`${CONTRIBUTIONS}${contributionId}/${PROCESS_FLAGGED}`), {
+        body: { detail: 'accepted' }
+      }).as('acceptSuccess');
+      cy.getByTestId('accept-flagged-button').click();
+
+      return cy.wait('@acceptSuccess').then((interception) => {
+        expect(interception.request.body.reject).to.equal(false);
+      });
+    });
+
+    it('should show error message if accept fails', () => {
+      const contributionId = flaggedContributionDetailData.id;
+      cy.intercept('POST', getEndpoint(`${CONTRIBUTIONS}${contributionId}/${PROCESS_FLAGGED}`), {
+        statusCode: 400
+      }).as('acceptFailure');
+      cy.getByTestId('accept-flagged-button').click();
+      cy.wait('@acceptFailure');
+      cy.getByTestId('alert').contains(GENERIC_ERROR);
+    });
+
+    it('should show success message if accept succeeds', () => {
+      const contributionId = flaggedContributionDetailData.id;
+      cy.intercept('POST', getEndpoint(`${CONTRIBUTIONS}${contributionId}/${PROCESS_FLAGGED}`), {
+        body: { detail: 'accepted' }
+      }).as('acceptSuccess');
+      cy.getByTestId('accept-flagged-button').click();
+      cy.wait('@acceptSuccess');
+      cy.getByTestId('alert').contains('Donation successfully accepted');
+    });
   });
 });

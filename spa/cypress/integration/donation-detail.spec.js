e2d1d5c8dc4b76a61ec976dfeef9737ce0423477
Michael Ashton <michael.ashton@caktusgroup.com>
2021-07-28 09:57:55 -0400

    [NRH-152] testing wip

--- spa/cypress/integration/donation-detail.spec.js
+++ spa/cypress/integration/donation-detail.spec.js
@@ -1,31 +1,72 @@
-import contributionDetailData from '../fixtures/donations/donation-244.json';
+import unflaggedContributionDetailData from '../fixtures/donations/donation-not-flagged.json';
+import prevFlaggedContributionDetailData from '../fixtures/donations/donation-prev-flagged.json';
+import flaggedContributionDetailData from '../fixtures/donations/donation-flagged.json';
+
 import { CONTRIBUTIONS } from 'ajax/endpoints';
 import { getEndpoint } from '../support/util';
 
-const CONTRIBUTION_DETAIL_PK = '244';
+const CONTRIBUTION_PK = 123;
 
-describe('Donation page', () => {
-  beforeEach(() => {
-    cy.login('user/stripe-verified.json');
-    cy.intercept('GET', getEndpoint(`${CONTRIBUTIONS}/${CONTRIBUTION_DETAIL_PK}/`), {
-      body: contributionDetailData
-    }).as('getDonation');
-    cy.visit(`/dashboard/donations/${CONTRIBUTION_DETAIL_PK}`);
+describe('Donation detail', () => {
+  describe('Unflagged donation', () => {
+    beforeEach(() => {
+      cy.login('user/stripe-verified.json');
+      cy.intercept('GET', getEndpoint(`${CONTRIBUTIONS}/${CONTRIBUTION_PK}/`), {
+        body: unflaggedContributionDetailData
+      }).as('getDonation');
+      cy.visit(`/dashboard/donations/${CONTRIBUTION_PK}`);
+    });
+    it('should display donation details', () => {
+      cy.wait('@getDonation');
+      cy.getByTestId('donation-detail');
+      const expectedAttrs = [
+        'donorEmail',
+        'amount',
+        'interval',
+        'lastPaymentDate',
+        'paymentProvider',
+        'payment-resource-link',
+        'subscription-resource-link',
+        'customer-resource-link',
+        'status'
+      ];
+      expectedAttrs.forEach((attr) => cy.getByTestId(attr).invoke('text').should('have.length.gte', 1));
+    });
   });
-  it('should display donation details', () => {
-    cy.wait('@getDonation');
-    cy.getByTestId('donation-detail');
-    const expectedAttrs = [
-      'donorEmail',
-      'amount',
-      'interval',
-      'lastPaymentDate',
-      'flaggedDate',
-      'paymentProvider',
-      'paymentProviderCustomerId',
-      'reason',
-      'status'
-    ];
-    expectedAttrs.forEach((attr) => cy.getByTestId(attr).invoke('text').should('have.length.gte', 1));
+  describe('Previously but not-longer-flagged donation', () => {
+    beforeEach(() => {
+      cy.login('user/stripe-verified.json');
+      cy.intercept('GET', getEndpoint(`${CONTRIBUTIONS}/${CONTRIBUTION_PK}/`), {
+        body: prevFlaggedContributionDetailData
+      }).as('getDonation');
+      cy.visit(`/dashboard/donations/${CONTRIBUTION_PK}`);
+    });
+
+    it('should display flagged details in addition to donation details', () => {
+      cy.wait('@getDonation');
+      cy.getByTestId('donation-detail');
+      const expectedAttrs = [
+        'donorEmail',
+        'amount',
+        'interval',
+        'lastPaymentDate',
+        'flaggedDate',
+        'paymentProvider',
+        'payment-resource-link',
+        'subscription-resource-link',
+        'customer-resource-link',
+        'status'
+      ];
+      expectedAttrs.forEach((attr) => cy.getByTestId(attr).invoke('text').should('have.length.gte', 1));
+      cy.getByTestId('accept-flagged-button').should('not.exist');
+      cy.getByTestId('reject-flagged-button').should('not.exist');
+    });
+  });
+
+  describe('Flagged donation', () => {
+    cy.intercept('GET', getEndpoint(`${CONTRIBUTIONS}/${CONTRIBUTION_PK}/`), {
+      body: flaggedContributionDetailData
+    }).as('getDonation');
+    cy.visit(`/dashboard/donations/${CONTRIBUTION_PK}`);
   });
 });

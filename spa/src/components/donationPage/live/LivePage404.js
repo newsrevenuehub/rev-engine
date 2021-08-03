a8122336a649cbc6fbee33ddba497bde412c7a46
Michael Ashton <michael.ashton@caktusgroup.com>
2021-08-03 09:07:46 -0400

    fix a few minor bugs in page creation, style live404 and livefallback

--- spa/src/components/donationPage/live/LivePage404.js
+++ spa/src/components/donationPage/live/LivePage404.js
@@ -3,7 +3,10 @@ import * as S from './LivePage404.styled';
 function LivePage404(props) {
   return (
     <S.LivePage404 data-testid="live-page-404">
-      <p>LivePage404</p>
+      <S.Wrapper>
+        <S.FourOhFour>404</S.FourOhFour>
+        <S.Description>We could not find the page you're looking for.</S.Description>
+      </S.Wrapper>
     </S.LivePage404>
   );
 }

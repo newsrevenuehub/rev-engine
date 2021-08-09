56cf431ce619db94f000394d535b4e8b72a041c3
Michael Ashton <michael.ashton@caktusgroup.com>
2021-08-09 17:22:15 -0400

    [NRH-239] add cypress tests

--- spa/src/components/donationPage/DonationPageFooter.js
+++ spa/src/components/donationPage/DonationPageFooter.js
@@ -2,7 +2,7 @@ import * as S from './DonationPageFooter.styled';
 
 function DonationPageFooter({ page }) {
   return (
-    <S.DonationPageFooter>
+    <S.DonationPageFooter data-testid="donation-page-footer">
       <S.Content>
         <a href="https://fundjournalism.org/" target="_blank" rel="noopener noreferrer">
           What is fundjournalism.org?

a4f3ba144a2c3ce87885e36349e6e3af0906de01
Michael Ashton <michael.ashton@caktusgroup.com>
2021-08-09 13:23:02 -0400

    [NRH-253] add a pre-packaged draftjs lib, configure, style

--- spa/src/components/donationPage/pageContent/DRichText.styled.js
+++ spa/src/components/donationPage/pageContent/DRichText.styled.js
@@ -3,7 +3,17 @@ import styled from 'styled-components';
 export const DRichText = styled.div``;
 
 export const RichTextContent = styled.div`
-  p {
+  p,
+  ul,
+  li {
     line-height: 1.6;
+  }
+
+  blockquote {
+    padding: 0.5rem 0 0.5rem 1rem;
+    border-left: 6px solid ${(props) => props.theme.colors.grey[0]};
+    line-height: 1.6;
+
+    font-weight: 200;
   }
 `;

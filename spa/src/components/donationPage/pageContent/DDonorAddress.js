9268ec61870b09ac0f94732e9aab510296b4aabe
Michael Ashton <michael.ashton@caktusgroup.com>
2021-08-10 15:50:06 -0400

    [NRH-267] begin pulling apart donorinfo, WIP

---
+++ spa/src/components/donationPage/pageContent/DDonorAddress.js
@@ -0,0 +1,11 @@
+import * as S from './DDonorAddress.styled';
+
+function DDonorAddress() {
+  return (
+    <S.DDonorAddress>
+      <p>DDonorAddress</p>
+    </S.DDonorAddress>
+  );
+}
+
+export default DDonorAddress;
--- spa/src/components/donationPage/pageContent/DDonorInfo.js
+++ spa/src/components/donationPage/pageContent/DDonorInfo.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { Grid } from 'semantic-ui-react';
+import Grid from '@material-ui/core/Grid';
 
 // Context
 import { usePage } from 'components/donationPage/DonationPage';
@@ -23,102 +23,92 @@ function DDonorInfo(props) {
   return (
     <>
       <DElement label="Name" {...props} data-testid="d-donor-info">
-        <Grid>
-          <Grid.Row>
-            <Grid.Column tablet={16} computer={8}>
-              <Input
-                type="text"
-                name="first_name"
-                label="First Name"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                errors={errors.first_name}
-              />
-            </Grid.Column>
-            <Grid.Column tablet={16} computer={8}>
-              <Input
-                type="text"
-                name="last_name"
-                label="Last Name"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                errors={errors.last_name}
-              />
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column>
-              <Input
-                type="email"
-                name="email"
-                label="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                errors={errors.email}
-              />
-            </Grid.Column>
-          </Grid.Row>
+        <Grid container spacing={3}>
+          <Grid item md={12} lg={6}>
+            <Input
+              type="text"
+              name="first_name"
+              label="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              errors={errors.first_name}
+            />
+          </Grid>
+          <Grid item md={12} lg={6}>
+            <Input
+              type="text"
+              name="last_name"
+              label="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              errors={errors.last_name}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Input
+              type="email"
+              name="email"
+              label="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              errors={errors.email}
+            />
+          </Grid>
         </Grid>
       </DElement>
       <DElement>
         <Grid>
-          <Grid.Row>
-            <Grid.Column width={16}>
-              <Input
-                type="text"
-                name="mailing_street"
-                label="Address"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                errors={errors.mailing_street}
-              />
-            </Grid.Column>
-          </Grid.Row>
+          <Grid item xs={12}>
+            <Input
+              type="text"
+              name="mailing_street"
+              label="Address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              errors={errors.mailing_street}
+            />
+          </Grid>
 
-          <Grid.Row>
-            <Grid.Column mobile={16} tablet={8} computer={12}>
-              <Input
-                type="text"
-                name="mailing_city"
-                label="City"
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
-                errors={errors.mailing_city}
-              />
-            </Grid.Column>
-            <Grid.Column mobile={16} tablet={8} computer={4}>
-              <Input
-                type="text"
-                name="mailing_state"
-                label="State"
-                value={state}
-                onChange={(e) => setState(e.target.value)}
-                errors={errors.mailing_state}
-              />
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column mobile={16} tablet={8} computer={6}>
-              <Input
-                type="text"
-                name="mailing_postal_code"
-                label="Zip"
-                value={zip}
-                onChange={(e) => setZip(e.target.value)}
-                errors={errors.mailing_postal_code}
-              />
-            </Grid.Column>
-            <Grid.Column mobile={16} tablet={8} computer={10}>
-              <Input
-                type="text"
-                name="mailing_country"
-                label="Country"
-                value={country}
-                onChange={(e) => setCountry(e.target.value)}
-                errors={errors.mailing_country}
-              />
-            </Grid.Column>
-          </Grid.Row>
+          <Grid item xs={12} md={6} lg={4}>
+            <Input
+              type="text"
+              name="mailing_city"
+              label="City"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              errors={errors.mailing_city}
+            />
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <Input
+              type="text"
+              name="mailing_state"
+              label="State"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+              errors={errors.mailing_state}
+            />
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <Input
+              type="text"
+              name="mailing_postal_code"
+              label="Zip"
+              value={zip}
+              onChange={(e) => setZip(e.target.value)}
+              errors={errors.mailing_postal_code}
+            />
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Input
+              type="text"
+              name="mailing_country"
+              label="Country"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+              errors={errors.mailing_country}
+            />
+          </Grid>
         </Grid>
       </DElement>
     </>

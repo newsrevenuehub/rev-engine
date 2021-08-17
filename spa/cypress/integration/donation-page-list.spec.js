2eda7dcba8bff392f0c14d1d4ca85e20a5eca637
Benjamin White <bwhite@caktusgroup.com>
2021-08-17 15:16:06 -0400

    feature seems to work, tests pass

--- spa/cypress/integration/donation-page-list.spec.js
+++ spa/cypress/integration/donation-page-list.spec.js
@@ -3,12 +3,17 @@ import { PAGES_SLUG } from 'routes';
 import { getEndpoint } from '../support/util';
 
 describe('Donation page list', () => {
-  before(() => {
+  beforeEach(() => {
     cy.login('user/stripe-verified.json');
     cy.intercept(
       { method: 'GET', pathname: getEndpoint(LIST_PAGES) },
       { fixture: 'pages/list-pages-1', statusCode: 200 }
     ).as('listPages');
+
+    cy.intercept(
+      { method: 'GET', pathname: getEndpoint(REVENUE_PROGRAMS) },
+      { fixture: 'org/revenue-programs-1', statusCode: 200 }
+    );
 
     cy.visit(PAGES_SLUG);
     cy.url().should('include', PAGES_SLUG);
@@ -26,24 +31,22 @@ describe('Donation page list', () => {
     });
 
     it('should ask if user wants to create rev program if none present', () => {
+      // override intercept set in beforeEach cause for this case we don't want any programs present
+      cy.intercept(
+        { method: 'GET', pathname: getEndpoint(REVENUE_PROGRAMS) },
+        { body: { results: [] }, statusCode: 200 }
+      );
+      cy.getByTestId('page-create-button').click();
       cy.contains('You need to set up a revenue program to create a page. Create one?');
     });
 
     it('should show select if rev programs present', () => {
-      cy.getByTestId('close-modal').click();
-      cy.intercept(
-        { method: 'GET', pathname: getEndpoint(REVENUE_PROGRAMS) },
-        { fixture: 'org/revenue-programs-1', statusCode: 200 }
-      );
       cy.getByTestId('page-create-button').click();
       cy.contains('Select a revenue program');
     });
 
-    // for now, skipping this test. the .type() command hangs, and never seems
-    // to happen. may be related to something with this happening in a portal.
-    // Don't have reason to believe that analytics code is causing regression in object code,
-    // but may somehow be causing test to regress. we'll handle this in a separate ticket.
-    it.skip('should add suggested slug on name field blur', () => {
+    it('should add suggested slug on name field blur', () => {
+      cy.getByTestId('page-create-button').click();
       cy.getByTestId('page-name').type('My Testing Page');
       cy.getByTestId('page-name').blur();
       cy.getByTestId('page-slug').should('have.value', 'my-testing-page');

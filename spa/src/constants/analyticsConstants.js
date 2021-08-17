9df694b55b4a473f20911636250916109f92b66d
Benjamin White <bwhite@caktusgroup.com>
2021-08-17 12:57:12 -0400

    update hub ga v3

--- spa/src/constants/analyticsConstants.js
+++ spa/src/constants/analyticsConstants.js
@@ -2,5 +2,7 @@ export const HUB_ANALYTICS_APP_NAME = 'rev-engine-analytics';
 export const HUB_GA_V3_PLUGIN_NAME = 'ga-v3-hub';
 // when NRH-76 has been resolved, should use line below instead of two below
 // export const { REACT_APP_HUB_V3_GOOGLE_ANALYTICS_ID: HUB_GA_V3_ID } = process.env;
-export const HUB_GA_V3_ID = 'UA-203260249-1';
+const TEMP_HARD_CODED_HUB_GA_V3_ID = 'UA-89391894-3';
+const NONSENSICAL_HUB_GA_V3_ID = 'UA-37373737yesyesyes';
+export const HUB_GA_V3_ID = process.env == 'production' ? TEMP_HARD_CODED_HUB_GA_V3_ID : NONSENSICAL_HUB_GA_V3_ID;
 export const ORG_GA_V3_PLUGIN_NAME = 'ga-v3-org';

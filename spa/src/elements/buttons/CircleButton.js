d5f4d69e683768fe784121adc608be40f1474376
Michael Ashton <michael.ashton@caktusgroup.com>
2021-08-20 13:37:51 -0400

    [NRH-276] fix cypress tests

--- spa/src/elements/buttons/CircleButton.js
+++ spa/src/elements/buttons/CircleButton.js
@@ -3,7 +3,7 @@ import * as S from './CircleButton.styled';
 
 function CircleButton({ icon, color, onClick, disabled, loading, ...props }) {
   return (
-    <S.CircleButton {...props} onClick={disabled || loading ? () => {} : onClick}>
+    <S.CircleButton {...props} disabled={disabled} onClick={disabled || loading ? () => {} : onClick}>
       {loading ? <Spinner /> : <S.Icon icon={icon} type={props.type} color={color} disabled={disabled || loading} />}
     </S.CircleButton>
   );

1dc9bbfb1011ce80b08e208e89a0d3010b673ddd
Michael Ashton <michael.ashton@caktusgroup.com>
2021-08-20 09:00:06 -0400

    [NRH-276] editstyles modal and button states

--- spa/src/components/content/styles/Styles.styled.js
+++ spa/src/components/content/styles/Styles.styled.js
@@ -1,3 +1,6 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-export const Styles = styled.div``;
+export const Styles = styled(motion.div)``;
+
+export const StylesList = styled(motion.ul)``;

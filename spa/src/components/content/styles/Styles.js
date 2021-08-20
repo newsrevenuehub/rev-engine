d16c10236d125d984bb19b2e7dc1d558d84323d1
Michael Ashton <michael.ashton@caktusgroup.com>
2021-08-20 10:39:29 -0400

    [NRH-276] style cards

--- spa/src/components/content/styles/Styles.js
+++ spa/src/components/content/styles/Styles.js
@@ -1,14 +1,47 @@
+import { useState, useEffect } from 'react';
 import * as S from './Styles.styled';
 import { ButtonSection, PlusButton } from 'components/content/pages/Pages.styled';
-
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+// AJAX
+import useRequest from 'hooks/useRequest';
+import { LIST_STYLES } from 'ajax/endpoints';
+import { GENERIC_ERROR } from 'constants/textConstants';
+
+// Deps
+import { useAlert } from 'react-alert';
+
+// Children
 import CircleButton from 'elements/buttons/CircleButton';
+import StyleCard from 'components/content/styles/StyleCard';
 
 function Styles({ setShowEditStylesModal, setStyleToEdit }) {
+  const alert = useAlert();
+  const requestGetStyles = useRequest();
+  const [styles, setStyles] = useState([]);
+
+  // Fetch styles
+  useEffect(() => {
+    requestGetStyles(
+      { method: 'GET', url: LIST_STYLES },
+      {
+        onSuccess: ({ data }) => setStyles(data),
+        onFailure: () => alert.error(GENERIC_ERROR)
+      }
+    );
+  }, [alert]);
+
+  const handleStyleSelect = (style) => {
+    setStyleToEdit(style);
+    setShowEditStylesModal(true);
+  };
+
   return (
     <S.Styles>
       <S.StylesList>
-        <p>Styles</p>
+        {styles.map((style) => (
+          <StyleCard style={style} onSelect={handleStyleSelect} />
+        ))}
       </S.StylesList>
       <ButtonSection>
         <PlusButton onClick={() => setShowEditStylesModal(true)} data-testid="style-create-button">
--- spa/src/components/content/styles/Styles.styled.js
+++ spa/src/components/content/styles/Styles.styled.js
@@ -3,4 +3,11 @@ import { motion } from 'framer-motion';
 
 export const Styles = styled(motion.div)``;
 
-export const StylesList = styled(motion.ul)``;
+export const StylesList = styled(motion.ul)`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 1rem;
+  margin: 0;
+`;

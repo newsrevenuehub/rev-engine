828b13b7cc7ebf25783d601406718aaf654cb973
Michael Ashton <michael.ashton@caktusgroup.com>
2021-08-20 08:47:26 -0400

    [NRH-276] make dashboard section optionally collapsible

--- spa/src/components/dashboard/DashboardSection.js
+++ spa/src/components/dashboard/DashboardSection.js
@@ -1,11 +1,33 @@
+import { useState } from 'react';
 import * as S from './DashboardSection.styled';
+import { accordionAnimation } from 'components/content/pages/Pages.styled';
 import PropTypes from 'prop-types';
 
+// Deps
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { AnimatePresence } from 'framer-motion';
+
 function DashboardSection({ children, heading, collapsible, ...props }) {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleClickHeading = () => {
+    if (!collapsible) return;
+    setCollapsed(!collapsed);
+  };
+
   return (
     <S.DashboardSection {...props} layout>
-      {heading && <S.SectionHeading layout>{heading}</S.SectionHeading>}
-      <S.SectionContent layout>{children}</S.SectionContent>
+      <S.SectionHeading layout collapsible={collapsible} onClick={handleClickHeading}>
+        {heading && <h2>{heading}</h2>}
+        {collapsible && <S.Chevron icon={faChevronDown} collapsed={collapsed} />}
+      </S.SectionHeading>
+      <AnimatePresence>
+        {(!collapsible || !collapsed) && (
+          <S.SectionContent layout {...accordionAnimation}>
+            {children}
+          </S.SectionContent>
+        )}
+      </AnimatePresence>
     </S.DashboardSection>
   );
 }

2bea6321b0eb2dc140d45e4bc40573f9ec1feb51
Benjamin White <bwhite@caktusgroup.com>
2021-08-25 10:55:53 -0400

    comment about temp approach for env var

--- spa/src/constants/analyticsConstants.js
+++ spa/src/constants/analyticsConstants.js
@@ -2,8 +2,8 @@ export const HUB_ANALYTICS_APP_NAME = 'rev-engine-analytics';
 export const HUB_GA_V3_PLUGIN_NAME = 'ga-v3-hub';
 // when NRH-76 has been resolved, should use line below instead of two below
 // export const { REACT_APP_HUB_V3_GOOGLE_ANALYTICS_ID: HUB_GA_V3_ID } = process.env;
-const TEMP_HARD_CODED_HUB_GA_V3_ID = 'UA-89391894-3';
 const NONSENSICAL_HUB_GA_V3_ID = 'UA-37373737yesyesyes';
-export const HUB_GA_V3_ID =
-  process.env?.NODE_ENV === 'production' ? TEMP_HARD_CODED_HUB_GA_V3_ID : NONSENSICAL_HUB_GA_V3_ID;
+// until NRH-76 is resolved, if deploying to prod, will need to push a commit to main
+// branch that changes this to the real NRH GA id.
+export const HUB_GA_V3_ID = NONSENSICAL_HUB_GA_V3_ID;
 export const ORG_GA_V3_PLUGIN_NAME = 'ga-v3-org';

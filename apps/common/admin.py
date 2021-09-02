eca740f3261c7d646e33fd5bde4a31155ae1cb2e
Michael Ashton <michael.ashton@caktusgroup.com>
2021-09-02 14:51:19 -0400

    [NRH-330] replace import style, remove cypress .onlys

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from django.db import models
 
 from django_json_widget.widgets import JSONEditorWidget
 
 
-class RevEngineBaseAdmin(admin.ModelAdmin):
+class RevEngineBaseAdmin(ModelAdmin):
     formfield_overrides = {
         models.JSONField: {"widget": JSONEditorWidget},
     }

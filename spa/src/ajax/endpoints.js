667088991159ea3e56a77ce935ed105add315007
Michael Ashton <michael.ashton@caktusgroup.com>
2021-10-01 12:01:46 -0400

    [NRH-376] enforce isauthenticated and permissions for page edit, extract full detail logic

--- spa/src/ajax/endpoints.js
+++ spa/src/ajax/endpoints.js
@@ -8,7 +8,8 @@ export const STRIPE_CONFIRMATION = 'stripe/confirmation/';
 export const ORG_STRIPE_ACCOUNT_ID = 'organizations/stripe_account_id/';
 
 // Pages
-export const FULL_PAGE = 'pages/full_detail/';
+export const LIVE_PAGE_DETAIL = 'pages/live-detail/';
+export const DRAFT_PAGE_DETAIL = 'pages/draft-detail/';
 export const LIST_PAGES = 'pages/';
 export const PATCH_PAGE = 'pages/';
 export const DELETE_PAGE = 'pages/';

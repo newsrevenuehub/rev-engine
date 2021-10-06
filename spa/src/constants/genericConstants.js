5608db56b9fc8afcdc98e9308da26bae800c60f5
Michael Ashton <michael.ashton@caktusgroup.com>
2021-10-06 14:42:17 -0400

    add org portal subdomain support

--- spa/src/constants/genericConstants.js
+++ spa/src/constants/genericConstants.js
@@ -2,3 +2,6 @@
 export const GRECAPTCHA_SCRIPT_URL = 'https://www.google.com/recaptcha/api.js';
 export const GRECAPTCHA_SITE_KEY = '6Lfuse8UAAAAAD9E6tCxKYrxO1IbnXp8IBa4u5Ri';
 export const HUB_GOOGLE_MAPS_API_KEY = 'AIzaSyDYgR8mULDoecl92BQ734H7YTLFzgjvtJ0';
+
+// These subdomain labels will redirect to the org portal.
+export const ORG_PORTAL_SUBDOMAINS = ['', 'support'];

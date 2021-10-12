993387cb841ddf4abbd5bf9b97fc3398e8c8897d
Jeremy Gibson <jgibson@caktusgroup.com>
2021-10-12 11:10:55 -0400

    refs #NRH-418 Adds a missed validation requirement

--- apps/organizations/validators.py
+++ apps/organizations/validators.py
@@ -4,6 +4,10 @@ from django.core.exceptions import ValidationError
 
 
 def validate_statement_descriptor_suffix(value):
-    test = re.search(r"[*\\\"<>']", value)
-    if test:
+    test_disallowed = re.search(r"[*\\\"<>']", value)
+    test_at_least_one_char = re.search(r"[a-zA-Z]", value)
+    if test_disallowed:
         raise ValidationError("Statement Descriptor Suffixes cannot contain [\",', <, >,\\, *]")
+
+    if not test_at_least_one_char:
+        raise ValidationError("There must be at least one letter in the descriptor.")

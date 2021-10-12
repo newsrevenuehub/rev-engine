a4a94f533261cb2b21e4f2249adc60d465bfca75
Michael Ashton <michael.ashton@caktusgroup.com>
2021-10-12 09:19:20 -0400

    remvoe conosle.log

--- spa/src/hooks/useErrorFocus.js
+++ spa/src/hooks/useErrorFocus.js
@@ -11,7 +11,6 @@ import isEmpty from 'lodash.isempty';
  */
 function useErrorFocus(formRef, errors) {
   useEffect(() => {
-    console.log('those errors tho', errors);
     if (!isEmpty(errors)) {
       const errorNames = Object.keys(errors);
       const inputNames = [...formRef.current.elements].map((el) => el.name);

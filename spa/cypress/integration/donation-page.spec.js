29547e266ccf45814ccd314b414a87b0c951484d
Benjamin White <bwhite@caktusgroup.com>
2021-10-13 17:21:51 -0400

    add back .then()

--- spa/cypress/integration/donation-page.spec.js
+++ spa/cypress/integration/donation-page.spec.js
@@ -396,10 +396,11 @@ describe('Resulting request', () => {
     const amount = '120';
 
     cy.setUpDonation(interval, amount);
-    cy.makeDonation();
-    cy.wait('@stripePayment', { timeout: LONG_WAIT })
-      .its('request.body')
-      .should('have.property', 'sf_campaign_id', sfCampaignId);
+    cy.makeDonation().then(() => {
+      cy.wait('@stripePayment', { timeout: LONG_WAIT })
+        .its('request.body')
+        .should('have.property', 'sf_campaign_id', sfCampaignId);
+    });
   });
 
   it('should send a request with the expected payment properties and values', () => {
@@ -411,12 +412,13 @@ describe('Resulting request', () => {
     const interval = 'One time';
     const amount = '120';
     cy.setUpDonation(interval, amount);
-    cy.makeDonation();
-    cy.wait('@stripePayment', { timeout: LONG_WAIT }).then((interception) => {
-      const { body: paymentData } = interception.request;
-      expect(paymentData).to.have.property('interval', 'one_time');
-      expect(paymentData).to.have.property('amount', amount);
-      expect(paymentData).to.have.property('captcha_token');
+    cy.makeDonation().then(() => {
+      cy.wait('@stripePayment', { timeout: LONG_WAIT }).then((interception) => {
+        const { body: paymentData } = interception.request;
+        expect(paymentData).to.have.property('interval', 'one_time');
+        expect(paymentData).to.have.property('amount', amount);
+        expect(paymentData).to.have.property('captcha_token');
+      });
     });
   });
 
@@ -432,12 +434,13 @@ describe('Resulting request', () => {
     const interval = 'One time';
     const amount = '120';
     cy.setUpDonation(interval, amount);
-    cy.makeDonation();
-    cy.wait(['@confirmCardPayment', '@stripePayment'], { timeout: LONG_WAIT }).spread(
-      (confirmCardPayment, stripePayment) => {
-        cy.wrap(confirmCardPayment.request.body).should('include', livePageOne.stripe_account_id);
-      }
-    );
+    cy.makeDonation().then(() => {
+      cy.wait(['@confirmCardPayment', '@stripePayment'], { timeout: LONG_WAIT }).spread(
+        (confirmCardPayment, stripePayment) => {
+          cy.wrap(confirmCardPayment.request.body).should('include', livePageOne.stripe_account_id);
+        }
+      );
+    });
   });
 
   it('should contain clearbit.js script in body', () => {
@@ -465,10 +468,11 @@ describe('Resulting request: special case -- error on submission', () => {
       { body: { [errorElementName]: errorMessage }, statusCode: 400 }
     ).as('stripePaymentError');
     cy.setUpDonation('One time', '120');
-    cy.makeDonation();
-    cy.wait('@stripePaymentError');
-    cy.get(`input[name="${errorElementName}"]`).should('have.focus');
-    cy.contains(errorMessage);
+    cy.makeDonation().then(() => {
+      cy.wait('@stripePaymentError');
+      cy.get(`input[name="${errorElementName}"]`).should('have.focus');
+      cy.contains(errorMessage);
+    });
   });
 });
 

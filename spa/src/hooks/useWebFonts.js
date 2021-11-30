a64ceeb0507b6685a6e48a23d702ef8eaa666c1e
Michael Ashton <michael.ashton@caktusgroup.com>
2021-11-30 12:23:14 -0500

    webfont script and initial fonts as tempate

--- spa/src/hooks/useWebFonts.js
+++ spa/src/hooks/useWebFonts.js
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 import loadWebFont from 'utilities/loadWebFont';
 
-function useWebFonts(fonts = {}, { context, onSuccess = () => {}, onError = () => {} } = {}) {
+function useWebFonts(fonts = {}, { onSuccess = () => {}, onError = () => {} } = {}) {
   useEffect(() => {
     Object.keys(fonts).forEach((fontKey) => {
-      loadWebFont(fonts[fontKey], context).then(onSuccess, onError);
+      loadWebFont(fonts[fontKey]).then(onSuccess, onError);
     });
   }, [fonts, onSuccess, onError]);
 }

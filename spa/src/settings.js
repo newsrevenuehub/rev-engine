681d2214dc1e4c05187ca6def468a9c5f644b1d2
Michael Ashton <michael.ashton@caktusgroup.com>
2021-12-02 08:18:28 -0500

    wip use different frontend env vars based on env

--- spa/src/settings.js
+++ spa/src/settings.js
@@ -2,17 +2,20 @@
 export const ORG_PORTAL_SUBDOMAINS = ['', 'support'];
 
 //Rev Engine - General
-export const REVENGINE_API_VERSION = window.ENV?.REVENGINE_API_VERSION;
-export const FREQUENCY_QUERYPARAM = window.ENV?.FREQUENCY_QUERYPARAM;
-export const AMOUNT_QUERYPARAM = window.ENV?.AMOUNT_QUERYPARAM;
-export const SALESFORCE_CAMPAIGN_ID_QUERYPARAM = window.ENV?.SALESFORCE_CAMPAIGN_ID_QUERYPARAM;
-export const CAPTURE_PAGE_SCREENSHOT = window.ENV?.CAPTURE_PAGE_SCREENSHOT === 'true';
+export const REVENGINE_API_VERSION = resolveConstantFromEnv('REVENGINE_API_VERSION', 'v1');
+export const FREQUENCY_QUERYPARAM = resolveConstantFromEnv('FREQUENCY_QUERYPARAM', 'frequency');
+export const AMOUNT_QUERYPARAM = resolveConstantFromEnv('AMOUNT_QUERYPARAM', 'amount');
+export const SALESFORCE_CAMPAIGN_ID_QUERYPARAM = resolveConstantFromEnv(
+  'SALESFORCE_CAMPAIGN_ID_QUERYPARAM',
+  'campaign'
+);
+export const CAPTURE_PAGE_SCREENSHOT = resolveConstantFromEnv('CAPTURE_PAGE_SCREENSHOT') === 'true';
 
 // Analytics
 export const HUB_ANALYTICS_APP_NAME = 'rev-engine-analytics';
 export const HUB_GA_V3_PLUGIN_NAME = 'ga-v3-hub';
 // Actual GA ID is stored in the Heroku config vars for app. Mock ID used here for cypress tests
-export const HUB_GA_V3_ID = window.ENV?.HUB_V3_GOOGLE_ANALYTICS_ID || 'UA-37373737yesyesyes';
+export const HUB_GA_V3_ID = resolveConstantFromEnv('HUB_V3_GOOGLE_ANALYTICS_ID', 'UA-37373737yesyesyes');
 export const ORG_GA_V3_PLUGIN_NAME = 'ga-v3-org';
 
 // Auth
@@ -27,8 +30,16 @@ export const GRECAPTCHA_SCRIPT_URL = 'https://www.google.com/recaptcha/api.js';
 export const GRECAPTCHA_SITE_KEY = '6Lfuse8UAAAAAD9E6tCxKYrxO1IbnXp8IBa4u5Ri';
 
 // Google Maps
-export const HUB_GOOGLE_MAPS_API_KEY = window.ENV?.HUB_GOOGLE_MAPS_API_KEY;
+export const HUB_GOOGLE_MAPS_API_KEY = resolveConstantFromEnv('HUB_GOOGLE_MAPS_API_KEY');
 
 // Stripe
-export const HUB_STRIPE_API_PUB_KEY = window.ENV?.HUB_STRIPE_API_PUB_KEY;
-export const STRIPE_API_VERSION = window.ENV?.STRIPE_API_VERSION;
+export const HUB_STRIPE_API_PUB_KEY = resolveConstantFromEnv('HUB_STRIPE_API_PUB_KEY');
+export const STRIPE_API_VERSION = resolveConstantFromEnv('STRIPE_API_VERSION', '2020-08-27');
+
+function resolveConstantFromEnv(constantName, defaultValue) {
+  console.log('node_env', process.env.NODE_ENV);
+  console.log(constantName, process.env[`REACT_APP_${constantName}`]);
+  // console.log('process.env.REACT_APP_HUB_STRIPE_API_PUB_KEY', process.env.REACT_APP_HUB_STRIPE_API_PUB_KEY)
+  if (process.env.NODE_ENV === 'development') return process.env[`REACT_APP_${constantName}`] || defaultValue;
+  else return window.ENV[constantName] || defaultValue;
+}

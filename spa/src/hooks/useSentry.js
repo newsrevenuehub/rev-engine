ec879f7dc3b5fe1691a6e35edefc372334b54586
Michael Ashton <michael.ashton@caktusgroup.com>
2022-01-06 11:23:59 -0500

    change all sentry dsns to env vars

---
+++ spa/src/hooks/useSentry.js
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+
+// Sentry
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+// Const
+import { SENTRY_DSN } from 'settings';
+
+/**
+ * Typically, just loading sentry at build time is sufficient.
+ * However, due to our front-end environment variable pattern,
+ * we need to wait till runtime to get the Sentry DSN.
+ */
+function useSentry() {
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'development') {
+      Sentry.init({
+        dsn: SENTRY_DSN,
+        integrations: [new Integrations.BrowserTracing()],
+        tracesSampleRate: 1.0,
+        environment: document.location.hostname
+      });
+    }
+  });
+}
+
+export default useSentry;
--- spa/src/index.js
+++ spa/src/index.js
@@ -2,25 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// Sentry
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
-
 // Fontawesome config for CSP`
 import { config as fontawesomeConfig } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 // import 'node_modules/@fortawesome/fontawesome-svg-core/styles.css';
 // Load css via webpack, not via inline styles (fontawesome default)
 fontawesomeConfig.autoAddCss = false;
-
-if (process.env.NODE_ENV !== 'development') {
-  Sentry.init({
-    dsn: 'https://e7c3a0467b6e4474a99922d00cec182e@o168020.ingest.sentry.io/5774473',
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0,
-    environment: document.location.hostname
-  });
-}
 
 // webpack CSP nonce concession
 /* eslint-disable-next-line */

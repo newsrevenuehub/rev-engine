e1e672a4d3015d36b6735c56a6679ab44327e4cf
Michael Ashton <michael.ashton@caktusgroup.com>
2022-01-07 11:24:47 -0500

    create management command for applepaydomain.create, update livemode setting for bools, test util for safety

--- apps/contributions/utils.py
+++ apps/contributions/utils.py
@@ -5,7 +5,7 @@ def get_hub_stripe_api_key(livemode=False):
     """
     Caller can force livemode with argument, otherwise use setting.
     """
-    if livemode or settings.STRIPE_LIVE_MODE == "True":
+    if livemode or settings.STRIPE_LIVE_MODE:
         return settings.STRIPE_LIVE_SECRET_KEY
     return settings.STRIPE_TEST_SECRET_KEY
 

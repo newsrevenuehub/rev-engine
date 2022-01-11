42f49c995bd8e2cc27db5426f2dadf13f58bd47c
LorienOlive <lorien.olive@gmail.com>
2022-01-11 13:09:44 -0500

    Removes error message and component stack from fallback and updates styles

--- spa/src/components/errors/ChunkErrorBoundary.js
+++ spa/src/components/errors/ChunkErrorBoundary.js
@@ -4,48 +4,29 @@ import * as Sentry from '@sentry/react';
 import * as S from '../../elements/buttons/Button.styled';
 import * as ErrorS from './ErrorFallback.styled';
 
-const ChunkErrorFallback = (error, componentStack, resetError) => {
-  console.log(error, componentStack, resetError);
-  const chunkFailedMessage = /Loading chunk [\d]+ failed/;
-
-  if (error?.message && chunkFailedMessage.test(error.message)) {
-    // Leaving this here for testing purposes, should retry only for chunk loading errors
-    console.error(error);
-    resetError();
-  } else {
-    return (
-      <>
-        <ErrorS.ErrorWrapper>
-          <ErrorS.ErrorHeading layout>
-            <h2>😰 We've encountered a problem. Click below to reload the page</h2>
-          </ErrorS.ErrorHeading>
-          <ErrorS.ErrorMessage>
-            <div>{error.toString()}</div>
-          </ErrorS.ErrorMessage>
-          <ErrorS.ErrorStack>
-            <div>{componentStack}</div>
-          </ErrorS.ErrorStack>
-          <S.Button
-            onClick={() => {
-              resetError();
-            }}
-          >
-            Reload Page
-          </S.Button>
-        </ErrorS.ErrorWrapper>
-      </>
-    );
-  }
+const ChunkErrorFallback = (error) => {
+  return (
+    <>
+      <ErrorS.ErrorWrapper>
+        <ErrorS.ErrorHeading layout>
+          <h2>😰 We've encountered a problem!</h2>
+          <h4>Click below to reload the page</h4>
+        </ErrorS.ErrorHeading>
+        <S.Button
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          Refresh
+        </S.Button>
+      </ErrorS.ErrorWrapper>
+    </>
+  );
 };
 
 const ChunkErrorBoundary = (props) => {
-  console.log(props);
   return (
-    <Sentry.ErrorBoundary
-      fallback={({ error, componentStack, resetError }) => ChunkErrorFallback(error, componentStack, resetError)}
-    >
-      {props.children}
-    </Sentry.ErrorBoundary>
+    <Sentry.ErrorBoundary fallback={({ error }) => ChunkErrorFallback(error)}>{props.children}</Sentry.ErrorBoundary>
   );
 };
 
--- spa/src/components/errors/ErrorFallback.styled.js
+++ spa/src/components/errors/ErrorFallback.styled.js
@@ -3,26 +3,29 @@ import { motion } from 'framer-motion';
 
 export const ErrorHeading = styled(motion.div)`
   display: flex;
-  justify-content: center;
-  height: 60px;
-  position: relative;
-
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 40%;
   h2 {
     margin: 0;
     font-size: ${(props) => props.theme.fontSizes[2]};
     color: ${(props) => props.theme.colors.black};
   }
+  h4 {
+    margin: 0;
+    font-size: ${(props) => props.theme.fontSizes[1]};
+    color: ${(props) => props.theme.colors.black};
+  }
 `;
 
 export const ErrorWrapper = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 0px auto;
   padding: 2rem;
-`;
-
-export const ErrorMessage = styled(motion.div)`
-  font-weight: 600;
-  padding-bottom: 2rem;
-`;
-
-export const ErrorStack = styled(motion.div)`
-  padding-bottom: 4rem;
+  max-width: 700px;
+  height: 50%;
 `;

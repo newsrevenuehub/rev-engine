d3a4c653cf3346ca195cf5e0acf8c48d15ee5302
Michael Ashton <michael.ashton@caktusgroup.com>
2022-01-14 07:44:01 -0500

    refs #sc-10749 fix bad object reference in theme objects, fix reauth modal style

--- spa/src/components/authentication/ReauthModal.styled.js
+++ spa/src/components/authentication/ReauthModal.styled.js
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 import RevEngineModal from 'elements/modal/Modal';
 
-export const Modal = styled(RevEngineModal)`
-  top: 30%;
-`;
+export const Modal = styled(RevEngineModal)``;
 
 export const ReauthModal = styled.div`
   height: 300px;

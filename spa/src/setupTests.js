7987c96c31320089308e7dc557f67694a5b33a2f
Michael Ashton <michael.ashton@caktusgroup.com>
2022-01-20 15:06:09 -0500

    add react-testing-library as option for testing, try ci

--- spa/src/setupTests.js
+++ spa/src/setupTests.js
@@ -1,9 +1,16 @@
 import '@testing-library/jest-dom';
-import { server } from '__test_utils__/mocks';
-// Establish API mocking before all tests.
-beforeAll(() => server.listen());
-// Reset any request handlers that we may add during the tests,
-// so they don't affect other tests.
-afterEach(() => server.resetHandlers());
-// Clean up after the tests are finished.
-afterAll(() => server.close());
+import { server } from 'test-server';
+
+beforeAll(() => {
+  // establish API mocking
+  server.listen();
+});
+
+afterEach(() => {
+  // reset any request handlers that are declared as a part of our tests
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});

a8fab603b9c8d57e783b019339b676bdcb503aeb
Michael Ashton <michael.ashton@caktusgroup.com>
2022-01-27 14:13:28 -0500

    refs #sc-9579 add comment to default page elements, use uuid4 for uuid, fix misleading test name

--- apps/pages/defaults.py
+++ apps/pages/defaults.py
@@ -1,14 +1,20 @@
+from uuid import uuid4
+
+
 def get_default_page_elements():
+    """
+    When a new DonationPage is created, it should contain these elements by default.
+    """
     return [
         {
             "type": "DRichText",
-            "uuid": "f5543c8b-91db-4724-8224-55417790700c",
+            "uuid": uuid4(),
             "content": '<h2 style="text-align:center;">Support our journalism today.</h2>\n<p style="text-align:center;">This work doesn\'t happen without your support. Contribute today!</p>\n',
             "requiredFields": [],
         },
         {
             "type": "DFrequency",
-            "uuid": "b642c18c-2e05-4ba6-bd9a-2c5b60e4a20b",
+            "uuid": uuid4(),
             "content": [
                 {"value": "one_time", "isDefault": False, "displayName": "One time"},
                 {"value": "month", "isDefault": True, "displayName": "Monthly"},
@@ -18,7 +24,7 @@ def get_default_page_elements():
         },
         {
             "type": "DAmount",
-            "uuid": "8b24074e-a458-4b78-bd33-7e966654d745",
+            "uuid": uuid4(),
             "content": {
                 "options": {"year": [120, 180, 365], "month": [10, 15, 25], "one_time": [120, 180, 365]},
                 "defaults": {"year": 180, "month": 15, "one_time": 180},
@@ -28,20 +34,20 @@ def get_default_page_elements():
         },
         {
             "type": "DDonorInfo",
-            "uuid": "7265c78b-7086-4e94-afc7-34ac4cc43db0",
+            "uuid": uuid4(),
             "content": {"askPhone": False},
             "requiredFields": [],
         },
-        {"type": "DDonorAddress", "uuid": "12927883-3790-46c0-80e7-e77965df6a6a", "requiredFields": []},
+        {"type": "DDonorAddress", "uuid": uuid4(), "requiredFields": []},
         {
             "type": "DPayment",
-            "uuid": "4f62dd4d-1aa4-4939-8f1a-f4709dbb303f",
+            "uuid": uuid4(),
             "content": {"stripe": ["card", "apple", "google", "browser"], "offerPayFees": True, "payFeesDefault": True},
             "requiredFields": [],
         },
         {
             "type": "DRichText",
-            "uuid": "e9e2865c-24f2-4a95-b8d6-83622113c32c",
+            "uuid": uuid4(),
             "content": '<p style="text-align:center;">Have questions or want to change a recurring donation? Contact us at YOUR EMAIL ADDRESS HERE. <br><br>Prefer to mail a check? Our mailing address is YOUR MAILING ADDRESS HERE.<br><br>Contributions or gifts to YOUR ORGANIZATION NAME HERE are tax deductible. Our tax ID is YOUR TAX ID HERE.&nbsp;</p>\n',
             "requiredFields": [],
         },

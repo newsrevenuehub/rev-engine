45e50ff593d3ed6cf478bb97d9970628da93755c
Michael Ashton <michael.ashton@caktusgroup.com>
2022-01-28 09:34:01 -0500

    refs #sc-11447 add denylistword model and admin

--- apps/config/admin.py
+++ apps/config/admin.py
@@ -1,1 +1,12 @@
-# Keep me
+from django.contrib import admin
+
+from apps.config.models import DenyListWord
+
+
+@admin.register(DenyListWord)
+class DenyListWordAdmin(admin.ModelAdmin):
+    list_display = ("word",)
+
+    ordering = ("word",)
+
+    search_fields = ("word",)

fb6c6f70262082e69a0d355e8ef8614863b3408b
Michael Ashton <michael.ashton@caktusgroup.com>
2022-01-28 09:34:01 -0500

    refs #sc-11447 add denylistword model and admin

--- apps/config/models.py
+++ apps/config/models.py
@@ -1,1 +1,13 @@
-# Keep me
+from django.contrib.postgres.fields import CICharField
+from django.db import models
+
+
+class DenyListWord(models.Model):
+    word = CICharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.word
+
+    class Meta:
+        verbose_name = "Deny-list word"
+        verbose_name_plural = "Deny-list words"

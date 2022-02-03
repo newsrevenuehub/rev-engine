2717774f0ce941288980b36bc8969c5636efd8d7
Dmitriy Chukhin <dchukhin@gmail.com>
2022-02-03 08:38:28 -0500

    add option to choose font-size to rich text editor

--- spa/src/components/pageEditor/elementEditors/richText/config.js
+++ spa/src/components/pageEditor/elementEditors/richText/config.js
@@ -1,7 +1,17 @@
 const richtextConfig = {
-  options: ['inline', 'blockType', 'list', 'textAlign', 'link'],
+  options: ['inline', 'blockType', 'fontSize', 'list', 'textAlign', 'link'],
   inline: {
     options: ['bold', 'italic', 'underline', 'strikethrough']
+  },
+  fontSize: {
+    // font-size options are:
+    //   - 13px - the size of the disclaimer text
+    //   - 14px - the default font-size (based on semantic.min.css)
+    //   - 15px - the size of <h4> elements on the page
+    //   - 16px - the size of some elements on the page
+    //   - 18px - the size of <h3> elements on the page
+    //   - 24px - the size of <h2> elements on the page
+    options: [13, 14, 15, 16, 24]
   },
   blockType: {
     inDropdown: false,

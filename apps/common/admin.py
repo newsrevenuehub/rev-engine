57bb2084c2bcba697e6bd915c4925fd7a3ae0ce3
Dmitriy Chukhin <dchukhin@gmail.com>
2022-02-07 08:35:06 -0500

    define a common admin class for showing model history in Django admin

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -2,9 +2,14 @@ from django.contrib.admin import ModelAdmin
 from django.db import models
 
 from django_json_widget.widgets import JSONEditorWidget
+from simple_history.admin import SimpleHistoryAdmin
 
 
 class RevEngineBaseAdmin(ModelAdmin):
     formfield_overrides = {
         models.JSONField: {"widget": JSONEditorWidget},
     }
+
+
+class RevEngineSimpleHistoryAdmin(RevEngineBaseAdmin, SimpleHistoryAdmin):
+    pass

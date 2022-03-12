c1516d75a27fd826833084c46237927b34110924
Daniel Craigmile <daniel@fundjournalism.org>
2022-03-12 15:10:57 -0600

    fix URL

--- scripts/heroku-postdeploy.sh
+++ scripts/heroku-postdeploy.sh
@@ -4,4 +4,8 @@ set -o nounset
 set -o allexport
 set -o xtrace
 
+echo $DATABASE_URL
+echo $REVIEW_APP_SOURCE_DATABASE_URL
+pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} > /tmp/pgdump.out 
+
 pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} | pg_restore --clean --no-owner --no-acl --format=custom --if-exists -d ${DATABASE_URL} || true

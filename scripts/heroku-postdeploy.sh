2e7e3d2ae790f69a4c8b14bf2d4b94db06ca409c
Daniel Craigmile <daniel@fundjournalism.org>
2022-03-12 15:42:15 -0600

    debug post deploy script

--- scripts/heroku-postdeploy.sh
+++ scripts/heroku-postdeploy.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 set -o nounset
-set -o allexport
 set -o xtrace
 
 echo $DATABASE_URL
 echo $REVIEW_APP_SOURCE_DATABASE_URL
-pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} > /tmp/pgdump.out 
 
-pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} | pg_restore --clean --no-owner --no-acl --format=custom --if-exists -d ${DATABASE_URL} || true
+pg_dump --version
+#pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} > /tmp/pgdump.out 
+#
+#pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} | pg_restore --clean --no-owner --no-acl --format=custom --if-exists -d ${DATABASE_URL} || true

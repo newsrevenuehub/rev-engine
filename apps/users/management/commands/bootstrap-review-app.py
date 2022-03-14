6a646b6fd4614d584c25b5d5d528432438812db9
Daniel Craigmile <daniel@fundjournalism.org>
2022-03-14 15:26:41 -0500

    don't create qatester user

--- apps/users/management/commands/bootstrap-review-app.py
+++ apps/users/management/commands/bootstrap-review-app.py
@@ -1,5 +1,3 @@
-from django.conf import settings  # pragma: no cover
-from django.contrib.auth import get_user_model  # pragma: no cover
 from django.core.management.base import BaseCommand  # pragma: no cover
 
 
@@ -7,10 +5,4 @@ class Command(BaseCommand):  # pragma: no cover
     help = "Bootstrap Heroku review app"
 
     def handle(self, *args, **options):
-        User = get_user_model()
-        qatester_exists = User.objects.filter(email="qatester@example.com").exists()
-        if settings.ENVIRONMENT != "review" or qatester_exists:
-            self.stdout.write("**Not running bootstrap tasks**")
-            return
-        User.objects.create_superuser(email="qatester@example.com", password="qatester")
-        self.stdout.write(self.style.SUCCESS("Successfully created qatester"))
+        self.stdout.write(self.style.SUCCESS("Bootstrapped review app"))

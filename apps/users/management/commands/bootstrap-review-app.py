785b7dde315b4ba79c2e4f5470bbe44498bc093f
Benjamin White <bwhite@caktusgroup.com>
2022-03-18 10:27:08 -0400

    make log info more informative

--- apps/users/management/commands/bootstrap-review-app.py
+++ apps/users/management/commands/bootstrap-review-app.py
@@ -10,7 +10,11 @@ class Command(BaseCommand):  # pragma: no cover
         User = get_user_model()
         qatester_exists = User.objects.filter(email="qatester@example.com").exists()
         if settings.ENVIRONMENT != "review" or qatester_exists:
-            self.stdout.write("**Not running bootstrap tasks**")
+            self.stdout.write("Not running bootstrap tasks because:")
+            if settings.ENVIRONMENT != "review":
+                self.stdout.write(f"\t- `ENVIRONMENT` value is {settings.ENVIRONMENT}")
+            if qatester_exists:
+                self.stdout.write("\t- a qatester account exists in this space already")
             return
         User.objects.create_superuser(email="qatester@example.com", password="qatester")
         self.stdout.write(self.style.SUCCESS("Successfully created qatester"))

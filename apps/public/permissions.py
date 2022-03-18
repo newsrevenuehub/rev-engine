9bfeccfa9b3517356c93129947372e4438871021
Benjamin White <bwhite@caktusgroup.com>
2022-03-18 10:20:38 -0400

    simplify syntax

--- apps/public/permissions.py
+++ apps/public/permissions.py
@@ -3,4 +3,4 @@ from rest_framework import permissions
 
 class IsSuperUser(permissions.BasePermission):
     def has_permission(self, request, *args):
-        return hasattr(request.user, "is_active") and request.user.is_active and request.user.is_superuser
+        return getattr(request.user, "is_active", False) and request.user.is_superuser

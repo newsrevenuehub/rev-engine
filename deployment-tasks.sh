f7f7ed3bebe4d9a2387ce96c2b0dc51adcc33847
Daniel Craigmile <daniel@fundjournalism.org>
2022-03-24 00:53:07 -0500

    try to get cred file generated

--- deployment-tasks.sh
+++ deployment-tasks.sh
@@ -4,5 +4,5 @@ set -e
 >&2 echo "Running Migrations"
 python manage.py migrate --noinput
 
->&2 echo "***TEMPORARY*** Running bootstrap-review-app"
-python manage.py bootstrap-review-app
+#>&2 echo "***TEMPORARY*** Running bootstrap-review-app"
+#python manage.py bootstrap-review-app

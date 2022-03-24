4dcd3ec1814190e4ebfcaa782da4ceaabdd3ba63
Daniel Craigmile <daniel@fundjournalism.org>
2022-03-24 00:10:14 -0500

    make release/review script run migrations but not bootstrapping

--- review-tasks.sh
+++ review-tasks.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -e
 
->&2 echo "Running bootstrap-review-app"
-python manage.py bootstrap-review-app
+/bin/sh ./google_sa.sh
+
+#>&2 echo "Running bootstrap-review-app"
+#python manage.py bootstrap-review-app
+
+>&2 echo "Running Migrations"
+python manage.py migrate --noinput

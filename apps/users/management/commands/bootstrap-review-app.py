3f10118b8ce9dc3e97bec58677701a93e498ce24
Daniel Craigmile <daniel@fundjournalism.org>
2022-03-24 17:25:17 -0500

    more idempotent

--- apps/users/management/commands/bootstrap-review-app.py
+++ apps/users/management/commands/bootstrap-review-app.py
@@ -21,30 +21,45 @@ class Command(BaseCommand):  # pragma: no cover
 
         heroku_conn = heroku3.from_key(heroku_api_key)
         heroku_app = heroku_conn.apps()[heroku_app_name]
+        heroku_config = heroku_app.config()
+        heroku_config["SITE_URL"] = f"{ticket_id}.{zone_name}"
         revenue_programs = RevenueProgram.objects.all()
         cloudflare_conn = CloudFlare.CloudFlare()
+        heroku_domains = [x.hostname for x in heroku_app.domains()]
+        zone_id = cloudflare_conn.zones.get(params={"name": zone_name})[0]["id"]
+        cloudflare_domains = {
+            x["name"]: x["content"] for x in cloudflare_conn.zones.dns_records.get(zone_id, params={"per_page": 300})
+        }
+        cloudflare_ids = {
+            x["name"]: x["id"] for x in cloudflare_conn.zones.dns_records.get(zone_id, params={"per_page": 300})
+        }
 
         for revenue_program in revenue_programs:
             if ticket_id in revenue_program.slug:
-                self.stdout.write(self.style.WARNING("slug already ticketed: %s" % revenue_program.slug))
-                continue
-            slug = f"{revenue_program.slug}-{ticket_id}".lower()
-            fqdn = f"{slug}.{zone_name}"
-            revenue_program.slug = slug
-            zone_id = cloudflare_conn.zones.get(params={"name": zone_name})[0]["id"]
+                self.stdout.write(self.style.WARNING("slug already modified: %s" % revenue_program.slug))
+            else:
+                revenue_program.slug = f"{revenue_program.slug}-{ticket_id}".lower()
+            fqdn = f"{revenue_program.slug}.{zone_name}"
+            content = (f"{heroku_app_name}.herokuapp.com",)
             dns_record = {
-                "name": f"{slug}",
+                "name": f"{revenue_program.slug}",
                 "type": "CNAME",
-                "content": f"{heroku_app_name}.herokuapp.com",
+                "content": content,
                 "proxied": True,
             }
             try:
-                cloudflare_conn.zones.dns_records.post(zone_id, data=dns_record)
+                if fqdn not in cloudflare_domains:
+                    self.stdout.write(self.style.SUCCESS(f"Creating DNS entry for {fqdn} → {content}"))
+                    cloudflare_conn.zones.dns_records.post(zone_id, data=dns_record)
+                if cloudflare_domains[fqdn] != content:
+                    self.stdout.write(self.style.SUCCESS(f"Updating DNS entry for {fqdn} → {content}"))
+                    record_id = cloudflare_ids[fqdn]
+                    cloudflare_conn.zones.dns_records.patch(zone_id, record_id, data=dns_record)
             except CloudFlare.exceptions.CloudFlareAPIError as error:
                 self.stdout.write(self.style.WARNING(error))
-                continue
-            heroku_app.add_domain(fqdn, None)
+            if fqdn not in heroku_domains:
+                self.stdout.write(self.style.SUCCESS(f"Creating Heroku domain entry entry for {fqdn}"))
+                heroku_app.add_domain(fqdn, None)
             revenue_program.save()
-            self.stdout.write(self.style.SUCCESS("Added DNS for entry for %s" % slug))
 
         self.stdout.write(self.style.SUCCESS("Configured DNS for %s" % heroku_app_name))

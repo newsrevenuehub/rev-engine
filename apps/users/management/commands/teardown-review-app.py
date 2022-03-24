e80036ec6d7e14cdf4aff276235b08e5669bfc7c
Daniel Craigmile <daniel@fundjournalism.org>
2022-03-24 11:21:31 -0500

    add teardown script

--- apps/users/management/commands/teardown-review-app.py
+++ apps/users/management/commands/teardown-review-app.py
@@ -3,7 +3,6 @@ import os
 from django.core.management.base import BaseCommand  # pragma: no cover
 
 import CloudFlare
-import heroku3
 
 from apps.common.utils import extract_ticket_id_from_branch_name
 from apps.organizations.models import RevenueProgram
@@ -16,31 +15,21 @@ class Command(BaseCommand):  # pragma: no cover
         branch_name = os.environ.get("HEROKU_BRANCH")
         zone_name = os.environ.get("CF_ZONE_NAME")
         heroku_app_name = os.environ.get("HEROKU_APP_NAME")
-        heroku_api_key = os.environ.get("HEROKU_API_KEY")
         ticket_id = extract_ticket_id_from_branch_name(branch_name)
 
-        heroku_conn = heroku3.from_key(heroku_api_key)
-        heroku_app = heroku_conn.apps()[heroku_app_name]
+        # TODO: remove this limit after testing:
         revenue_programs = RevenueProgram.objects.all()
         cloudflare_conn = CloudFlare.CloudFlare()
-
         for revenue_program in revenue_programs:
             if ticket_id in revenue_program.slug:
                 self.stdout.write(self.style.WARNING("slug already ticketed: %s" % revenue_program.slug))
                 continue
             slug = f"{revenue_program.slug}-{ticket_id}".lower()
-            fqdn = f"{slug}.{zone_name}"
-            revenue_program.slug = slug
             zone_id = cloudflare_conn.zones.get(params={"name": zone_name})[0]["id"]
-            dns_record = {
-                "name": f"{slug}",
-                "type": "CNAME",
-                "content": f"{heroku_app_name}.herokuapp.com",
-                "proxied": True,
-            }
-            cloudflare_conn.zones.dns_records.post(zone_id, data=dns_record)
-            heroku_app.add_domain(fqdn, None)
-            revenue_program.save()
-            self.stdout.write(self.style.SUCCESS("Added DNS for entry for %s" % slug))
+            fqdn = f"{slug}.{zone_name}"
+            dns_records = cloudflare_conn.zones.dns_records.get(zone_id, params={"name": fqdn})
+            for dns_record in dns_records:
+                dns_record_id = dns_record["id"]
+                cloudflare_conn.zones.dns_records.delete(zone_id, dns_record_id)
 
-        self.stdout.write(self.style.SUCCESS("Configured DNS for %s" % heroku_app_name))
+        self.stdout.write(self.style.SUCCESS("Tore down DNS for %s" % heroku_app_name))

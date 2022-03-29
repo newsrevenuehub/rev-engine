5fd0ca13fe5bbf74909b999a22d6f3a878cbbad3
Benjamin White <bwhite@caktusgroup.com>
2022-03-29 10:04:30 -0400

    remove file

--- apps/api/filters.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import filters
-
-from apps.api.permissions import filter_from_permissions
-
-
-class RoleAssignmentFilterBackend(filters.BaseFilterBackend):
-    def filter_queryset(self, request, queryset, view):
-        return filter_from_permissions(request, queryset, view.model)

8707f5ead53567183d3034b0f1841718f4cd032b
gs <dev_toptal@pieguytechs-MacBook-Air.local>
2022-04-02 00:16:20 -0700

    reverting back

--- spa/src/components/donationPage/DonationPageNavbar.js
+++ spa/src/components/donationPage/DonationPageNavbar.js
@@ -7,11 +7,23 @@ function DonationPageNavbar({ page }) {
     } else return img;
   };
 
+  let headerLogoMarkup = null;
+
+  if (page?.header_logo) {
+    headerLogoMarkup = <S.DonationPageNavbarLogo src={getImageUrl(page?.header_logo)} />;
+
+    if (page?.header_link) {
+      headerLogoMarkup = (
+        <a href={page?.header_link} target="_blank" rel="noreferrer noopener">
+          <S.DonationPageNavbarLogo src={getImageUrl(page?.header_logo)} />
+        </a>
+      );
+    }
+  }
+
   return (
     <S.DonationPageNavbar bgImg={getImageUrl(page?.header_bg_image)} data-testid="s-header-bar">
-      <a href={page?.header_link} target="_blank" rel="noreferrer noopener">
-        <S.DonationPageNavbarLogo src={getImageUrl(page?.header_logo)} />
-      </a>
+      {headerLogoMarkup}
     </S.DonationPageNavbar>
   );
 }

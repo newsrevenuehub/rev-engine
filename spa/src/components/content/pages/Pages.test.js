a1f68ca170e36010f304265d7ff8c3adad15792c
gurjittoptal <dev_toptal@pieguytechs-MacBook-Air.local>
2022-04-04 23:06:21 -0700

    remove cypress test and include a unit test

--- spa/src/components/content/pages/Pages.test.js
+++ spa/src/components/content/pages/Pages.test.js
@@ -13,7 +13,7 @@ describe('Given pages list', () => {
     result = await pagesbyRP(inp);
   });
 
-  it('should group pages by RevenueProgramin in pagesByRevProgram ', () => {
+  it('should group pages by RevenueProgram in pagesByRevProgram ', () => {
     expect(result.length).toEqual(2);
   });
 });

522d917869b427a8206eaa60c5f0f3b983f9e832
gurjittoptal <dev_toptal@pieguytechs-MacBook-Air.local>
2022-05-03 11:11:30 +0530

    dev-1111_contributor_portal_rp_branded

---
+++ spa/src/components/ContributorRouter.js
@@ -0,0 +1,92 @@
+import React, { lazy } from 'react';
+
+import { useEffect, useCallback, useState } from 'react';
+
+// AJAX
+import useRequest from 'hooks/useRequest';
+import { LIVE_PAGE_DETAIL } from 'ajax/endpoints';
+
+// Constants
+import { DASHBOARD_SUBDOMAINS } from 'settings';
+
+// Routing
+import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
+import ProtectedRoute from 'components/authentication/ProtectedRoute';
+
+// Hooks
+import useSubdomain from 'hooks/useSubdomain';
+
+// Slugs
+import * as ROUTES from 'routes';
+
+// Components/Children
+import GlobalLoading from 'elements/GlobalLoading';
+import TrackPageView from 'components/analytics/TrackPageView';
+import ChunkErrorBoundary from 'components/errors/ChunkErrorBoundary';
+import SegregatedStyles from 'components/donationPage/SegregatedStyles';
+import DonationPageNavbar from 'components/donationPage/DonationPageNavbar';
+
+// Utilities
+import componentLoader from 'utilities/componentLoader';
+
+// Split bundles
+const ContributorEntry = lazy(() => componentLoader(() => import('components/contributor/ContributorEntry')));
+const ContributorVerify = lazy(() => componentLoader(() => import('components/contributor/ContributorVerify')));
+const ContributorDashboard = lazy(() =>
+  componentLoader(() => import('components/contributor/contributorDashboard/ContributorDashboard'))
+);
+
+function ContributorRouter() {
+  const [pageData, setPageData] = useState(null);
+
+  const subdomain = useSubdomain();
+  const requestFullPage = useRequest();
+
+  const fetchRPLiveContent = useCallback(async () => {
+    const requestParams = {
+      revenue_program: subdomain
+    };
+    requestFullPage(
+      {
+        method: 'GET',
+        url: LIVE_PAGE_DETAIL,
+        params: requestParams
+      },
+      {
+        onSuccess: ({ data }) => {
+          setPageData(data);
+        },
+        onFailure: (e) => {}
+      }
+    );
+  }, [subdomain]);
+
+  useEffect(() => {
+    if (!DASHBOARD_SUBDOMAINS.includes(subdomain)) fetchRPLiveContent();
+  }, [fetchRPLiveContent]);
+
+  if (!DASHBOARD_SUBDOMAINS.includes(subdomain) && !pageData) return null;
+
+  return (
+    <SegregatedStyles page={pageData}>
+      {pageData ? <DonationPageNavbar page={pageData} /> : null}
+      <BrowserRouter>
+        <ChunkErrorBoundary>
+          <React.Suspense fallback={<GlobalLoading />}>
+            <Switch>
+              <ProtectedRoute
+                path={ROUTES.CONTRIBUTOR_DASHBOARD}
+                render={() => <TrackPageView component={ContributorDashboard} />}
+                contributor
+              />
+              <Route path={ROUTES.CONTRIBUTOR_ENTRY} render={() => <TrackPageView component={ContributorEntry} />} />
+              <Route path={ROUTES.CONTRIBUTOR_VERIFY} render={() => <TrackPageView component={ContributorVerify} />} />
+            </Switch>
+          </React.Suspense>
+        </ChunkErrorBoundary>
+      </BrowserRouter>
+    </SegregatedStyles>
+  );
+}
+
+export default ContributorRouter;

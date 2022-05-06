c5c089b6f370a201b3f1273dd839fac4338ff12b
Benjamin White <bwhite@caktusgroup.com>
2022-05-06 14:19:23 -0400

    refactor tasks into distinct send_email and send_contribution_confirmation_email

--- apps/emails/tasks.py
+++ apps/emails/tasks.py
@@ -6,21 +6,45 @@ from celery import shared_task
 from celery.utils.log import get_task_logger
 
 
+DEFAULT_CONTRIBUTION_CONFIRMATION_EMAIL_SUBJECT = "Thank you for your contribution!"
+
+
 logger = get_task_logger(f"{settings.DEFAULT_LOGGER}.{__name__}")
 
 
+class EmailTaskError(Exception):
+    pass
+
+
 @shared_task(
-    name="send_donor_email",
+    name="send_email",
     max_retries=5,
     retry_backoff=True,
     retry_jitter=False,
     autoretry_for=(AnymailAPIError,),
 )
-def send_donor_email(identifier, to, subject, template_data):  # pragma: no cover
-    logger.info("Sending a donation email")
+def send_email(identifier, to, subject, template_data):
     message = AnymailMessage()
     message.template_id = identifier
     message.to = [to]
     message.subject = subject
     message.merge_global_data = template_data
     message.send()
+
+
+@shared_task(
+    name="send_contribution_confirmation_email",
+    max_retries=5,
+    retry_backoff=True,
+    retry_jitter=False,
+    autoretry_for=(AnymailAPIError,),
+)
+def send_contribution_confirmation_email(
+    to,
+    subject=DEFAULT_CONTRIBUTION_CONFIRMATION_EMAIL_SUBJECT,
+    esp_template_id=settings.ESP_TEMPLATE_ID_FOR_CONTRIBUTION_CONFIRMATION,
+    **template_data,
+):
+    if not esp_template_id:
+        raise EmailTaskError("`esp_template_id` must be set")
+    return send_email(esp_template_id, to, subject, template_data)

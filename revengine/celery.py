fef08a191a9937cfe7fd8621814047d1bc0c0a05
Benjamin White <bwhite@caktusgroup.com>
2022-05-06 15:10:24 -0400

    assert about confirmation emails in tests

--- revengine/celery.py
+++ revengine/celery.py
@@ -11,7 +11,7 @@ app = Celery("revengine")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
-app.config_from_object("django.conf:settings")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()

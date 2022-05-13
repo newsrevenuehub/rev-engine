5cc296877cee72b00e7ff39eda2f7648fbd0231d
Benjamin White <bwhite@caktusgroup.com>
2022-05-13 17:12:59 -0400

    new fk validator

--- apps/pages/validators.py
+++ apps/pages/validators.py
@@ -14,9 +14,6 @@ logger = logging.getLogger(f"{settings.DEFAULT_LOGGER}.{__name__}")
 required_style_keys = {"radii": list, "font": dict, "fontSizes": list}
 
 
-UNOWNED_TEMPLATE_FROM_PAGE_PAGE_PK_MESSAGE = "You don't own the page you're trying to make a template from"
-
-
 def style_validator(value):
     errors = []
     for key, valType in required_style_keys.items():
@@ -25,36 +22,6 @@ def style_validator(value):
     if errors:
         raise ValidationError(errors)
 
-
-class PagePkIsForOwnedPage:
-    """Used in serializer to ensure that the id supplied for `page_pk` is for a permitted resource"""
-
-    requires_context = True
-
-    def __init__(self, page_model):
-        self.page_model = page_model
-
-    def __call__(self, value, serializer):
-        """Get the instance referred to by `page_pk`...
-
-        ...if user is superuser or hub admin, valid
-        ...if user is an org admin, valid if referenced page belongs to org
-        ...if user is rp admin, valid if referenced page belongs to their rp
-        """
-        target_page = self.page_model.objects.get(pk=value["page_pk"])
-        if (request := serializer.context.get("request", None)) is not None:
-            user = request.user
-            ra = getattr(user, "roleassignment", None)
-            if user.is_superuser or (ra is not None and ra.role_type == Roles.HUB_ADMIN):
-                return
-            elif ra.role_type == Roles.ORG_ADMIN and target_page.revenue_program.organization != ra.organization:
-                raise serializers.ValidationError(UNOWNED_TEMPLATE_FROM_PAGE_PAGE_PK_MESSAGE)
-            elif ra.role_type == Roles.RP_ADMIN and target_page.revenue_program not in ra.revenue_programs.all():
-                raise serializers.ValidationError(UNOWNED_TEMPLATE_FROM_PAGE_PAGE_PK_MESSAGE)
-
-
-UNOWNED_REVENUE_PROGRAM_PK_MESSAGE = "You aren't permitted to reference this revenue program"
-MISSING_REFRENCE_TO_REV_PROGRAM_MESSAGE = "Can't determine relationship with revenue program"
 
 MISSING_USER_IN_CONTEXT_MESSAGE = "Can't determine relationship of requesting user to request {} instance"
 
@@ -69,14 +36,22 @@ def _has_ensured_user_ownership_by_default(user, role_assignment):
 
 
 class ValidateFkReferenceOwnership:
-    """ """
+    """Used to validate that a requesting user owns the requested resource
+
+    ...where ownership is by virtue of user and role assignment instances.
+    """
 
     requires_context = True
 
     def _validate_passed_fn(self, fn, expected_params):
+        """Used to validate that a passed function has expected parameters
+
+        ...since callers can choose to pass a `has_default_access_fn` to replace
+        the default one.
+        """
         if not set(expected_params).issubset(set(fn.__code__.co_varnames)):
-            logger.warn("unexpected params")
-            raise serializers.ValidationError("unexpected params")
+            logger.warn("`ValidateFkReferenceOwnership` initialized with  ")
+            raise serializers.ValidationError("Somethin")
 
     def __init__(self, fk_attribute, has_default_access_fn=_has_ensured_user_ownership_by_default):
         """
@@ -113,5 +88,7 @@ class ValidateFkReferenceOwnership:
             if instance is None:
                 return
             if not instance.user_has_ownership_via_role(ra):
-                logger.warn("accessing unowned")
-                raise serializers.ValidationError("unowned")
+                logger.warn(
+                    f"User with role assignment ${ra} attempted to access unowned resource: {instance.pk}: {instance}"
+                )
+                raise serializers.ValidationError({self.fk_attribute: "Not found"})

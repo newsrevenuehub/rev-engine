39336ede2951a6404f049035050c00eda371df84
Benjamin White <bwhite@caktusgroup.com>
2022-05-17 13:52:39 -0400

    add doc string

--- apps/flags/models.py
+++ apps/flags/models.py
@@ -2,6 +2,11 @@ from waffle.models import AbstractUserFlag
 
 
 class Flag(AbstractUserFlag):
-    """ """
+    """Custom flag model
+
+    Initially, we are not overriding anything from `AbstractUserFlag`,
+    but we know we'll soon be integrating our custom flag model
+    with roleassignments, so we're creating a custom flag model at the outset.
+    """
 
     pass

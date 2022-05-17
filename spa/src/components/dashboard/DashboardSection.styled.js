558a27723a6f0f764b9173c5bb26809c95ddada2
gs <gs@pieguytechs-MacBook-Air.local>
2022-05-17 22:25:10 +0530

    DEV-1777 dev-1111 regression on content-dashboard

--- spa/src/components/dashboard/DashboardSection.styled.js
+++ spa/src/components/dashboard/DashboardSection.styled.js
@@ -5,14 +5,14 @@ import { motion } from 'framer-motion';
 export const DashboardSection = styled(motion.section)`
   border-radius: ${(props) => props.theme.radii[0]};
   box-shadow: ${(props) => props.theme.shadows[0]};
-  background: ${(props) => props.theme.colors.cstm_formPanelBackground};
+  background: ${(props) => props.theme.colors.paneBackground};
 `;
 
 export const SectionHeading = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: ${(props) => props.theme.colors.cstm_CTAs};
+  background: ${(props) => props.theme.colors.primary};
   border-top-right-radius: ${(props) => props.theme.radii[0]};
   border-top-left-radius: ${(props) => props.theme.radii[0]};
   height: 50px;

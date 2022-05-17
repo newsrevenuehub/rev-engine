105d88d138267b72f68870738896b4a1955bbc85
Norman J. Harman Jr <norman.harman@fundjournalism.org>
2022-05-17 11:48:36 -0700

    DEV-1545 Environment Vars
    Reorganize settings into categories; Django, django apps / 3rd party,
    RevEngine /  1st party.
    Move os.getenv into settings (from app code)

--- apps/users/management/commands/teardown-review-app.py
+++ apps/users/management/commands/teardown-review-app.py
@@ -1,5 +1,4 @@
-import os
-
+from django.conf import settings
 from django.core.management.base import BaseCommand  # pragma: no cover
 from django.urls import reverse
 
@@ -19,15 +18,13 @@ class Command(BaseCommand):  # pragma: no cover
 
     def handle(self, *args, **options):
         if not options["ticket"]:
-            branch_name = os.getenv("HEROKU_BRANCH")
-            ticket_id = extract_ticket_id_from_branch_name(branch_name)
+            ticket_id = extract_ticket_id_from_branch_name(settings.HEROKU_BRANCH)
         else:
             ticket_id = options["ticket"].lower()
 
         delete_cloudflare_cnames(ticket_id)
 
-        zone_name = os.getenv("CF_ZONE_NAME")
-        site_url = f"https://{ticket_id}.{zone_name}"
+        site_url = f"https://{ticket_id}.{settings.CF_ZONE_NAME}"
         webhook_url = f"{site_url}{reverse('stripe-webhooks')}".lower()
         api_key = get_hub_stripe_api_key()
         delete_stripe_webhook(webhook_url=webhook_url, api_key=api_key)

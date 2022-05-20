999a1a50b04ffb24ebebc155bfdf16d8bc177943
Benjamin White <bwhite@caktusgroup.com>
2022-05-20 15:16:16 -0400

    add comment about purpose of constant

--- apps/common/constants.py
+++ apps/common/constants.py
@@ -65,5 +65,6 @@ STATE_CHOICES = [
     ("YT", "Yukon"),
 ]
 
-
+# HasFlaggedAccessToContributionsApiResource references a flag with this name in order
+# to determine access permission.
 CONTRIBUTIONS_API_ENDPOINT_ACCESS_FLAG_NAME = "contributions-api-endpoint-access"

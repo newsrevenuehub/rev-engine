289d528b008164f95ed7f096abe628ce823de0d4
gs <gs@pieguytechs-MacBook-Air.local>
2022-06-02 22:39:03 +0530

    css fix

--- spa/src/components/pageEditor/PageEditor.styled.js
+++ spa/src/components/pageEditor/PageEditor.styled.js
@@ -15,7 +15,7 @@ export const ButtonOverlayOuter = styled.div`
   @media (${(props) => props.theme.breakpoints.tabletLandscapeDown}) {
     padding-top: 0;
     margin-top: 81px;
-    height: 100%x;
+    height: 100%;
     width: 90px;
     box-shadow: ${(props) => props.theme.shadows[0]};
   }

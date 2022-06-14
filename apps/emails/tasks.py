eab292b7e9b3abc0e6ce8092664ff0c08b473f36
Benjamin White <960351+benjaminEwhite@users.noreply.github.com>
2022-06-14 18:34:21 -0400

    add debug log with additional function args

--- apps/emails/tasks.py
+++ apps/emails/tasks.py
@@ -21,6 +21,12 @@ def send_templated_email(
     to, subject, text_template, html_template, template_data, from_email=settings.EMAIL_DEFAULT_TRANSACTIONAL_SENDER
 ):
     logger.info("Sending email to recipient `%s` with subject `%s`", to, subject)
+    logger.debug(
+        "`send_templated_email`\ntemplate_data: %s\n\ntext_template: %s\n\nhtml_template: %s",
+        template_data,
+        text_template,
+        html_template,
+    )
     send_mail(
         subject,
         render_to_string(text_template, template_data),

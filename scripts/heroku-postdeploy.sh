b7452e1ba3a30c62b63a557c724295b2f0b509d1
Benjamin White <960351+benjaminEwhite@users.noreply.github.com>
2022-06-22 23:37:05 -0400

    try nuking db before restore

--- scripts/heroku-postdeploy.sh
+++ scripts/heroku-postdeploy.sh
@@ -3,5 +3,13 @@
 set -o nounset
 set -o xtrace
 
+
+
+
+psql -d ${DATABASE_URL} -c "DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;"
+
 pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} | pg_restore --clean --no-owner --no-acl --format=custom --if-exists -d ${DATABASE_URL} || true
 python manage.py bootstrap-review-app

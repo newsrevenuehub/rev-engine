4cdd54278266f3b2319ab7a82f67efbc060577a0
Benjamin White <960351+benjaminEwhite@users.noreply.github.com>
2022-06-23 22:15:32 -0400

    refactor tests, and no bare raise

--- revengine/tests/test_views.py
+++ revengine/tests/test_views.py
@@ -1,39 +1,29 @@
-import re
-
 from django.urls import reverse
 
 from bs4 import BeautifulSoup as bs4
 
 
-support_address_for_500_pages = "support@fundjournalism.org"
-
-expected_elements_by_kwargs = {
-    "h1": {"string": re.compile("something went wrong", re.IGNORECASE)},
-    "a": {"href": f"mailto:{support_address_for_500_pages}", "string": support_address_for_500_pages},
-    "footer": {},
-}
+def assert_500_page(soup):
+    support_address_for_500_pages = "support@fundjournalism.org"
+    assert "something went wrong" in soup.find("h1").text.lower()
+    assert support_address_for_500_pages in soup.find("a", href=f"mailto:{support_address_for_500_pages}").text
+    assert "news revenue hub" in soup.find("footer").text.lower()
 
 
 def test_custom_500(client):
     """Test that custom 500 page is as expected"""
-    client.raise_request_exception = False
+    client.raise_request_exception = True
     response = client.get(reverse("dummy-500"))
     assert response.status_code == 500
-    soup = bs4(response.content, "html.parser")
-    for elem, kwargs in expected_elements_by_kwargs.items():
-        assert soup.find(elem, **kwargs)
+    assert_500_page(bs4(response.content, "html.parser"))
 
 
 def test_cloudflare_500_page(client):
     """Test that the custom 500 page we expose to Cloudflare is as expected"""
     response = client.get(reverse("cloudflare-500"))
     assert response.status_code == 200
-    expectations = expected_elements_by_kwargs | {
-        "h2": {"class": "error-trace", "string": "::CLOUDFLARE_ERROR_500S_BOX::"}
-    }
     soup = bs4(response.content, "html.parser")
-    for elem, kwargs in expectations.items():
-        assert soup.find(elem, **kwargs)
+    assert_500_page(soup)
     cloudflare_errors_div = soup.find("div", {"id": "cloudflare-error-data", "class": "hidden"})
     assert cloudflare_errors_div
     assert cloudflare_errors_div.findChild("div", string="::RAY_ID::")

729a4ca51a99f8cc75bbfb223479abe7bb5eb3e3
Harsha Bharadwaj <harsha.bajju@gmail.com>
2022-07-13 22:17:28 +0530

    move contributor creation logic to view

--- apps/api/tests/test_serializers.py
+++ apps/api/tests/test_serializers.py
@@ -1,5 +1,3 @@
-from unittest import mock
-
 import faker
 import pytest
 
@@ -26,10 +24,10 @@ contributor = apps.contributions.models.Contributor(
         {"email": "bob@example.com", "subdomain": "ncn\n"},  # something strips whitespace
     ],
 )
-@mock.patch("apps.api.serializers.Contributor.objects.get_or_create", return_value=(contributor, False))
-def test_good_ContributorObtainTokenSerializer(mock_get, data):
+def test_good_ContributorObtainTokenSerializer(data):
     t = ContributorObtainTokenSerializer(data=data)
     assert t.is_valid(), t.errors
+    t.update_short_lived_token(contributor)
     result = t.validated_data
     assert result["access"] not in ("notblank", "", None)  # set to random auth value
     if expected := data.get("subdomain"):
@@ -45,7 +43,6 @@ def test_good_ContributorObtainTokenSerializer(mock_get, data):
         {"email": "bob@example.com", "subdomain": "https://ncn"},  # only a-z, 0-9, -
     ],
 )
-@mock.patch("apps.api.serializers.Contributor.objects.get_or_create", return_value=(contributor, False))
-def test_bad_ContributorObtainTokenSerializer(mock_get, data):
+def test_bad_ContributorObtainTokenSerializer(data):
     t = ContributorObtainTokenSerializer(data=data)
     assert not t.is_valid(), t.errors

406829a20f4ca2d7968a1029592ed9984d62058b
gs <gs@pieguytechs-MacBook-Air.local>
2022-07-17 12:15:19 -0700

    new login page

--- spa/src/components/account/SignIn/SignIn.js
+++ spa/src/components/account/SignIn/SignIn.js
@@ -16,13 +16,24 @@ import { handleLoginSuccess } from 'components/authentication/util';
 import { PASSWORD_RESET_URL } from 'settings';
 
 // Elements
-import Input from 'elements/inputs/Input';
 import FormErrors from 'elements/inputs/FormErrors';
 
 // Analytics
 import { useConfigureAnalytics } from '../../analytics';
 
-function Login({ onSuccess, message }) {
+import purpleFooterImage from 'assets/images/account/purple-bottombar.png';
+
+import Logobar from 'components/account/common/logobar/Logobar';
+import Leftbar from 'components/account/common/leftbar/Leftbar';
+
+import Input from 'elements/inputs/Input';
+import InputWrapped from 'components/account/common/elements/InputWrapped';
+
+import { SIGN_UP, FORGOT_PASSWORD } from 'routes';
+
+import validateEmail from 'utilities/validateEmail';
+
+function Login2({ onSuccess, message }) {
   const history = useHistory();
 
   const [email, setEmail] = useState('');
@@ -95,6 +106,103 @@ function Login({ onSuccess, message }) {
         </S.LoginForm>
       </S.LoginCard>
     </S.Login>
+  );
+}
+
+function Login({ onSuccess, message }) {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const [{ loading, errors }, dispatch] = useReducer(fetchReducer, initialState);
+
+  useConfigureAnalytics();
+
+  const handlePostLogin = () => {
+    if (onSuccess) onSuccess();
+    else history.push(CONTENT_SLUG);
+  };
+
+  const onSubmitClick = async (e) => {
+    setEmailError('');
+
+    /*
+    if (!validateEmail(email)) {
+      setEmailError('Entered email is invalid');
+    }*/
+
+    if (emailError === '' && password !== '') {
+      dispatch({ type: FETCH_START });
+      try {
+        const { data, status } = await axios.post(TOKEN, { email, password });
+        if (status === 200 && data.detail === 'success') {
+          handleLoginSuccess(data);
+          handlePostLogin();
+        }
+        dispatch({ type: FETCH_SUCCESS });
+      } catch (e) {
+        dispatch({ type: FETCH_FAILURE, payload: e?.response?.data });
+      }
+    }
+  };
+
+  const submitDisabled = email === '' || password === '' || loading;
+
+  return (
+    <S.SignIn>
+      <S.Outer>
+        <S.Left>
+          <Leftbar />
+        </S.Left>
+        <S.Right>
+          <S.FormElements>
+            <S.Heading>Welcome Back!</S.Heading>
+            <br />
+            <InputWrapped
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              errors={''}
+              label="Email"
+              disabled={loading}
+              type={Input.types.EMAIL}
+              testid="signup-email"
+              errorMessage={emailError}
+            />
+            <S.PasswordLabel>
+              Password
+              <a href={FORGOT_PASSWORD}>Forgot Password?</a>
+            </S.PasswordLabel>
+            <InputWrapped
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              errors={''}
+              disabled={loading}
+              type={Input.types.PASSWORD}
+              testid="signup-password"
+              instructions="Password must be 8 characters long and alphanumerical."
+            />
+            {errors?.detail} <br />
+            <br />
+            <S.Submit
+              type={'neutral'}
+              disabled={submitDisabled}
+              onClick={loading || submitDisabled ? () => {} : onSubmitClick}
+            >
+              Sign In
+            </S.Submit>
+            <S.SignInLink>
+              Not a member?&nbsp;<a href={SIGN_UP}>Create an account</a>
+            </S.SignInLink>
+          </S.FormElements>
+
+          <Logobar />
+        </S.Right>
+        <S.BottomBar>
+          <S.BottomBarImg src={purpleFooterImage} />
+        </S.BottomBar>
+      </S.Outer>
+    </S.SignIn>
   );
 }
 

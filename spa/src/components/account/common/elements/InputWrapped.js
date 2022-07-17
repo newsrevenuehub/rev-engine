2d48abf0f68e8f1adbf13b631e52b9b1cdcc836a
gs <gs@pieguytechs-MacBook-Air.local>
2022-07-17 09:09:18 -0700

    code refactor

--- spa/src/components/account/common/elements/InputWrapped.js
+++ spa/src/components/account/common/elements/InputWrapped.js
@@ -3,40 +3,31 @@ import { useState } from 'react';
 import * as S from './InputWrapped.styled';
 import PropTypes from 'prop-types';
 
-import Input from 'components/account/common/elements/Input';
+import InputAccount from 'components/account/common/elements/InputAccount';
+import Input from 'elements/inputs/Input';
 
 import visibilityOn from 'assets/images/account/visibility_on.png';
 import visibilityOff from 'assets/images/account/visibility_off.png';
 
-function InputWrapped({ value, onChange, type, label, instructions, errorMessage }) {
+function InputWrapped({ value, onChange, type, label, disabled, instructions, errorMessage }) {
   const [updated, setUpdated] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
 
-  const togglePasswordDisplay = (e) => {
-    if (showPassword) {
-      setShowPassword(false);
-    } else {
-      setShowPassword(true);
-    }
+  const togglePasswordDisplay = () => {
+    setShowPassword(showPassword ? false : true);
   };
 
-  let inpType = type;
-  if (type === Input.types.PASSWORD) {
-    if (showPassword) {
-      inpType = Input.types.TEXT;
-    }
-  }
+  let inpType = Input.types.PASSWORD && showPassword ? Input.types.TEXT : type;
 
   return (
     <>
       {label && label !== '' && <S.Label>{label}</S.Label>}
       <S.InputWrapped>
-        <Input value={value} onChange={onChange} type={inpType} />
+        <InputAccount value={value} onChange={onChange} type={inpType} />
         {type === Input.types.PASSWORD && (
           <S.Visibility onClick={togglePasswordDisplay} src={showPassword ? visibilityOn : visibilityOff} />
         )}
       </S.InputWrapped>
-
       {instructions && instructions !== '' && <S.Instructions>{instructions}</S.Instructions>}
       {errorMessage && errorMessage !== '' ? <S.ErrorMessage>{errorMessage}</S.ErrorMessage> : <S.ErrorSpacer />}
     </>

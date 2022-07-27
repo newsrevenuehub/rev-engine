1eda95f829572cb33e8d649195fca191415acff9
Guilherme Goes Mendonca <9381325+GuiMend@users.noreply.github.com>
2022-07-27 17:14:23 -0300

    DEV-841: Update test for unsaved changes modal

--- spa/src/components/pageEditor/UnsavedChangesModal.test.js
+++ spa/src/components/pageEditor/UnsavedChangesModal.test.js
@@ -2,14 +2,21 @@ import { render, screen } from 'test-utils';
 
 import UnsavedChangesModal from './UnsavedChangesModal';
 
-it('should show button "exit without saving"', () => {
-  render(<UnsavedChangesModal isOpen />);
-  const exitButton = screen.getByRole('button', { name: 'Yes, exit' });
-  expect(exitButton).toBeInTheDocument();
-});
+describe('Unsaved Changes Modal', () => {
+  test('should have enabled buttons for "close" and "exit without saving"', () => {
+    render(<UnsavedChangesModal isOpen />);
 
-it('should show modal close button', () => {
-  render(<UnsavedChangesModal isOpen />);
-  const closeButton = screen.getByRole('button', { name: 'Cancel' });
-  expect(closeButton).toBeInTheDocument();
+    const exitButton = screen.getByRole('button', { name: 'Yes, exit' });
+    expect(exitButton).toBeEnabled();
+
+    const closeButton = screen.getByRole('button', { name: 'Cancel' });
+    expect(closeButton).toBeEnabled();
+  });
+
+  test('modal is not rendered when isOpen = false', () => {
+    render(<UnsavedChangesModal isOpen={false} />);
+
+    const title = screen.queryByText('Unsaved Changes');
+    expect(title).not.toBeInTheDocument();
+  });
 });

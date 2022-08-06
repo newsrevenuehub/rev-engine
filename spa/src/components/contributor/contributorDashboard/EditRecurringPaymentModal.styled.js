73307423d9fcf149e0809b0de0af0ca80c8fbbbe
gs <gs@pieguytechs-MacBook-Air.local>
2022-08-05 20:41:09 -0400

    modal font fixes

--- spa/src/components/contributor/contributorDashboard/EditRecurringPaymentModal.styled.js
+++ spa/src/components/contributor/contributorDashboard/EditRecurringPaymentModal.styled.js
@@ -8,6 +8,12 @@ export const EditRecurringPaymentModal = styled.div`
   margin: 1rem;
   background: ${(props) => props.theme.colors.paneBackground};
   border-radius: ${(props) => props.theme.radii[0]};
+
+  h2,
+  button,
+  p {
+    font-family: ${(props) => props.theme.systemFont};
+  }
 `;
 
 export const CurrentList = styled.ul`
@@ -18,6 +24,7 @@ export const CurrentList = styled.ul`
 
 export const CurrentDatum = styled.li`
   margin: 1rem;
+  font-family: ${(props) => props.theme.systemFont};
   span {
     display: block;
   }

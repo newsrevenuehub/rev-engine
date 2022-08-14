f6c2e80c385ebc43b5e8d256f05e9334c1dcb180
gs <gs@pieguytechs-MacBook-Air.local>
2022-08-14 11:18:28 -0400

    unit tests for connect stripe elements

--- spa/src/components/dashboard/connectStripe/userHasSingleRPNotConnectedToStripe.test.js
+++ spa/src/components/dashboard/connectStripe/userHasSingleRPNotConnectedToStripe.test.js
@@ -1,6 +1,9 @@
 import userHasSingleRPNotConnectedToStripe from './userHasSingleRPNotConnectedToStripe';
 
-const userHasSingleRP = { role_type: ['org_admin', 'Org Admin'], revenue_programs: [{ id: 1, slug: 'first' }] };
+const userHasSingleRPWithRoleOrgAdmin = {
+  role_type: ['org_admin', 'Org Admin'],
+  revenue_programs: [{ id: 1, slug: 'first' }]
+};
 const userHasManyRPs = {
   role_type: ['org_admin', 'Org Admin'],
   revenue_programs: [
@@ -11,10 +14,14 @@ const userHasManyRPs = {
 const userHasNoRPs = { role_type: ['org_admin', 'Org Admin'], revenue_programs: [] };
 const userisInvalid = null;
 const userHasNoRPsDefined = {};
+const userHasSingleRPWithRoleRPAdmin = {
+  role_type: ['rp_admin', 'RP Admin'],
+  revenue_programs: [{ id: 1, slug: 'first' }]
+};
 
-describe('Page Title component', () => {
-  test('returns true if user has single RP', async () => {
-    expect(userHasSingleRPNotConnectedToStripe(userHasSingleRP)).toBe(true);
+describe('ConnectStripeElements Allowed', () => {
+  test('returns true if user has single RP and has role org_admin', async () => {
+    expect(userHasSingleRPNotConnectedToStripe(userHasSingleRPWithRoleOrgAdmin)).toBe(true);
   });
   test('returns false if user has many RPs', async () => {
     expect(userHasSingleRPNotConnectedToStripe(userHasManyRPs)).toBe(false);
@@ -27,5 +34,8 @@ describe('Page Title component', () => {
   });
   test('returns false if user does not RPs defined', async () => {
     expect(userHasSingleRPNotConnectedToStripe(userHasNoRPsDefined)).toBe(false);
+  });
+  test('returns false if user has single RP and has role rp_admin', async () => {
+    expect(userHasSingleRPNotConnectedToStripe(userHasSingleRPWithRoleRPAdmin)).toBe(false);
   });
 });

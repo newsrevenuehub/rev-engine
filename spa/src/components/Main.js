c1fae9dd3eba2bfcad96a0a3502c7bc29610d0f3
gurjittoptal <30353391+gurjittoptal@users.noreply.github.com>
2022-08-15 12:11:49 -0400

    Update spa/src/components/Main.js
    Co-authored-by: Guilherme Goes Mendonca <9381325+GuiMend@users.noreply.github.com>

--- spa/src/components/Main.js
+++ spa/src/components/Main.js
@@ -14,7 +14,7 @@ function Main() {
 
   const [loadingFlags, setLoadingFlags] = useState(true);
   const [featureFlags, setFeatureFlags] = useState();
-  const [userDetails, setuserDetails] = useState();
+  const [userDetails, setUserDetails] = useState();
 
   const requestUser = useRequest();
 

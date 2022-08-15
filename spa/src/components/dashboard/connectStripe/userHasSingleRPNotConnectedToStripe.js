10299443b76757770b6b8ae8e7a030730ee7431d
gurjittoptal <30353391+gurjittoptal@users.noreply.github.com>
2022-08-15 12:11:25 -0400

    Update spa/src/components/dashboard/connectStripe/userHasSingleRPNotConnectedToStripe.js
    Co-authored-by: Guilherme Goes Mendonca <9381325+GuiMend@users.noreply.github.com>

--- spa/src/components/dashboard/connectStripe/userHasSingleRPNotConnectedToStripe.js
+++ spa/src/components/dashboard/connectStripe/userHasSingleRPNotConnectedToStripe.js
@@ -1,6 +1,6 @@
 function userHasSingleRPNotConnectedToStripe(userDetails) {
   if (userDetails && userDetails.revenue_programs && userDetails.revenue_programs.length === 1) {
-    // Add a check for the RP Payment Provider when api returns
+    // TODO: Add a check for the RP Payment Provider when api returns
 
     // Only org_admin and not rp_admin is  allowed to connect stripe
     const roles = userDetails.role_type;

77f6dd4e20e96832ce80a420bf9d3b882f9144f6
Harsha Bharadwaj <harsha.bajju@gmail.com>
2022-08-17 01:24:49 +0530

    add status negation filter

--- apps/contributions/filters.py
+++ apps/contributions/filters.py
@@ -6,6 +6,7 @@ from apps.contributions.models import Contribution, ContributionStatus
 class ContributionFilter(django_filters.FilterSet):
     contributor_email = django_filters.CharFilter(field_name="contributor__email", lookup_expr="icontains")
     status = django_filters.MultipleChoiceFilter(choices=ContributionStatus.choices)
+    status__not = django_filters.CharFilter(field_name="status", exclude=True)
     amount = django_filters.RangeFilter()
     created = django_filters.DateTimeFromToRangeFilter()
 
@@ -23,6 +24,7 @@ class ContributionFilter(django_filters.FilterSet):
             ("flagged_date", "flagged_date"),
             ("contributor__email", "contributor_email"),
             ("status", "status"),
+            ("status__not", "status__not"),
             ("interval", "interval"),
             ("auto_accept_on", "auto_accept_on"),
         )

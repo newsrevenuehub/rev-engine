1102af2ac46e50a80a25cbee7eda70b24736cc10
Harsha Bharadwaj <harsha.bajju@gmail.com>
2022-08-19 01:08:10 +0530

    remove flagged, rejected contributions

--- spa/src/components/donations/filters/StatusFilter.js
+++ spa/src/components/donations/filters/StatusFilter.js
@@ -7,22 +7,25 @@ import { PAYMENT_STATUS } from 'constants';
 
 const STATUS_FILTERS = Object.values(PAYMENT_STATUS);
 
-function StatusFilter({ filter = [], handleFilterChange }) {
+function StatusFilter({ filter = [], handleFilterChange, excludeStatusFilters = [] }) {
   return (
     <FilterWrapper data-testid="status-filter">
       <S.StatusFilter>
         <FilterLabel>Status:</FilterLabel>
         <S.Statuses>
-          {STATUS_FILTERS.map((f) => (
-            <S.StatusBadge
-              key={f}
-              data-testid={`status-filter-${f}`}
-              selected={filter.includes(f)}
-              onClick={() => handleFilterChange('status', f)}
-            >
-              <StatusCellIcon status={f} showText size="sm" />
-            </S.StatusBadge>
-          ))}
+          {STATUS_FILTERS.map(
+            (f) =>
+              excludeStatusFilters.indexOf(f) == -1 && (
+                <S.StatusBadge
+                  key={f}
+                  data-testid={`status-filter-${f}`}
+                  selected={filter.includes(f)}
+                  onClick={() => handleFilterChange('status', f)}
+                >
+                  <StatusCellIcon status={f} showText size="sm" />
+                </S.StatusBadge>
+              )
+          )}
         </S.Statuses>
       </S.StatusFilter>
     </FilterWrapper>

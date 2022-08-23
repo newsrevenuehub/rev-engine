0fd8c19edf3ebd1ba318e6833c57e7468317ec52
benjaminEwhite <960351+benjaminEwhite@users.noreply.github.com>
2022-08-23 15:43:42 -0400

    use old address

--- spa/src/components/donationPage/elements/form/address/_Address.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import Input from 'elements/inputs/Input';
-
-export const ConditionallyHiddenInput = styled(Input)`
-  display: ${(props) => (props.show ? 'block' : 'none')};
-`;

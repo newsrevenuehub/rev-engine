82174c63c9f18366a106bd9ceca9cd946c7144fb
Guilherme Goes Mendonca <9381325+GuiMend@users.noreply.github.com>
2022-08-24 16:18:37 -0300

    DEV-2232: Update PageButton to styled components

--- spa/src/components/common/Button/PageButton/PageButton.js
+++ spa/src/components/common/Button/PageButton/PageButton.js
@@ -1,44 +1,38 @@
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
+
+import { Flex, Tag, Button, Icon, Background, Label } from './PageButton.styled';
 
 import EditIcon from 'assets/icons/edit.svg';
 
-const PageButton = ({ name, page_screenshot, published_date, onClick }) => (
-  <div className={clsx('flex flex-col items-start relative')}>
-    {published_date && (
-      <p className="absolute px-2 py-1 z-10 leading-3 top-1.5 left-1.5 text-xs font-semibold bg-nature-green rounded-sm text-white">
-        LIVE
-      </p>
-    )}
-    <button
-      onClick={onClick}
-      className="h-[120px] w-[168px] bg-white/[0.3] absolute hover:bg-zinc-800/[0.5] group flex justify-center items-center rounded-md active:bg-light-blue/[0.5] peer"
-    >
-      <img src={EditIcon} alt="edit page" className="hidden group-hover:block" />
-    </button>
-    <div
+const PageButton = ({ className, name, page_screenshot, published_date, onClick }) => (
+  <Flex className={className}>
+    {published_date && <Tag>LIVE</Tag>}
+    <Button onClick={onClick}>
+      <Icon src={EditIcon} alt="edit page" />
+    </Button>
+    <Background
       data-testid="background-image"
+      hasImage={!!page_screenshot}
       {...(page_screenshot && { style: { backgroundImage: `url(${page_screenshot})` } })}
-      className={clsx('bg-slate-200 rounded-md h-[120px] w-[168px] flex justify-center items-center', {
-        'bg-no-repeat bg-cover bg-center': page_screenshot
-      })}
     >
-      {!page_screenshot && <p className="text-neutral-500">No preview</p>}
-    </div>
-    <label className="font-semibold mt-3 break-words max-w-[168px] peer-active:text-light-blue">{name}</label>
-  </div>
+      {!page_screenshot && <p>No preview</p>}
+    </Background>
+    <Label>{name}</Label>
+  </Flex>
 );
 
 PageButton.propTypes = {
   name: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   page_screenshot: PropTypes.string,
-  published_date: PropTypes.string
+  published_date: PropTypes.string,
+  className: PropTypes.string
 };
 
 PageButton.defaultProps = {
   page_screenshot: null,
-  published_date: null
+  published_date: null,
+  className: ''
 };
 
 export default PageButton;

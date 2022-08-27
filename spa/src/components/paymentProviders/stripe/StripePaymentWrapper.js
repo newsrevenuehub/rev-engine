8c089b5a7c2c91e3132724279c7b34f250067212
benjaminEwhite <960351+benjaminEwhite@users.noreply.github.com>
2022-08-26 21:38:00 -0400

    add PaymentSuccess component, refactor GenericThankYou

---
+++ spa/src/components/paymentProviders/stripe/StripePaymentWrapper.js
@@ -0,0 +1,43 @@
+import { useRef } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+import { HUB_STRIPE_API_PUB_KEY } from 'settings';
+import StripePaymentForm from 'components/paymentProviders/stripe/StripePaymentForm';
+import { usePage } from 'components/donationPage/DonationPage';
+import GlobalLoading from 'elements/GlobalLoading';
+
+// This element loads the Stripe `Elements` component, which the Stripe `PaymentElement` needs
+// in order to have expected context. This is where we configure and load Stripe with the connected
+// account for this page. Note that the `stripeClientSecret` value from `usePage` gets created
+// only after user has submitted initial checkout form, which causes a Stripe PaymentIntent to
+// be created on the backend.
+function StripePaymentWrapper() {
+  const {
+    page: {
+      payment_provider: { stripe_account_id: stripeAccount }
+    },
+    stripeClientSecret
+  } = usePage();
+
+  // we pass the connected RP's Stripe Account Id into options
+  // see https://stripe.com/docs/connect/authentication#adding-the-connected-account-id-to-a-client-side-application
+  // Also, we use `useRef` here with `loadStripe` because we have dynamic data (the connected Stripe account)
+  // which will only be available after `usePage` runs. In the Stripe docs, they show
+  // running `loadStripe` outside of the component in module level scope.
+  const stripePromise = useRef(loadStripe(HUB_STRIPE_API_PUB_KEY, { stripeAccount }));
+
+  const options = {
+    clientSecret: stripeClientSecret
+  };
+
+  return stripeClientSecret ? (
+    <Elements stripe={stripePromise} options={options}>
+      <StripePaymentForm />
+    </Elements>
+  ) : (
+    <GlobalLoading />
+  );
+}
+
+export default StripePaymentWrapper;

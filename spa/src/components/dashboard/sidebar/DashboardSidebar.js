d0c237ca19cb7de82b6ef34ee4f37acd96ea94f2
Chris Klimas <chris.klimas@fundjournalism.org>
2022-09-01 12:44:59 -0400

    DEV-1969: Add dashboard footer with help and FAQ links

--- spa/src/components/dashboard/sidebar/DashboardSidebar.js
+++ spa/src/components/dashboard/sidebar/DashboardSidebar.js
@@ -9,6 +9,7 @@ import { CONTENT_SECTION_ACCESS_FLAG_NAME } from 'constants/featureFlagConstants
 import flagIsActiveForUser from 'utilities/flagIsActiveForUser';
 import { useFeatureFlagsProviderContext } from 'components/Main';
 import hasContributionsDashboardAcessToUser from 'utilities/hasContributionsDashboardAcessToUser';
+import DashboardSidebarFooter from './DashboardSidebarFooter';
 
 /*
 // Commenting out for now as this will needed in future
@@ -35,6 +36,7 @@ function DashboardSidebar() {
         {hasContentSectionAccess ? <ContentSectionNav /> : null}
         {hasContributionsSectionAccess ? <ContributionSectionNav /> : null}
       </S.NavList>
+      <DashboardSidebarFooter />
     </S.DashboardSidebar>
   );
 }
---
+++ spa/src/components/dashboard/sidebar/DashboardSidebarFooter.js
@@ -0,0 +1,42 @@
+import * as S from './DashboardSidebar.styled';
+import { ICONS } from 'assets/icons/SvgIcon';
+import HelpOutlined from '@material-ui/icons/HelpOutline';
+
+// Exported mainly to help with unit tests.
+
+export const footerHrefs = {
+  faq: 'https://news-revenue-hub.atlassian.net/servicedesk/customer/portal/11/article/2195423496',
+  help: 'mailto:revengine-support@fundjournalism.org'
+};
+
+const DashboardSidebarFooter = () => (
+  <S.NavSection>
+    <S.NavItem
+      aria-labelledby="help-nav-item-id"
+      as="a"
+      href={footerHrefs.help}
+      role="listitem"
+      data-testid="nav-help-item"
+    >
+      <S.NavItemIcon icon={ICONS.RING_BUOY} />
+      <S.SideBarText id="help-nav-item-id">Help</S.SideBarText>
+    </S.NavItem>
+    <S.NavItem
+      aria-labelledby="faq-nav-item-id"
+      as="a"
+      href={footerHrefs.faq}
+      role="listitem"
+      data-testid="nav-faq-item"
+      target="_blank"
+    >
+      <S.NavItemMaterialIcon>
+        <HelpOutlined />
+      </S.NavItemMaterialIcon>
+      <S.SideBarText id="faq-nav-item-id" viewBox="0 0 16 16">
+        FAQ
+      </S.SideBarText>
+    </S.NavItem>
+  </S.NavSection>
+);
+
+export default DashboardSidebarFooter;

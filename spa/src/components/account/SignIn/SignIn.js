b87ff4a2f6a80173a0d332493c3a760f02ad6c86
gs <gs@pieguytechs-MacBook-Air.local>
2022-09-06 21:27:02 -0400

    sign-n refactor and unit tests

--- spa/src/components/account/SignIn/SignIn.js
+++ spa/src/components/account/SignIn/SignIn.js
@@ -1,6 +1,5 @@
 import { useState, useReducer } from 'react';
 import * as S from '../Account.styled';
-import { useForm } from 'react-hook-form';
 
 // AJAX
 import axios from 'ajax/axios';
@@ -18,24 +17,18 @@ import { handleLoginSuccess } from 'components/authentication/util';
 // Analytics
 import { useConfigureAnalytics } from '../../analytics';
 
+import SignInForm from './SignInForm';
 import Logobar from 'components/account/common/logobar/Logobar';
 import Leftbar from 'components/account/common/leftbar/Leftbar';
 
-import visibilityOn from 'assets/images/account/visibility_on.png';
-import visibilityOff from 'assets/images/account/visibility_off.png';
-
-import { SIGN_UP, FORGOT_PASSWORD } from 'routes';
+import { SIGN_UP } from 'routes';
 import YellowSVG from 'assets/images/account/yellow-bar.svg';
 
-function SignIn({ onSuccess, message }) {
+function SignIn({ onSuccess }) {
   const history = useHistory();
 
   const [signInState, dispatch] = useReducer(fetchReducer, initialState);
-
-  const [showPassword, setShowPassword] = useState(false);
-  const togglePasswordVisiblity = () => {
-    setShowPassword(showPassword ? false : true);
-  };
+  const [loading, setLoading] = useState(false);
 
   useConfigureAnalytics();
 
@@ -46,12 +39,8 @@ function SignIn({ onSuccess, message }) {
 
   const formSubmitErrors = signInState?.errors?.detail;
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm();
   const onSubmitSignIn = async (fdata) => {
+    setLoading(true);
     dispatch({ type: FETCH_START });
     try {
       const { data, status } = await axios.post(TOKEN, { email: fdata.email, password: fdata.password });
@@ -63,12 +52,13 @@ function SignIn({ onSuccess, message }) {
     } catch (e) {
       dispatch({ type: FETCH_FAILURE, payload: e?.response?.data });
     }
+    setLoading(false);
   };
 
   return (
     <>
       <S.Outer>
-        <S.Left data-testid="left-yellow">
+        <S.Left data-testid="left-section">
           <Leftbar />
         </S.Left>
         <S.Right>
@@ -76,61 +66,7 @@ function SignIn({ onSuccess, message }) {
             <S.Heading>Welcome Back!</S.Heading>
             <br />
 
-            <form onSubmit={handleSubmit(onSubmitSignIn)}>
-              <S.InputLabel data-testid={`email-label`} hasError={errors.email}>
-                Email
-              </S.InputLabel>
-              <S.InputOuter hasError={errors.email}>
-                <input
-                  id="email"
-                  {...register('email', {
-                    required: 'Please enter a valid email',
-                    pattern: {
-                      value: /\S+@\S+\.\S+/,
-                      message: 'Please enter a valid email'
-                    }
-                  })}
-                  type="text"
-                  status={errors.email}
-                  data-testid="signin-email"
-                />
-              </S.InputOuter>
-              {errors.email ? (
-                <S.Message role="error" data-testid="email-error">
-                  {errors.email.message}
-                </S.Message>
-              ) : (
-                <S.MessageSpacer />
-              )}
-
-              <S.PasswordLabel>
-                Password
-                <a href={FORGOT_PASSWORD} data-testid="reset-password" tabindex="-1">
-                  Forgot Password?
-                </a>
-              </S.PasswordLabel>
-              <S.InputOuter hasError={errors.password}>
-                <input
-                  id="password"
-                  {...register('password', {
-                    required: 'Please enter your password'
-                  })}
-                  type={showPassword ? 'text' : 'password'}
-                  status={errors.password}
-                  data-testid="signin-password"
-                />
-                <S.Visibility
-                  data-testid="toggle-password"
-                  onClick={togglePasswordVisiblity}
-                  src={showPassword ? visibilityOn : visibilityOff}
-                />
-              </S.InputOuter>
-              {errors.password ? <S.Message role="error">{errors.password.message}</S.Message> : <S.MessageSpacer />}
-
-              <S.Submit type="submit" role="button">
-                Sign In
-              </S.Submit>
-            </form>
+            <SignInForm onSubmitSignIn={onSubmitSignIn} loading={loading} />
             {formSubmitErrors ? <S.Message>{formSubmitErrors} </S.Message> : <S.MessageSpacer />}
 
             <S.SignInLink>
@@ -144,8 +80,8 @@ function SignIn({ onSuccess, message }) {
           <Logobar />
         </S.Right>
       </S.Outer>
-      <S.BottomBar data-testid={`bottom-yellow-bar`}>
-        <S.BottomBarYellowSVG src={YellowSVG} />
+      <S.BottomBar>
+        <S.BottomBarYellowSVG src={YellowSVG} data-testid="bottom-yellow-svg" />
       </S.BottomBar>
     </>
   );
--- spa/src/components/account/SignIn/SignIn.test.js
+++ spa/src/components/account/SignIn/SignIn.test.js
@@ -3,39 +3,45 @@ import ReactTestUtils from 'react-dom/test-utils';
 import SignIn from './SignIn';
 import { SIGN_UP, FORGOT_PASSWORD } from 'routes';
 
-it('should show the right revengine logo', () => {
-  render(<SignIn />);
-  const bottomYellowBar = screen.queryByTestId('logo2');
-  expect(bottomYellowBar).toBeInTheDocument();
-});
+import Input from 'elements/inputs/Input';
 
-it('should have yellow left bar and purple bottom bar', () => {
-  render(<SignIn />);
-  const bottomYellowBar = screen.queryByTestId('bottom-yellow-bar');
-  expect(bottomYellowBar).toBeInTheDocument();
-  const leftPurple = screen.queryByTestId('left-yellow');
-  expect(leftPurple).toBeInTheDocument();
-});
+describe('SignIn Component Tests', () => {
+  it('should show the right revengine logo', () => {
+    render(<SignIn />);
+    const bottomYellowBar = screen.queryByTestId('logo2');
+    expect(bottomYellowBar).toBeInTheDocument();
+  });
 
-it('should have heading - Welcome Back!', () => {
-  render(<SignIn />);
-  const title = screen.queryByText('Welcome Back!');
-  expect(title).toBeInTheDocument();
-});
+  it('should have left section and yellow bottom bar', () => {
+    render(<SignIn />);
+    const bottomYellowBar = screen.queryByTestId('bottom-yellow-svg');
+    expect(bottomYellowBar).toBeInTheDocument();
+    const leftSection = screen.queryByTestId('left-section');
+    expect(leftSection).toBeInTheDocument();
+  });
 
-it('should have link to take user to sign-in page and reset password page', () => {
-  render(<SignIn />);
-  const signUp = screen.queryByTestId('create-account');
-  expect(signUp).toHaveAttribute('href', SIGN_UP);
-  const resetPassword = screen.queryByTestId('reset-password');
-  expect(resetPassword).toHaveAttribute('href', FORGOT_PASSWORD);
-});
+  it('should have heading - Welcome Back!', () => {
+    render(<SignIn />);
+    const title = screen.queryByText('Welcome Back!');
+    expect(title).toBeInTheDocument();
+  });
 
-it('should have link to take user to sign in page and reset password page', () => {
-  render(<SignIn />);
-  const signUp = screen.queryByTestId('create-account');
-  expect(signUp).toHaveAttribute('href', SIGN_UP);
+  it('should have link to take user to sign-in page and reset-password page', () => {
+    render(<SignIn />);
+    const signUp = screen.queryByTestId('create-account');
+    expect(signUp).toHaveAttribute('href', SIGN_UP);
+    const resetPassword = screen.queryByTestId('reset-password');
+    expect(resetPassword).toHaveAttribute('href', FORGOT_PASSWORD);
+  });
 
-  const resetPassword = screen.queryByTestId('reset-password');
-  expect(resetPassword).toHaveAttribute('href', FORGOT_PASSWORD);
+  it('should have password toggle icon to show/hide value of password if visibility icon is clicked', () => {
+    render(<SignIn />);
+    const password = screen.queryByTestId(`signin-pwd-${Input.types.PASSWORD}`);
+    ReactTestUtils.Simulate.change(password, { target: { value: 'password' } });
+    const toggleIcon = screen.getByTestId('toggle-password-invisible');
+    fireEvent.click(toggleIcon);
+    expect(screen.getByTestId(`signin-pwd-${Input.types.TEXT}`)).toBeInTheDocument();
+    fireEvent.click(toggleIcon);
+    expect(screen.getByTestId(`signin-pwd-${Input.types.PASSWORD}`)).toBeInTheDocument();
+  });
 });

bab2edf91b4843467ec3eaf0ee8715b79be340e5
benjaminEwhite <960351+benjaminEwhite@users.noreply.github.com>
2022-09-07 14:17:19 -0400

    remove allow_null=True

--- apps/contributions/serializers.py
+++ apps/contributions/serializers.py
@@ -266,7 +266,10 @@ class BaseCreatePaymentSerializer(serializers.Serializer):
     phone = serializers.CharField(max_length=40, required=False, allow_blank=True, write_only=True, default="")
     # See class-level doc string for info on why `default=''` here
     reason_for_giving = serializers.CharField(
-        max_length=255, required=False, allow_blank=True, write_only=True, default=""
+        max_length=255,
+        required=False,
+        allow_blank=True,
+        write_only=True,
     )
     # See class-level doc string for info on why `default=''` here
     reason_other = serializers.CharField(max_length=255, required=False, allow_blank=True, write_only=True, default="")
@@ -329,7 +332,7 @@ class BaseCreatePaymentSerializer(serializers.Serializer):
                 # reason_for_giving because that field was not in the request data. If that happens and the SPA has included
                 # `reason_other` as an entry in the request data, that means that the page has configured to require a reason_for_giving,
                 # but a dropdown of preset choices has not been configured.
-                reason_for_giving == "" and "reason_other" in self.initial_data.keys(),
+                reason_for_giving is None and "reason_other" in self.initial_data.keys(),
             ]
         ):
             return reason_other

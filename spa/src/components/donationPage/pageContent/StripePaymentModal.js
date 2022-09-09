2612f6956ae1754966a7d32e3b61e553303949d0
benjaminEwhite <960351+benjaminEwhite@users.noreply.github.com>
2022-09-09 15:04:16 -0400

    remove unused file

--- spa/src/components/donationPage/pageContent/StripePaymentModal.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-import Modal from 'elements/modal/Modal';
-
-function StripePaymentModal({ stripeClientSecret }) {
-  return <>Stripe payment modal!</>;
-}
-
-export default StripePaymentModal;

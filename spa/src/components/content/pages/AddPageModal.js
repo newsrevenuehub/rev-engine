9529a86d32f47b258844cb42b79452a3699ef8c7
gs <gs@pieguytechs-MacBook-Air.local>
2022-09-12 18:58:09 -0400

    modal fix

--- spa/src/components/content/pages/AddPageModal.js
+++ spa/src/components/content/pages/AddPageModal.js
@@ -34,10 +34,9 @@ function AddPageModal({ isOpen, closeModal }) {
   const alert = useAlert();
   const theme = useTheme();
   const history = useHistory();
-  const { revenue_programs: revenuePrograms } = useUser();
 
   const { user } = useUserContext();
-  const revenue_programs = user?.revenuePrograms;
+  const revenuePrograms = user?.revenue_programs;
 
   const fetchTemplates = useRequest();
   const createPage = useRequest();

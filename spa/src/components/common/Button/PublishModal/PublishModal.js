d1ccde897ca6e444b29d1df820d2b7663db7a07c
Guilherme Goes Mendonca <9381325+GuiMend@users.noreply.github.com>
2022-09-12 16:53:14 -0300

    DEV-2303: Create PublishModal

---
+++ spa/src/components/common/Button/PublishModal/PublishModal.js
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Divider, Grid } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import PublicIcon from '@material-ui/icons/Public';
+
+import {
+  Flex,
+  Title,
+  Paper,
+  Modal,
+  Icon,
+  IconButton,
+  Content,
+  Label,
+  Input,
+  UnderText,
+  Actions,
+  PublishButton,
+  CancelButton
+} from './PublishModal.styled';
+import getDomain from 'utilities/getDomain';
+import { PagePropTypes } from 'constants/proptypes';
+import slugify from 'utilities/slugify';
+
+const PublishModal = ({ open, onClose, onPublish, page }) => {
+  const [slug, setSlug] = useState('');
+  const domain = getDomain(window.location.host);
+
+  const domainUrl = `.${domain}/`;
+
+  const handleChangeSlug = (event) => {
+    setSlug(slugify(event.target.value ?? ''));
+  };
+
+  return (
+    <Modal open={open} onClose={onClose} aria-labelledby={`Publish page ${page?.name}`}>
+      <Paper>
+        <IconButton onClick={onClose}>
+          <Icon type="grey">
+            <CloseIcon />
+          </Icon>
+        </IconButton>
+        <Flex>
+          <PublicIcon />
+          <Title>Publish Page</Title>
+        </Flex>
+        <Divider />
+        <Content>
+          <p style={{ margin: 0 }}>
+            Your URL is where contributors will access your contribution page. Fill out the fields to create your
+            contribution page link.
+          </p>
+          <Grid container>
+            <Grid item xs={4}>
+              <Label>Site Name*</Label>
+            </Grid>
+            <Grid item xs={3} />
+            <Grid item xs={5}>
+              <Label>Page Name</Label>
+            </Grid>
+            <Grid item xs={4}>
+              <Input value={page?.revenue_program?.slug} start="true" onChange={() => {}} />
+            </Grid>
+            <Grid item xs={3}>
+              <Input disabled defaultValue={domainUrl} center="true" aria-label={`Domain url: ${domainUrl}`} />
+            </Grid>
+            <Grid item xs={5}>
+              <Input
+                placeholder="Ex. contribute, donate, join"
+                end="true"
+                value={slug}
+                onChange={handleChangeSlug}
+                aria-label="Page name"
+              />
+            </Grid>
+            <Grid item xs={5}>
+              <UnderText>*Site name can’t be changed upon publish.</UnderText>
+            </Grid>
+            <Grid item xs={2} />
+            <Grid item xs={5}>
+              {/* TODO: add input error messages */}
+            </Grid>
+          </Grid>
+          <Actions>
+            <CancelButton variant="contained" onClick={onClose} disableElevation>
+              Cancel
+            </CancelButton>
+            <PublishButton variant="contained" onClick={onPublish} disableElevation disabled={!slug}>
+              Publish
+            </PublishButton>
+          </Actions>
+        </Content>
+      </Paper>
+    </Modal>
+  );
+};
+
+PublishModal.propTypes = {
+  className: PropTypes.string,
+  page: PropTypes.shape(PagePropTypes).isRequired,
+  open: PropTypes.bool,
+  onClose: PropTypes.func.isRequired,
+  onPublish: PropTypes.func.isRequired
+};
+
+PublishModal.defaultProps = {
+  className: '',
+  open: false
+};
+
+export default PublishModal;

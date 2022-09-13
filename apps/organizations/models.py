d065de03a1f454dd34e6a4e11926ac075afcf68c
benjaminEwhite <960351+benjaminEwhite@users.noreply.github.com>
2022-09-13 10:33:56 -0400

    remove stale TODO

--- apps/organizations/models.py
+++ apps/organizations/models.py
@@ -335,7 +335,6 @@ class RevenueProgram(IndexedTimeStampedModel):
 
 class PaymentProvider(IndexedTimeStampedModel):
     stripe_account_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
-    # TODO find out logic around this. Should it have a default value
     stripe_product_id = models.CharField(max_length=255, blank=True, null=True)
 
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default="USD")

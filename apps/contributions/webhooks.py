8389469dad36acbb20c8afcf6227b9b2a1e73caf
benjaminEwhite <960351+benjaminEwhite@users.noreply.github.com>
2022-09-21 15:06:28 -0400

    add log

--- apps/contributions/webhooks.py
+++ apps/contributions/webhooks.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(f"{settings.DEFAULT_LOGGER}.{__name__}")
 
 class StripeWebhookProcessor:
     def __init__(self, event):
+        logger.info("StripeWebhookProcessor initialized with event data: %s", event)
         self.event = event
         self.obj_data = self.event.data["object"]
 

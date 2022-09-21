48630baa089b49da404eaa2aaeffe814af9df59e
Harsha Bharadwaj <harsha.bajju@gmail.com>
2022-09-22 01:45:30 +0530

    add fname lname job title user fields in admin page

--- apps/users/tests/test_admin.py
+++ apps/users/tests/test_admin.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
+from bs4 import BeautifulSoup
 
 import apps.users
 
@@ -27,3 +28,12 @@ class TestUsersAdmin(TestCase):
         assert [
             "user",
         ] == t.get_readonly_fields(None, obj=self.user)
+
+    def test_user_fields_in_admin(self):
+        user_id = user_model.objects.all()[0].id
+        response = self.client.get(f"/nrhadmin/users/user/{user_id}/change/")
+        soup = BeautifulSoup(response.content)
+        self.assertIsNotNone(soup.find("input", {"name": "email"}))
+        self.assertIsNotNone(soup.find("input", {"name": "first_name"}))
+        self.assertIsNotNone(soup.find("input", {"name": "last_name"}))
+        self.assertIsNotNone(soup.find("input", {"name": "job_title"}))

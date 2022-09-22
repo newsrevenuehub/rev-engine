49d81b7db502133df62605658c7b8e95872b9c2b
Chris Klimas <chris.klimas@fundjournalism.org>
2022-09-22 13:04:05 -0400

    DEV-2387: Fix test

--- spa/src/components/account/Profile/InfoTooltip.test.js
+++ spa/src/components/account/Profile/InfoTooltip.test.js
@@ -4,32 +4,32 @@ import { render, screen, waitFor } from 'test-utils';
 import InfoTooltip from './InfoTooltip';
 
 function tree(props) {
-  return render(<InfoTooltip title="mock-tooltip-text" {...props} />);
+  return render(<InfoTooltip buttonLabel="mock-label" title="mock-tooltip-text" {...props} />);
 }
 
 describe('InfoTooltip', () => {
-  it('displays a button labeled Help', () => {
-    tree();
-    expect(screen.getByRole('button', { name: 'Help' })).toBeVisible();
+  it('displays a button labeled with the buttonLabel prop', () => {
+    tree({ buttonLabel: 'test label' });
+    expect(screen.getByRole('button', { name: 'test label' })).toBeVisible();
   });
 
   it('displays the tooltip when the button is clicked', async () => {
     tree({ title: 'test tooltip' });
-    userEvent.click(screen.getByRole('button', { name: 'Help' }));
+    userEvent.click(screen.getByRole('button', { name: 'mock-label' }));
     await waitFor(() => expect(screen.getByText('test tooltip')).toBeVisible());
   });
 
   it('keeps the tooltip open if the button is clicked repeatedly', async () => {
     tree();
-    userEvent.click(screen.getByRole('button', { name: 'Help' }));
+    userEvent.click(screen.getByRole('button', { name: 'mock-label' }));
     await waitFor(() => expect(screen.getByText('mock-tooltip-text')).toBeVisible());
-    userEvent.click(screen.getByRole('button', { name: 'Help' }));
+    userEvent.click(screen.getByRole('button', { name: 'mock-label' }));
     expect(screen.getByText('mock-tooltip-text')).toBeVisible();
   });
 
   it('hides the tooltip if the user clicks elsewhere', async () => {
     tree();
-    userEvent.click(screen.getByRole('button', { name: 'Help' }));
+    userEvent.click(screen.getByRole('button', { name: 'mock-label' }));
     await waitFor(() => expect(screen.getByText('mock-tooltip-text')).toBeVisible());
     userEvent.click(document.body);
     await waitFor(() => expect(screen.queryByText('mock-tooltip-text')).not.toBeInTheDocument());
@@ -45,7 +45,7 @@ describe('InfoTooltip', () => {
     it('is accessible when the tooltip is open', async () => {
       const { container } = tree();
 
-      userEvent.click(screen.getByRole('button', { name: 'Help' }));
+      userEvent.click(screen.getByRole('button', { name: 'mock-label' }));
       expect(await axe(container)).toHaveNoViolations();
     });
   });

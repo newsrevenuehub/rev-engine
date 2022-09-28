a7e732f74213958fff6c7adf1bf3e68850bfb231
benjaminEwhite <960351+benjaminEwhite@users.noreply.github.com>
2022-09-28 08:30:30 -0400

    react query approach

--- spa/src/components/account/Profile/Profile.js
+++ spa/src/components/account/Profile/Profile.js
@@ -1,5 +1,6 @@
-import { useReducer } from 'react';
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import * as S from './Profile.styled';
 import * as A from 'components/account/Account.styled';
 
@@ -8,7 +9,6 @@ import axios from 'ajax/axios';
 import { CUSTOMIZE_ACCOUNT_ENDPOINT } from 'ajax/endpoints';
 
 // State management
-import fetchReducer, { initialState, FETCH_START, FETCH_FAILURE, FETCH_SUCCESS } from 'state/fetch-reducer';
 import useUser from 'hooks/useUser';
 
 import useModal from 'hooks/useModal';
@@ -18,49 +18,55 @@ import { useConfigureAnalytics } from 'components/analytics';
 
 import ProfileForm from './ProfileForm';
 
+function patchAccountCustomization(userId, data) {
+  return axios.patch(`users/${userId}/${CUSTOMIZE_ACCOUNT_ENDPOINT}`, data);
+}
+
 function Profile() {
   const { open } = useModal(true);
   const history = useHistory();
-  const [profileState, dispatch] = useReducer(fetchReducer, initialState);
-  const { refetch: refetchUser, user } = useUser();
+  const { user } = useUser();
+  const [errors, setErrors] = useState();
   useConfigureAnalytics();
 
-  const onProfileSubmit = async (formData) => {
-    dispatch({ type: FETCH_START });
+  const queryClient = useQueryClient();
 
-    try {
-      const { data, status } = await axios.patch(`users/${user.id}/${CUSTOMIZE_ACCOUNT_ENDPOINT}`, {
-        first_name: formData.firstName,
-        last_name: formData.lastName,
-        // Don't send job_title at all if the user omitted it.
-        job_title: formData.jobTitle.trim() !== '' ? formData.jobTitle : undefined,
-        organization_name: formData.companyName,
-        organization_tax_status: formData.companyTaxStatus
-      });
-
-      if (status === 204) {
-        dispatch({ type: FETCH_SUCCESS });
-        await refetchUser();
-        history.push('/');
-      } else {
-        dispatch({ type: FETCH_FAILURE, payload: data });
+  const { mutate: doProfileCustomization, isLoading: profileCustomizationIsLoading } = useMutation(
+    (userId, data) => {
+      return patchAccountCustomization(userId, data);
+    },
+    {
+      onSuccess: () => queryClient.invalidateQueries(['user']),
+      onError: (err) => {
+        // calling console.error will create a Sentry error
+        // console.error(err);
+        //
+        // do the stuff to trigger displaying form errors
+        // (side note, this is where RHF would be nice)
+        //
       }
-    } catch (e) {
-      // If we didn't get a specific error message from the API, default to
-      // something generic.
-
-      dispatch({ type: FETCH_FAILURE, payload: e?.response?.data ?? [new Error('Request failed')] });
     }
+  );
+
+  const onProfileSubmit = async (formData) => {
+    doProfileCustomization(user.id, {
+      first_name: formData.firstName,
+      last_name: formData.lastName,
+      // Don't send job_title at all if the user omitted it.
+      job_title: formData.jobTitle.trim() !== '' ? formData.jobTitle : undefined,
+      organization_name: formData.companyName,
+      organization_tax_status: formData.companyTaxStatus
+    }).then(() => history.push('/'));
   };
 
-  const formSubmitErrors = profileState?.errors?.length && 'An Error Occurred';
+  const formSubmitErrors = errors?.length && 'An Error Occurred';
 
   return (
     <S.Modal open={open} aria-labelledby="profile-modal-title" data-testid="finalize-profile-modal">
       <S.Profile>
         <S.h1 id="profile-modal-title">Let's Customize Your Account</S.h1>
         <S.Description>Help us create your personalized experience!</S.Description>
-        <ProfileForm onProfileSubmit={onProfileSubmit} disabled={profileState.loading} />
+        <ProfileForm onProfileSubmit={onProfileSubmit} disabled={profileCustomizationIsLoading} />
         {formSubmitErrors ? (
           <A.Message data-testid="profile-modal-error">{formSubmitErrors}</A.Message>
         ) : (

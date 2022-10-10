76bd9775eae392c48f98490397538844c17a7439
Guilherme Goes Mendonca <9381325+GuiMend@users.noreply.github.com>
2022-10-10 18:51:28 -0300

    DEV-2555: Tooltip base component update spacing

--- spa/src/components/base/Tooltip/Tooltip.js
+++ spa/src/components/base/Tooltip/Tooltip.js
@@ -1,5 +1,5 @@
-import { Tooltip as MuiTooltip, TooltipProps as MuiTooltipProps } from '@material-ui/core';
-import propTypes from 'prop-types';
+import { Tooltip as MuiTooltip } from '@material-ui/core';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 // See https://v4.mui.com/guides/interoperability/#portals
@@ -14,6 +14,7 @@ const WrappedTooltip = ({ className, tooltipWidth, ...other }) => (
 export const Tooltip = styled(WrappedTooltip)`
   && {
     background: rgb(50, 50, 50);
+    margin-top: 6px;
     border-radius: 4px;
     color: white;
     font: 12px Roboto, sans-serif;
@@ -23,12 +24,12 @@ export const Tooltip = styled(WrappedTooltip)`
 `;
 
 Tooltip.propTypes = {
-  ...MuiTooltip.propTypes,
   /**
    * Width of the tooltip in pixels. If omitted, this lets MUI size it to what
    * it thinks is appropriate.
    */
-  tooltipWidth: propTypes.number
+  tooltipWidth: PropTypes.number,
+  className: PropTypes.string
 };
 
 export default Tooltip;

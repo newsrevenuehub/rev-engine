17ff3f9fc93c77c65effcf125b12ec81ca30203b
Guilherme Goes Mendonca <9381325+GuiMend@users.noreply.github.com>
2022-10-11 11:14:02 -0300

    DEV-2555: Update tooltip component and tests

--- spa/src/components/base/Tooltip/Tooltip.js
+++ spa/src/components/base/Tooltip/Tooltip.js
@@ -1,5 +1,5 @@
 import { Tooltip as MuiTooltip } from '@material-ui/core';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 import styled from 'styled-components';
 
 // See https://v4.mui.com/guides/interoperability/#portals
@@ -14,22 +14,22 @@ const WrappedTooltip = ({ className, tooltipWidth, ...other }) => (
 export const Tooltip = styled(WrappedTooltip)`
   && {
     background: rgb(50, 50, 50);
-    margin-top: 6px;
+    margin: 6px;
     border-radius: 4px;
     color: white;
     font: 12px Roboto, sans-serif;
     padding: 8px;
-    width: ${({ tooltipWidth }) => `${tooltipWidth}px` ?? 'auto'};
+    width: ${({ tooltipWidth }) => (tooltipWidth ? `${tooltipWidth}px` : 'auto')};
   }
 `;
 
 Tooltip.propTypes = {
+  ...MuiTooltip.propTypes,
   /**
    * Width of the tooltip in pixels. If omitted, this lets MUI size it to what
    * it thinks is appropriate.
    */
-  tooltipWidth: PropTypes.number,
-  className: PropTypes.string
+  tooltipWidth: propTypes.number
 };
 
 export default Tooltip;

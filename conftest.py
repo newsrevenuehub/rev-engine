a9936e212b2cf032d0ff3eb94d49f546fa8f561d
Leo Alves <leo.dcfa@gmail.com>
2022-10-30 17:35:07 +1000

    DEV-2470: more env variables + markers

--- conftest.py
+++ conftest.py
@@ -11,7 +11,8 @@ def dont_use_ssl(settings):
 
 @pytest.fixture(autouse=True)
 def patch_google_cloud_pub_sub_publisher(request):
-    if "no_patch_google_cloud_pub_sub_publisher" in request.keywords:
+    marker = request.node.get_closest_marker("no_patch_google_cloud_pub_sub_publisher")
+    if marker:
         return
     patched = mock.patch("apps.google_pub_sub.publisher.GoogleCloudPubSubPublisher.publish", MagicMock())
     patched.__enter__()

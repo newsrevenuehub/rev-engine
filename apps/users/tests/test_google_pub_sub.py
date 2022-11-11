6ba17823d1eb6ad76436a2ffd61f2f296390c94e
Leo Alves <leo.dcfa@gmail.com>
2022-11-12 08:53:46 +1000

    DEV-2470: apply feedback second code review

--- apps/users/tests/test_google_pub_sub.py
+++ apps/users/tests/test_google_pub_sub.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock
 
 from google.cloud.pubsub_v1 import futures
 
@@ -9,10 +9,11 @@ def test_encodes_string_to_bytes():
     assert isinstance(Message("irrelevant").data, bytes)
 
 
-@patch("django.conf.settings.GOOGLE_CLOUD_PROJECT")
-@patch("apps.users.google_pub_sub.pubsub_v1.PublisherClient")
-def test_publishes_to_google_cloud_pub_sub(publisher_client, google_cloud_project):
-    google_cloud_project.return_value = "project"
+def test_publishes_to_google_cloud_pub_sub(monkeypatch):
+    google_cloud_project = "project"
+    publisher_client = MagicMock()
+    monkeypatch.setattr("django.conf.settings.GOOGLE_CLOUD_PROJECT", google_cloud_project)
+    monkeypatch.setattr("apps.users.google_pub_sub.pubsub_v1.PublisherClient", publisher_client)
     topic = "topic"
     expected = "result"
     future = futures.Future()
@@ -25,5 +26,5 @@ def test_publishes_to_google_cloud_pub_sub(publisher_client, google_cloud_projec
     message = Message(data="some message")
     result = publisher.publish(topic, message)
     assert expected == result
-    publisher_client.topic_path.assert_called_with(google_cloud_project, topic)
-    publisher_client.publish.assert_called_with(full_topic_path, message.data)
+    publisher_client.topic_path.called_with(google_cloud_project, topic)
+    publisher_client.publish.called_with(full_topic_path, message.data)

e302c5bdc246bab604a6c30a715781ca9803d132
benjaminEwhite <960351+benjaminEwhite@users.noreply.github.com>
2022-11-15 09:56:27 -0500

    add minimal unit tests

---
+++ spa/src/components/dashboard/connectStripe/ConnectStripeNeedHelpCTA.test.tsx
@@ -0,0 +1,17 @@
+import exp from 'constants';
+import { axe } from 'jest-axe';
+import { render, screen } from 'test-utils';
+
+import ConnectStripeNeedHelpCTA from './ConnectStripeNeedHelpCTA';
+
+describe('ConnectStripeNeedHelpTA', () => {
+  it('is accessible', async () => {
+    const { container } = render(<ConnectStripeNeedHelpCTA />);
+    expect(await axe(container)).toHaveNoViolations();
+  });
+
+  it('has the right link', () => {
+    render(<ConnectStripeNeedHelpCTA />);
+    expect(screen.getByRole('link', { name: 'FAQ' })).toBeInTheDocument();
+  });
+});

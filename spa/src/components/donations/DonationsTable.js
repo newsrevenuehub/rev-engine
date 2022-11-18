7877a66b69a05a9e8433b3dfeedfd4393f83806e
benjaminEwhite <960351+benjaminEwhite@users.noreply.github.com>
2022-11-18 10:24:13 -0500

    object code changes to add rp name

--- spa/src/components/donations/DonationsTable.js
+++ spa/src/components/donations/DonationsTable.js
@@ -21,7 +21,12 @@ function DonationsTable({ columns = [], fetchDonations, pageIndex, refetch, onPa
   const [totalResults, setTotalResults] = useState(0);
 
   const convertOrderingToString = (ordering) => {
-    return ordering.map((item) => (item.desc ? `-${item.id}` : item.id)).toString();
+    return ordering
+      .map((item) => {
+        const normalizedId = item.id.replace('.', '__');
+        return item.desc ? `-${normalizedId}` : normalizedId;
+      })
+      .toString();
   };
 
   const fetchData = useCallback(

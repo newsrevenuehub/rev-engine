67ab74048ba32550c437657043b852050f256018
Chris Klimas <chris.klimas@fundjournalism.org>
2022-11-21 16:32:45 -0500

    DEV-2767: Add EditTabHeader

---
+++ spa/src/components/pageEditor/editInterface/EditTabHeader.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Prompt = styled('span')`
+  color: #707070;
+  font-size: 14px;
+`;
+
+export const Root = styled('div')`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding-top: 20px;
+  width: 90%; /* mimicking content container */
+`;
---
+++ spa/src/components/pageEditor/editInterface/EditTabHeader.test.tsx
@@ -0,0 +1,47 @@
+import userEvent from '@testing-library/user-event';
+import { axe } from 'jest-axe';
+import { render, screen } from 'test-utils';
+import EditTabHeader, { EditTabHeaderProps } from './EditTabHeader';
+
+function tree(props?: Partial<EditTabHeaderProps>) {
+  return render(<EditTabHeader prompt="mock-prompt" {...props} />);
+}
+
+describe('EditTabHeader', () => {
+  it('displays the prompt', () => {
+    tree({ prompt: 'test-prompt' });
+    expect(screen.getByText('test-prompt')).toBeVisible();
+  });
+
+  it("doesn't display an add button if the onAdd prop is omitted", () => {
+    tree({ addButtonLabel: 'test-label' });
+    expect(screen.queryByRole('button')).not.toBeInTheDocument();
+  });
+
+  it("doesn't display an add button if the addButtonLabel prop is omitted", () => {
+    tree({ onAdd: jest.fn() });
+    expect(screen.queryByRole('button')).not.toBeInTheDocument();
+  });
+
+  describe('When both addButtonLabel and onAdd props are provided', () => {
+    it('displays a button with the addButtonLabel text', () => {
+      tree({ addButtonLabel: 'test-label', onAdd: jest.fn() });
+      expect(screen.getByRole('button', { name: 'test-label' })).toBeInTheDocument();
+    });
+
+    it('calls onAdd when the button is clicked', () => {
+      const onAdd = jest.fn();
+
+      tree({ onAdd, addButtonLabel: 'test-label' });
+      expect(onAdd).not.toBeCalled();
+      userEvent.click(screen.getByRole('button', { name: 'test-label' }));
+      expect(onAdd).toBeCalledTimes(1);
+    });
+  });
+
+  it('is accessible', async () => {
+    const { container } = tree();
+
+    expect(await axe(container)).toHaveNoViolations();
+  });
+});

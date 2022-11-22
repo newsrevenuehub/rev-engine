3cc122d20f547d10c958d26dd1def552cef8f770
Harsha Bharadwaj <harsha.bajju@gmail.com>
2022-11-22 19:26:32 +0530

    add export contributions to csv file

--- apps/contributions/utils.py
+++ apps/contributions/utils.py
@@ -1,4 +1,6 @@
+import csv
 import hashlib
+from io import StringIO
 
 from django.conf import settings
 
@@ -22,3 +24,57 @@ def get_sha256_hash(string):
     result = hashlib.sha256(string.encode())
     hash_str = result.hexdigest()
     return hash_str[:15]
+
+
+def export_contributions_to_csv(contributions):
+    columns = (
+        "Contributor",
+        "Amount",
+        "Donor Selected Amount",
+        "Agreed to Pay Fees",
+        "Frequency",
+        "Payment Received Date",
+        "Payment status",
+        "Address",
+        "Email",
+        "Phone",
+        "Page URL",
+    )
+    address_keys = ("line1", "line2", "city", "postal_code", "state", "country")
+    data = []
+    for contribution in contributions:
+        payment_provider_data = contribution.payment_provider_data or {}
+        charge_details = (
+            payment_provider_data.get("data", {}).get("object", {}).get("charges", {}).get("data") or [{}]
+        )[0]
+        metadata = payment_provider_data.get("data", {}).get("object", {}).get("metadata", {})
+        billing_details = charge_details.get("billing_details", {})
+        billing_address = billing_details.get("address") or {}
+
+        data.append(
+            {
+                "Contributor": billing_details.get("name") or "Unknown",
+                "Amount": contribution.amount,
+                "Donor Selected Amount": metadata.get("donor_selected_amount"),
+                "Agreed to Pay Fees": metadata.get("agreed_to_pay_fees"),
+                "Frequency": contribution.interval,
+                "Payment Received Date": contribution.created,
+                "Payment status": contribution.status,
+                "Address": ", ".join(
+                    [
+                        billing_address.get(address_key)
+                        for address_key in address_keys
+                        if billing_address.get(address_key)
+                    ]
+                ),
+                "Email": billing_details.get("email"),
+                "Phone": billing_details.get("phone"),
+                "Page URL": metadata.get("referer"),
+            }
+        )
+
+    with StringIO() as csv_file:
+        csv_writer = csv.DictWriter(csv_file, quoting=csv.QUOTE_ALL, fieldnames=columns)
+        csv_writer.writeheader()
+        csv_writer.writerows(data)
+        return csv_file.getvalue()

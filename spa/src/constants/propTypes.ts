7424b19b0014af39d9b36b54625f75aa3bbddb8e
Chris Klimas <chris.klimas@fundjournalism.org>
2022-11-30 11:02:29 -0500

    DEV-2864: Add prop type for optional page, fix logic on back button

--- spa/src/constants/propTypes.ts
+++ spa/src/constants/propTypes.ts
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types';
 
-export const PagePropTypes = {
+export const PartialPagePropTypes = {
   name: PropTypes.string,
   revenue_program: PropTypes.shape({
     slug: PropTypes.string
-  }).isRequired,
+  }),
   payment_provider: PropTypes.shape({
     stripe_verified: PropTypes.bool
-  }).isRequired,
+  }),
   slug: PropTypes.string,
   published_date: PropTypes.string
+};
+
+export const PagePropTypes = {
+  ...PartialPagePropTypes,
+  revenue_program: PartialPagePropTypes.revenue_program.isRequired,
+  payment_provider: PartialPagePropTypes.payment_provider.isRequired
 };
 
 export const UserPropTypes = {

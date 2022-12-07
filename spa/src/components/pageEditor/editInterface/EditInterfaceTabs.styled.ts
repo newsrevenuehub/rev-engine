38086ed23ed0ac63dc0148292c35b3d6029125c7
Chris Klimas <chris.klimas@fundjournalism.org>
2022-12-07 08:50:53 -0500

    DEV-2761: Add clarifying comment

--- spa/src/components/pageEditor/editInterface/EditInterfaceTabs.styled.ts
+++ spa/src/components/pageEditor/editInterface/EditInterfaceTabs.styled.ts
@@ -5,5 +5,6 @@ export const Tabs = styled(BaseTabs)`
   background-color: ${({ theme }) => theme.colors.white};
   position: sticky;
   top: 0;
+  /* To appear above the tab content, which has the default z-index. */
   z-index: 1;
 `;

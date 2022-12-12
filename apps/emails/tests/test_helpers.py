fbf166ba0105186ad0b0d8b60770084f670fcdf3
Guilherme Goes Mendonca <9381325+GuiMend@users.noreply.github.com>
2022-12-12 17:12:15 -0300

    update py tests

--- apps/emails/tests/test_helpers.py
+++ apps/emails/tests/test_helpers.py
@@ -1,40 +1,26 @@
 import datetime
 
-from django.test import SimpleTestCase
-
+import pytest
 import pytz
 
 from apps.emails.helpers import convert_to_timezone_formatted
 
 
-class HelperTests(SimpleTestCase):
-    def test_convert_to_timezone_formatted_with_custom_format_string(self):
-        test_date = datetime.datetime(2022, 12, 6)
-        self.assertEqual(test_date.strftime("%m-%d-%y %H:%M %Z"), "12-06-22 00:00 ")
+@pytest.mark.parametrize(
+    "timezone, fmt, isUTC, expect",
+    [
+        ("America/New_York", "%d/%m/%Y %H:%M %Z", False, "05/12/2022 19:00 EST"),
+        ("America/New_York", None, False, "12-05-22 19:00 EST"),
+        ("America/New_York", None, True, "12-05-22 19:00 EST"),
+        (None, None, False, "12-06-22 00:00 UTC"),
+    ],
+)
+def test__convert_to_timezone_formatted(timezone, fmt, isUTC, expect):
+    test_date = datetime.datetime(2022, 12, 6)
 
-        tz_date = convert_to_timezone_formatted(test_date, "America/New_York", "%d/%m/%Y %H:%M %Z")
-        self.assertEqual(tz_date, "05/12/2022 19:00 EST")
+    if isUTC:
+        test_date = test_date.replace(tzinfo=pytz.UTC)
+    assert test_date.strftime("%m-%d-%y %H:%M %Z") == "12-06-22 00:00" + " UTC" if isUTC else " "
 
-    def test_convert_to_timezone_formatted_with_selected_timezone_and_date_without_timezone_info(self):
-        test_date = datetime.datetime(2022, 12, 6)
-        self.assertEqual(test_date.strftime("%m-%d-%y %H:%M %Z"), "12-06-22 00:00 ")
-
-        # Even with test_date not having timezone information, `convert_to_timezone_formatted` assumes it's UTC
-        tz_date = convert_to_timezone_formatted(test_date, "America/New_York")
-        self.assertEqual(tz_date, "12-05-22 19:00 EST")
-
-    def test_convert_to_timezone_formatted_with_selected_timezone_and_date_with_UTC_timezone(self):
-        test_date = datetime.datetime(2022, 12, 6)
-        utc = test_date.replace(tzinfo=pytz.UTC)
-        self.assertEqual(utc.strftime("%m-%d-%y %H:%M %Z"), "12-06-22 00:00 UTC")
-
-        tz_date = convert_to_timezone_formatted(utc, "America/New_York")
-        self.assertEqual(tz_date, "12-05-22 19:00 EST")
-
-    def test_convert_to_timezone_formatted_without_selected_timezone(self):
-        test_date = datetime.datetime(2022, 12, 6)
-        self.assertEqual(test_date.strftime("%m-%d-%y %H:%M %Z"), "12-06-22 00:00 ")
-
-        tz_date = convert_to_timezone_formatted(test_date)
-        # Should be the same as the "timezone.get_current_timezone()" is UTC
-        self.assertEqual(tz_date, "12-06-22 00:00 UTC")
+    tz_date = convert_to_timezone_formatted(test_date, timezone, fmt)
+    assert tz_date == expect

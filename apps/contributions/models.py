63c3cd958b0e4208b07c10f9d30b483f9b27d133
benjaminEwhite <960351+benjaminEwhite@users.noreply.github.com>
2022-12-20 14:37:48 -0500

    add None as default value

--- apps/contributions/models.py
+++ apps/contributions/models.py
@@ -220,8 +220,8 @@ class Contribution(IndexedTimeStampedModel, RoleAssignmentResourceModelMixin):
         previous = self.__class__.objects.filter(pk=self.pk).first()
         logger.info(
             "`Contribution.save` called. Existing contribution has id: %s and provider_payment_method_id: %s \n The save value for provider_payment_method_id is %s",
-            getattr(previous, "id"),
-            getattr(previous, "provider_payment_method_id"),
+            getattr(previous, "id", None),
+            getattr(previous, "provider_payment_method_id", None),
             self.provider_payment_method_id,
         )
         # this is failing because provider_payment_method_id is blank at time webhook gets called

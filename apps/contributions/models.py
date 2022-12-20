cd4018bd76915db081625cc56a8b4375402edde2
benjaminEwhite <960351+benjaminEwhite@users.noreply.github.com>
2022-12-20 12:54:02 -0500

    add verbose log to debug

--- apps/contributions/models.py
+++ apps/contributions/models.py
@@ -218,7 +218,12 @@ class Contribution(IndexedTimeStampedModel, RoleAssignmentResourceModelMixin):
                 logger.info("Something went wrong sending Slack notification")
         # Check if we should update stripe payment method details
         previous = self.__class__.objects.filter(pk=self.pk).first()
-        logger.info("`Contribution.save` called with %s, %s", previous, self.provider_payment_method_id)
+        logger.info(
+            "`Contribution.save` called. Existing contribution has id: %s and provider_payment_method_id: %s \n The save value for provider_payment_method_id is %s",
+            getattr(previous, "id"),
+            self.provider_payment_method_id,
+            self.provider_payment_method_id,
+        )
         # this is failing because provider_payment_method_id is blank at time webhook gets called
         if (
             (previous and previous.provider_payment_method_id != self.provider_payment_method_id)

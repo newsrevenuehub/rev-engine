4d1cec7667e9a552071ab6c4a8b89d4a729e8575
Leo Alves <leo.dcfa@gmail.com>
2022-12-21 05:57:25 +1000

    DEV-3039: add migration to remove slack tables and delete most files prior to app removal

--- apps/slack/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from solo.admin import SingletonModelAdmin
-
-from apps.slack.models import HubSlackIntegration, OrganizationSlackIntegration
-
-
-admin.site.register(HubSlackIntegration, SingletonModelAdmin)
-admin.site.register(OrganizationSlackIntegration)

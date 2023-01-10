1c0778e81b830e45561a93f86365822006425093
Guilherme Goes Mendonca <9381325+GuiMend@users.noreply.github.com>
2023-01-10 18:24:00 -0300

    remove unused py test

--- apps/common/tests/test_views.py
+++ apps/common/tests/test_views.py
@@ -1,31 +1,13 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase, override_settings
+from django.test import TestCase
 from django.urls import reverse
 
-from apps.common.models import SocialMeta
 from apps.organizations.tests.factories import OrganizationFactory, RevenueProgramFactory
 from apps.pages.tests.factories import StyleFactory
 from revengine.views import SAFE_ADMIN_SELECT_ACCESSOR_METHODS, SAFE_ADMIN_SELECT_PARENTS
 
 
 user_model = get_user_model()
-
-
-@override_settings(ALLOWED_HOSTS=["my-test.test-domain.com", "testserver"])
-class ReactAppViewTestCase(TestCase):
-    def setUp(self):
-        self.revenue_program = RevenueProgramFactory(name="My Test", slug="my-test")
-        SocialMeta.objects.create(
-            title="title", description="description", url="https://example.com", revenue_program=self.revenue_program
-        )
-
-    def test_page_title_includes_rev_program_name_when_subdomain(self):
-        response = self.client.get(reverse("index"), HTTP_HOST=f"{self.revenue_program.slug}.test-domain.com")
-        self.assertContains(response, f"<title>Join | {self.revenue_program.name}</title>")
-
-    def test_page_title_is_default_when_not_subdomain(self):
-        response = self.client.get(reverse("index"))
-        self.assertContains(response, "<title>RevEngine</title>")
 
 
 class AdminSelectOptionsTest(TestCase):

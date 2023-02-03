daa3440eaa71965dbe4a5bffa12367d0b44bb64e
Chris Klimas <chris.klimas@fundjournalism.org>
2023-02-03 09:15:17 -0500

    DEV-2857: Handle whitespace in URLs consistently

--- spa/src/utilities/isValidUrl.ts
+++ spa/src/utilities/isValidUrl.ts
@@ -3,6 +3,13 @@ export function isValidUrl(value: string, allowEmpty?: boolean) {
     return true;
   }
 
+  // Some browsers allow whitespace when creating URL objects (and automatically
+  // encode them), others don't. We want to block it universally.
+
+  if (/\s/.test(value)) {
+    return false;
+  }
+
   try {
     new URL(value);
     return true;

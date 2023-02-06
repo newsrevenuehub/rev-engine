361234ec3c8ac18443fc2e86f2882983cf43b757
Guilherme Mendonca <9381325+GuiMend@users.noreply.github.com>
2023-02-06 11:11:26 -0300

    DEV-2314: Update cypress + jest tests

---
+++ spa/src/components/errors/ChunkErrorBoundary.test.tsx
@@ -0,0 +1,71 @@
+import { render, screen } from '@testing-library/react';
+import { ReactNode } from 'react';
+
+import ChunkErrorBoundary from './ChunkErrorBoundary';
+import logout from 'components/authentication/logout';
+import userEvent from '@testing-library/user-event';
+
+jest.mock('components/authentication/logout');
+
+const Child = () => {
+  return <div>mock-child</div>;
+};
+
+const ChildWithError = () => {
+  throw new Error();
+};
+
+describe('ChunkErrorBoundary', () => {
+  function tree(children: ReactNode) {
+    return render(<ChunkErrorBoundary>{children}</ChunkErrorBoundary>);
+  }
+
+  it('should render children', () => {
+    tree(<Child />);
+
+    expect(screen.getByText('mock-child')).toBeInTheDocument();
+  });
+
+  describe('Error', () => {
+    it('should render error message', () => {
+      tree(<ChildWithError />);
+
+      expect(screen.getByText(/We've encountered a problem!/i)).toBeInTheDocument();
+      expect(screen.getByText(/Click below to reload the page/i)).toBeInTheDocument();
+    });
+
+    it('should render reload button', () => {
+      tree(<ChildWithError />);
+
+      expect(screen.getByRole('button', { name: /refresh/i })).toBeEnabled();
+    });
+
+    it('should reload window if reload button clicked', () => {
+      const reload = jest.fn();
+      Object.defineProperty(window, 'location', {
+        value: { reload }
+      });
+
+      tree(<ChildWithError />);
+
+      expect(reload).not.toBeCalled();
+      userEvent.click(screen.getByRole('button', { name: /refresh/i }));
+      expect(reload).toBeCalledTimes(1);
+    });
+
+    it('should render Sign out button', () => {
+      tree(<ChildWithError />);
+
+      expect(screen.getByTestId('error-sign-out')).toBeInTheDocument();
+    });
+
+    it('should call logout if Sign out button clicked', () => {
+      const logoutMock = logout as jest.Mock;
+      tree(<ChildWithError />);
+
+      expect(logoutMock).not.toBeCalled();
+      userEvent.click(screen.getByTestId('error-sign-out'));
+      expect(logoutMock).toBeCalledTimes(1);
+    });
+  });
+});

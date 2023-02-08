906bfae8a6a559f2e854701cbce3494ba352e2df
Leo Alves <leo.dcfa@gmail.com>
2023-02-08 16:22:40 +1000

    DEV-3146: fix error user allowed to reject/accept non-flagged contribution

--- apps/contributions/admin.py
+++ apps/contributions/admin.py
@@ -148,6 +148,7 @@ class ContributionAdmin(RevEngineBaseAdmin, CompareVersionAdmin):
                 f"Cannot {action} a non-flagged Contribution.",
                 messages.ERROR,
             )
+            return
 
         succeeded = 0
         failed = []

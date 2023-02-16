0f85d1f4914325d64edb7bf599922ca4c580f428
Benjamin White <ben.white@fundjournalism.org>
2023-02-16 12:15:18 -0500

    unit test new HasFlaggedAccessToMailChimp permission

--- apps/api/tests/test_permissions.py
+++ apps/api/tests/test_permissions.py
@@ -1,6 +1,11 @@
 import pytest
+import pytest_cases
+from rest_framework.test import APIRequestFactory
 
-from apps.api.permissions import HasFlaggedAccessToContributionsApiResource
+from apps.api.permissions import (
+    HasFlaggedAccessToContributionsApiResource,
+    HasFlaggedAccessToMailChimp,
+)
 from apps.common.tests.test_resources import AbstractTestCase
 
 
@@ -10,3 +15,21 @@ class TestHasFlaggedAccessToContributionsApiResource(AbstractTestCase):
         self._set_up_default_feature_flags()
         t = HasFlaggedAccessToContributionsApiResource()
         assert isinstance(str(t), str)
+
+
+@pytest.mark.django_db
+class TestHasFlaggedAccessToMailChimp:
+    @pytest_cases.parametrize(
+        "user",
+        (
+            pytest_cases.fixture_ref("superuser"),
+            pytest_cases.fixture_ref("hub_admin_user"),
+            pytest_cases.fixture_ref("org_user_free_plan"),
+            pytest_cases.fixture_ref("rp_user"),
+        ),
+    )
+    def test_when_flag_set_to_everyone(self, user, default_feature_flags):
+        factory = APIRequestFactory()
+        request = factory.get("/")
+        request.user = user
+        assert HasFlaggedAccessToMailChimp().has_permission(request, None) is True

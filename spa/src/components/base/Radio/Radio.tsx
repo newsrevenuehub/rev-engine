57ebdd4c31dc0c3a9147e8a1f45835d28f9f0ce3
Chris Klimas <chris.klimas@fundjournalism.org>
2023-02-22 10:41:44 -0500

    DEV-2979: Use library icon for unchecked state

--- spa/src/components/base/Radio/Radio.tsx
+++ spa/src/components/base/Radio/Radio.tsx
@@ -2,7 +2,7 @@ import { Radio as MuiRadio, RadioProps as MuiRadioProps } from '@material-ui/cor
 import styled from 'styled-components';
 // See https://create-react-app.dev/docs/adding-images-fonts-and-files/#adding-svgs
 import { ReactComponent as CheckedIcon } from './checked.svg';
-import { ReactComponent as UncheckedIcon } from './unchecked.svg';
+import { RadioButtonUnchecked } from '@material-ui/icons';
 
 export type RadioProps = MuiRadioProps;
 
@@ -21,7 +21,7 @@ const StyledMuiRadio = styled(MuiRadio)`
 `;
 
 export function Radio(props: RadioProps) {
-  return <StyledMuiRadio checkedIcon={<CheckedIcon />} icon={<UncheckedIcon />} {...props} />;
+  return <StyledMuiRadio checkedIcon={<CheckedIcon />} icon={<RadioButtonUnchecked />} {...props} />;
 }
 
 export default Radio;

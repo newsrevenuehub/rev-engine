9241ff7e16e6b5b99e550926848669645b8dc1ac
Leo Alves <leo.dcfa@gmail.com>
2023-03-03 16:43:13 +1000

    DEV-3171: re-add changes

--- apps/contributions/stripe_contributions_provider.py
+++ apps/contributions/stripe_contributions_provider.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import logging
+from functools import cached_property
 
 from django.conf import settings
 from django.core.cache import caches
@@ -177,8 +178,10 @@ class StripeContributionsProvider:
         self.email_id = email_id
         self.stripe_account_id = stripe_account_id
 
+    @cached_property
     def customers(self):
         """
+        Cached Property.
         Gets all the customers associated with an email for a given stripe account
 
         Returns:

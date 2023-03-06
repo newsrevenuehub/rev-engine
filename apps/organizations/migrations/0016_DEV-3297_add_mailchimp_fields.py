a510c48b2fdd70c99ef883e59e48509c10289c5d
Benjamin White <ben.white@fundjournalism.org>
2023-03-06 13:51:28 -0500

    fix migration

--- apps/organizations/migrations/0016_DEV-3297_add_mailchimp_fields.py
+++ apps/organizations/migrations/0016_DEV-3297_add_mailchimp_fields.py
@@ -18,6 +18,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="revenueprogram",
             name="mailchimp_server_prefix",
-            field=models.Text(blank=True, null=True),
+            field=models.TextField(blank=True, null=True),
         ),
     ]

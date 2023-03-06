6611b43ca764c11e6f957a67930b5324fd3e00f2
Benjamin White <ben.white@fundjournalism.org>
2023-03-06 13:49:32 -0500

    make it a textfield

--- apps/organizations/migrations/0016_DEV-3297_add_mailchimp_fields.py
+++ apps/organizations/migrations/0016_DEV-3297_add_mailchimp_fields.py
@@ -18,6 +18,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="revenueprogram",
             name="mailchimp_server_prefix",
-            field=models.CharField(blank=True, max_length=100, null=True),
+            field=models.Text(blank=True, null=True),
         ),
     ]

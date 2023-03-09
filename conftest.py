d1c0223a7b379a388fd8e4a1cf39f14f4c08135e
Guilherme Mendonca <9381325+GuiMend@users.noreply.github.com>
2023-03-09 09:10:36 -0300

    remove comment

--- conftest.py
+++ conftest.py
@@ -213,12 +213,6 @@ def free_plan_revenue_program():
     return RevenueProgramFactory(onboarded=True, organization=OrganizationFactory(free_plan=True))
 
 
-# TODO: uncomment when CORE plan implemented
-# @pytest.fixture
-# def core_plan_revenue_program():
-#     return RevenueProgramFactory(onboarded=True, organization=OrganizationFactory(core_plan=True))
-
-
 @pytest.fixture
 def plus_plan_revenue_program():
     return RevenueProgramFactory(onboarded=True, organization=OrganizationFactory(plus_plan=True))

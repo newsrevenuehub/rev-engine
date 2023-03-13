b12729e0776141f5279c9aa62a3ac3feac4f9fcf
Guilherme Mendonca <9381325+GuiMend@users.noreply.github.com>
2023-03-13 13:50:53 -0300

    DEV-3296: Add feature flag and connection notification

--- spa/src/hooks/useConnectMailchimp.tsx
+++ spa/src/hooks/useConnectMailchimp.tsx
@@ -1,15 +1,22 @@
 import { UseQueryResult } from '@tanstack/react-query';
 import queryString from 'query-string';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 
 import { NRE_MAILCHIMP_CLIENT_ID } from 'appSettings';
 import { MAILCHIMP_OAUTH_SUCCESS_ROUTE } from 'routes';
 import { EnginePlan } from './useContributionPage';
 import useUser from './useUser';
+import useFeatureFlags from './useFeatureFlags';
+import flagIsActiveForUser from 'utilities/flagIsActiveForUser';
+import { MAILCHIMP_INTEGRATION_ACCESS_FLAG_NAME } from 'constants/featureFlagConstants';
+import { useSnackbar } from 'notistack';
+import SystemNotification from 'components/common/SystemNotification';
+
+import usePreviousState from './usePreviousState';
 
 export type UnverifiedReason = '' | 'pending_verification' | 'past_due';
 
-export interface UseConnectMailchimpAccountResult {
+export interface UseConnectMailchimpResult {
   isLoading: UseQueryResult['isLoading'];
   isError: UseQueryResult['isError'];
   /**
@@ -26,10 +33,14 @@ export interface UseConnectMailchimpAccountResult {
   organizationPlan?: EnginePlan['name']; //'FREE' | 'CORE' | 'PLUS';
 }
 
-export default function useConnectMailchimpAccount(): UseConnectMailchimpAccountResult {
+export default function useConnectMailchimp(): UseConnectMailchimpResult {
   const BASE_URL = window.location.origin;
   const OAUTH_CALLBACK = `${BASE_URL}${MAILCHIMP_OAUTH_SUCCESS_ROUTE}`;
+  const { enqueueSnackbar } = useSnackbar();
   const { user, isError, isLoading } = useUser();
+  const { flags } = useFeatureFlags();
+  const hasMailchimpAccess = flagIsActiveForUser(MAILCHIMP_INTEGRATION_ACCESS_FLAG_NAME, flags);
+  const prevMailchimpConnection = usePreviousState(user?.revenue_programs?.[0]?.mailchimp_integration_connected);
 
   const sendUserToMailchimp = useCallback(() => {
     if (!NRE_MAILCHIMP_CLIENT_ID) {
@@ -44,13 +55,25 @@ export default function useConnectMailchimpAccount(): UseConnectMailchimpAccount
     })}`;
   }, [OAUTH_CALLBACK]);
 
+  useEffect(() => {
+    if (user?.revenue_programs?.[0]?.mailchimp_integration_connected && prevMailchimpConnection === false) {
+      enqueueSnackbar('You’ve successfully connected to Mailchimp! Your contributor data will sync automatically.', {
+        persist: true,
+        content: (key: string, message: string) => (
+          <SystemNotification id={key} message={message} header="Successfully Connected!" type="success" />
+        )
+      });
+    }
+  }, [enqueueSnackbar, prevMailchimpConnection, user?.revenue_programs]);
+
   // If the user is loading or errored, return that status.
   if (isLoading || isError) {
     return { isError, isLoading, connectedToMailchimp: false };
   }
 
-  // If the user has no revenue programs, no org or multiple orgs return that there's no action to take.
+  // If the user has no feature flag enabling mailchimp integration, has no revenue programs, no org or multiple orgs return that there's no action to take.
   if (
+    !hasMailchimpAccess ||
     user?.organizations?.length === 0 ||
     (user?.organizations?.length ?? 0) > 1 ||
     !user?.revenue_programs ||

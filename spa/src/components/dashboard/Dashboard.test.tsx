833f1681f96c31f376387e09abd4f6be6d08e7d7
Guilherme Mendonca <9381325+GuiMend@users.noreply.github.com>
2023-03-27 19:43:14 -0300

    add AudienceListModal test in Dashboard

--- spa/src/components/dashboard/Dashboard.test.tsx
+++ spa/src/components/dashboard/Dashboard.test.tsx
@@ -7,13 +7,16 @@ import Dashboard from './Dashboard';
 import { CONTENT_SLUG } from 'routes';
 
 import useConnectStripeAccount from 'hooks/useConnectStripeAccount';
+import useConnectMailchimp from 'hooks/useConnectMailchimp';
 import useFeatureFlags from 'hooks/useFeatureFlags';
 import useUser from 'hooks/useUser';
 
+jest.mock('components/common/Modal/AudienceListModal');
 jest.mock('elements/GlobalLoading');
 jest.mock('hooks/useFeatureFlags');
 jest.mock('hooks/useRequest');
 jest.mock('hooks/useUser');
+jest.mock('hooks/useConnectMailchimp');
 jest.mock('hooks/useConnectStripeAccount');
 jest.mock('react-router-dom', () => ({
   ...(jest.requireActual('react-router-dom') as object),
@@ -37,6 +40,7 @@ const useUserMockDefaults = {
 
 describe('Dashboard', () => {
   const useConnectStripeAccountMock = useConnectStripeAccount as jest.Mock;
+  const useConnectMailchimpMock = jest.mocked(useConnectMailchimp);
   const useFeatureFlagsMock = useFeatureFlags as jest.Mock;
   const useUserMock = useUser as jest.Mock;
   const useLocationMock = useLocation as jest.Mock;
@@ -44,6 +48,12 @@ describe('Dashboard', () => {
 
   beforeEach(() => {
     useSnackbarMock.mockReturnValue({ enqueueSnackbar: jest.fn(), closeSnackbar: jest.fn() });
+    useConnectMailchimpMock.mockReturnValue({
+      requiresAudienceSelection: false,
+      isLoading: false,
+      connectedToMailchimp: false,
+      isError: false
+    });
     useConnectStripeAccountMock.mockReturnValue({
       requiresVerification: false,
       displayConnectionSuccess: false,
@@ -92,6 +102,19 @@ describe('Dashboard', () => {
         })
       )
     );
+  });
+  it('should display Audience List modal if requiresAudienceSelection = true', () => {
+    useConnectMailchimpMock.mockReturnValue({
+      requiresAudienceSelection: true,
+      revenueProgram: 'mock-rp' as any,
+      isLoading: false,
+      connectedToMailchimp: false,
+      isError: false
+    });
+    render(<Dashboard />);
+    const audienceListModal = screen.getByTestId('mock-audience-list-modal');
+    expect(audienceListModal).toBeInTheDocument();
+    expect(audienceListModal).toHaveTextContent('mock-rp');
   });
   it('shows a loading status when Stripe account link status is loading', () => {
     useConnectStripeAccountMock.mockReturnValue({ isLoading: true });

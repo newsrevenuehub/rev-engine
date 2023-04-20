7d4ddffbd8f5d6c8265724280edf6ece466e5118
Guilherme Mendonca <9381325+GuiMend@users.noreply.github.com>
2023-04-20 15:41:58 -0300

    DEV-3442: Update files to TS and write tests

--- spa/src/hooks/useContributionPage/useContributionPage.types.ts
+++ spa/src/hooks/useContributionPage/useContributionPage.types.ts
@@ -96,6 +96,22 @@ export interface ContributionPageElement {
    * Internal ID of the element.
    */
   uuid: string;
+  /**
+   * User-visible name of the element.
+   */
+  displayName?: string;
+  /**
+   * User-visible description of the element.
+   */
+  description?: string;
+  /**
+   * Edit block is required?
+   */
+  required?: boolean;
+  /**
+   * Edit block is unique?
+   */
+  unique?: boolean;
 }
 
 export interface AmountElement extends ContributionPageElement {

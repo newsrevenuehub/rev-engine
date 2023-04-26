c99db334caa2b7df2c22a56bab5dc869916ad240
Benjamin White <ben.white@fundjournalism.org>
2023-04-26 10:33:07 -0400

    new rp post save handler

---
+++ apps/organizations/tests/test_signals.py
@@ -0,0 +1,44 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from apps.organizations.signals import revenue_program_post_save
+from apps.organizations.tests.factories import RevenueProgramFactory
+
+
+@pytest.mark.django_db
+class TestRevenueProgramPostSaveHandler:
+    @pytest.mark.parametrize(
+        "make_rp_kwargs,expect_task_called",
+        (
+            ({"mailchimp_list_id": None}, False),
+            ({"mailchimp_list_id": "some-id"}, True),
+        ),
+    )
+    def test_when_new_instance(self, make_rp_kwargs, expect_task_called, mocker):
+        rp = RevenueProgramFactory.build(**make_rp_kwargs)
+        setup_mc_task = mocker.patch("apps.organizations.signals.setup_mailchimp_entities_for_rp_mailing_list")
+        revenue_program_post_save(sender=MagicMock(), instance=rp, created=True)
+        if expect_task_called:
+            setup_mc_task.delay.assert_called_once_with(rp.id)
+        else:
+            assert not setup_mc_task.delay.called
+
+    @pytest.mark.parametrize(
+        "update_rp_kwargs,expect_task_called",
+        (
+            ({"mailchimp_list_id": None}, False),
+            ({"mailchimp_list_id": "some-id"}, True),
+        ),
+    )
+    def test_when_existing_instance(self, update_rp_kwargs, expect_task_called, mocker, revenue_program):
+        setup_mc_task = mocker.patch("apps.organizations.signals.setup_mailchimp_entities_for_rp_mailing_list")
+        update_fields = set()
+        for k, v in update_rp_kwargs.items():
+            setattr(revenue_program, k, v)
+            update_fields.add(k)
+        revenue_program.save(update_fields=update_fields)
+        if expect_task_called:
+            setup_mc_task.delay.assert_called_once_with(revenue_program.id)
+        else:
+            assert not setup_mc_task.delay.called

06b6c441e30094d080e83a63a8408065adea74c4
Benjamin White <ben.white@fundjournalism.org>
2023-05-09 17:10:01 -0400

    fix test

--- apps/google_cloud/tests/test_pubsub.py
+++ apps/google_cloud/tests/test_pubsub.py
@@ -9,16 +9,15 @@ def test_encodes_string_to_bytes():
     assert isinstance(Message("irrelevant").data, bytes)
 
 
-def test_publishes_to_google_cloud_pub_sub(monkeypatch):
-    google_cloud_project = "project"
+def test_publishes_to_google_cloud_pub_sub(mocker, settings):
     publisher_client = MagicMock()
-    monkeypatch.setattr("django.conf.settings.GOOGLE_CLOUD_PROJECT", google_cloud_project)
-    monkeypatch.setattr("apps.google_cloud.pubsub.pubsub_v1.PublisherClient", publisher_client)
+    settings.GOOGLE_CLOUD_PROJECT = "project"
+    mocker.patch("apps.google_cloud.pubsub.pubsub_v1.PublisherClient", publisher_client)
     topic = "topic"
     expected = "result"
     future = futures.Future()
     future.set_result(expected)
-    full_topic_path = f"projects/{google_cloud_project}/topics/{topic}"
+    full_topic_path = f"projects/{settings.GOOGLE_CLOUD_PROJECT}/topics/{topic}"
     publisher = Publisher()
     publisher.client = publisher_client
     publisher_client.topic_path.return_value = full_topic_path
@@ -26,5 +25,5 @@ def test_publishes_to_google_cloud_pub_sub(monkeypatch):
     message = Message(data="some message")
     result = publisher.publish(topic, message)
     assert expected == result
-    publisher_client.topic_path.called_with(google_cloud_project, topic)
-    publisher_client.publish.called_with(full_topic_path, message.data)
+    publisher_client.topic_path.assert_called_once_with(settings.GOOGLE_CLOUD_PROJECT, topic)
+    publisher_client.publish.assert_called_once_with(full_topic_path, message.data)

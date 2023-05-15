c81ce50ca1dc58c23f56b02f2958acb3cb0f9fe8
Guilherme Mendonca <9381325+GuiMend@users.noreply.github.com>
2023-05-15 19:20:41 -0300

    DEV-3177: Add IntegrationCardHeader and update components

---
+++ spa/src/components/common/IntegrationCard/IntegrationCardHeader/IntegrationCardHeader.styled.ts
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+import LaunchMui from '@material-ui/icons/Launch';
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  position: relative;
+`;
+
+export const Image = styled.img`
+  height: 55px;
+  width: 55px;
+  border-radius: ${(props) => props.theme.muiBorderRadius.lg};
+`;
+
+export const Required = styled.p`
+  margin: 0;
+  position: absolute;
+  font-weight: 500;
+  top: 0;
+  right: 0;
+  font-size: ${(props) => props.theme.fontSizesUpdated.sm};
+  color: ${(props) => props.theme.colors.error.primary};
+`;
+
+export const CornerMessage = styled.div`
+  margin: 0;
+  position: absolute;
+  font-weight: 500;
+  top: 0;
+  right: 0;
+  font-size: ${(props) => props.theme.fontSizesUpdated.sm};
+  color: ${(props) => props.theme.colors.topbarBackground};
+`;
+
+export const Site = styled.a`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  font-size: ${(props) => props.theme.fontSizesUpdated.sm};
+  color: ${(props) => props.theme.colors.muiGrey[600]};
+  cursor: pointer;
+
+  &:hover {
+    color: ${(props) => props.theme.colors.muiGrey[600]};
+    text-decoration: none;
+  }
+`;
+
+export const LaunchIcon = styled(LaunchMui)`
+  && {
+    color: ${(props) => props.theme.colors.muiGrey[600]};
+    margin-left: 5px;
+    height: 16px;
+    width: 16px;
+  }
+`;
+
+export const Title = styled.p`
+  font-size: ${(props) => props.theme.fontSizesUpdated.lg};
+  color: ${(props) => props.theme.colors.muiGrey[900]};
+  font-weight: 500;
+  margin: 0;
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+`;
---
+++ spa/src/components/common/IntegrationCard/IntegrationCardHeader/IntegrationCardHeader.tsx
@@ -0,0 +1,70 @@
+import PropTypes, { InferProps } from 'prop-types';
+
+import {
+  Header,
+  Image,
+  Required,
+  CornerMessage,
+  Site,
+  LaunchIcon,
+  Title,
+  TitleWrapper
+} from './IntegrationCardHeader.styled';
+
+export type IntegrationCardHeaderProps = InferProps<typeof IntegrationCardPropTypes>;
+
+const IntegrationCardHeader = ({
+  isActive = false,
+  enableCornerMessage = false,
+  isRequired,
+  cornerMessage,
+  title,
+  image,
+  site
+}: IntegrationCardHeaderProps) => {
+  const showCornerMessage = (isRequired || !!cornerMessage) && enableCornerMessage!;
+
+  const renderCornerMessage = (showMessage: boolean) => {
+    if (!showMessage) return null;
+    if (isRequired) {
+      return <Required>*Required</Required>;
+    }
+    if (cornerMessage && !isActive) {
+      return <CornerMessage>{cornerMessage}</CornerMessage>;
+    }
+    return null;
+  };
+
+  return (
+    <Header>
+      <Image src={image} aria-label={`${title} logo`} />
+      <TitleWrapper>
+        <Title>{title}</Title>
+        <div style={{ display: 'flex' }}>
+          <Site href={site.url} rel="noopener noreferrer" target="_blank">
+            {site.label}
+            <LaunchIcon />
+          </Site>
+        </div>
+      </TitleWrapper>
+      {renderCornerMessage(showCornerMessage)}
+    </Header>
+  );
+};
+
+const IntegrationCardPropTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  isRequired: PropTypes.bool.isRequired,
+  cornerMessage: PropTypes.node,
+  site: PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+  }).isRequired,
+  isActive: PropTypes.bool,
+  enableCornerMessage: PropTypes.bool
+};
+
+IntegrationCardHeader.propTypes = IntegrationCardPropTypes;
+
+export default IntegrationCardHeader;

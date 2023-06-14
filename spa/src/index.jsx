34514d759abafb4dc353f641699b4eb04b60dcdb
Chris Klimas <chris.klimas@fundjournalism.org>
2023-06-14 16:03:46 -0400

    Bootstraps app

--- spa/src/index.jsx
+++ spa/src/index.jsx
@@ -5,6 +5,9 @@ import App from './App';
 // Fontawesome config for CSP`
 import { config as fontawesomeConfig } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+
+window.global ||= window;
+
 // import 'node_modules/@fortawesome/fontawesome-svg-core/styles.css';
 // Load css via webpack, not via inline styles (fontawesome default)
 fontawesomeConfig.autoAddCss = false;
--- spa/vite.config.js
+++ spa/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 const config = defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {}
+  plugins: [react(), svgr(), tsconfigPaths()],
+  server: {
+    open: true,
+    port: 3000
+  }
 });
 
 if (!process.env.REACT_APP_NO_PROXY) {

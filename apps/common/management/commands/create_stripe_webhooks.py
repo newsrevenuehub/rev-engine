d8084d208df5bf0f74b2faad90274721d07cacc6
Benjamin White <ben.white@fundjournalism.org>
2023-07-18 11:28:47 -0400

    cleanup

--- apps/common/management/commands/create_stripe_webhooks.py
+++ apps/common/management/commands/create_stripe_webhooks.py
@@ -11,31 +11,18 @@ class Command(BaseCommand):
         parser.add_argument("url-contributions", nargs="?", type=str)
         parser.add_argument("url-upgrades", nargs="?", type=str)
 
-    def get_contributions_endpoint_url(self, options: dict) -> str:
-        if options.get("url-contributions", False):
-            return options["url-contributions"]
-        else:
-            return settings.SITE_URL + reverse("stripe-webhooks-contributions")
-
-    def get_upgrades_endpoint_url(self, options: dict) -> str:
-        if options.get("url-upgrades", False):
-            return options["url-upgrades"]
-        else:
-            return settings.SITE_URL + reverse("organization-handle-stripe-webhook")
-
     def handle(self, *args, **options):
         contributions_stripe_secret = create_stripe_webhook(
             api_key=settings.STRIPE_LIVE_SECRET_KEY_CONTRIBUTIONS
             if (live := options["live"])
             else settings.STRIPE_TEST_SECRET_KEY_CONTRIBUTIONS,
             enabled_events=settings.STRIPE_WEBHOOK_EVENTS_FOR_CONTRIBUTIONS,
-            webhook_url=self.get_contributions_endpoint_url(options),
+            webhook_url=options.get("url-contributions", settings.SITE_URL + reverse("stripe-webhooks-contributions")),
         )
         self.stdout.write(self.style.WARNING("For contributions, wh_sec = %s" % contributions_stripe_secret))
-
         upgrade_stripe_secret = create_stripe_webhook(
             api_key=settings.STRIPE_LIVE_SECRET_KEY_UPGRADES if live else settings.STRIPE_TEST_SECRET_KEY_UPGRADES,
             enabled_events=settings.STRIPE_WEBHOOK_EVENTS_FOR_UPGRADES,
-            webhook_url=self.get_upgrades_endpoint_url(options),
+            webhook_url=options.get("url-upgrades", settings.SITE_URL + reverse("organization-handle-stripe-webhook")),
         )
         self.stdout.write(self.style.WARNING("For self-upgrade, wh_sec = %s" % upgrade_stripe_secret))

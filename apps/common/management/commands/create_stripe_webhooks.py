651c156a93ae9e2b56e6a8fce352da24dfdaa478
Benjamin White <ben.white@fundjournalism.org>
2023-07-18 12:09:07 -0400

    improve interface

--- apps/common/management/commands/create_stripe_webhooks.py
+++ apps/common/management/commands/create_stripe_webhooks.py
@@ -8,8 +8,18 @@ from apps.common.utils import create_stripe_webhook
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("live", nargs="?", type=bool, default=False)
-        parser.add_argument("url-contributions", nargs="?", type=str)
-        parser.add_argument("url-upgrades", nargs="?", type=str)
+        parser.add_argument(
+            "url-contributions",
+            nargs="?",
+            type=str,
+            default=settings.SITE_URL + reverse("stripe-webhooks-contributions"),
+        )
+        parser.add_argument(
+            "url-upgrades",
+            nargs="?",
+            type=str,
+            default=settings.SITE_URL + reverse("organization-handle-stripe-webhook"),
+        )
 
     def handle(self, *args, **options):
         contributions_stripe_secret = create_stripe_webhook(
@@ -17,12 +27,12 @@ class Command(BaseCommand):
             if (live := options["live"])
             else settings.STRIPE_TEST_SECRET_KEY_CONTRIBUTIONS,
             enabled_events=settings.STRIPE_WEBHOOK_EVENTS_FOR_CONTRIBUTIONS,
-            webhook_url=options.get("url-contributions", settings.SITE_URL + reverse("stripe-webhooks-contributions")),
+            webhook_url=options["url-contributions"],
         )
         self.stdout.write(self.style.WARNING("For contributions, wh_sec = %s" % contributions_stripe_secret))
         upgrade_stripe_secret = create_stripe_webhook(
             api_key=settings.STRIPE_LIVE_SECRET_KEY_UPGRADES if live else settings.STRIPE_TEST_SECRET_KEY_UPGRADES,
             enabled_events=settings.STRIPE_WEBHOOK_EVENTS_FOR_UPGRADES,
-            webhook_url=options.get("url-upgrades", settings.SITE_URL + reverse("organization-handle-stripe-webhook")),
+            webhook_url=options["url-upgrades"],
         )
         self.stdout.write(self.style.WARNING("For self-upgrade, wh_sec = %s" % upgrade_stripe_secret))

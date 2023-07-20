3f1d282834f492d66d0a71f56546ade38d187e6a
Benjamin White <ben.white@fundjournalism.org>
2023-07-20 09:50:10 -0400

    add comment explaining usage

--- apps/organizations/models.py
+++ apps/organizations/models.py
@@ -122,6 +122,9 @@ class OrganizationManager(models.Manager):
 
 
 class Organization(IndexedTimeStampedModel):
+    # used in self-upgrade flow. Stripe checkouts can have associated client-reference-id, and we set that
+    # to the value of an org.uuid so that we can look up the org in the self-upgrade flow, which is triggered
+    # by stripe webhooks.
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=255, unique=True)
     plan_name = models.CharField(choices=Plans.choices, max_length=10, default=Plans.FREE)

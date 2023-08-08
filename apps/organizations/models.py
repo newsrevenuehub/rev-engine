4a5ff17431204f5550e256f394b05f52863d3dbc
Guilherme Mendonca <9381325+GuiMend@users.noreply.github.com>
2023-08-08 10:42:18 -0300

    update default value and simplify property

--- apps/organizations/models.py
+++ apps/organizations/models.py
@@ -293,7 +293,7 @@ class TransactionalEmailStyle:
 
     is_default_logo: bool = False
     logo_url: str = None
-    logo_alt_text: str = None
+    logo_alt_text: str = ""
     header_color: str = None
     header_font: str = None
     body_font: str = None
@@ -982,12 +982,12 @@ class RevenueProgram(IndexedTimeStampedModel):
             return HubDefaultEmailStyle
         else:
             _style = AttrDict(page.styles.styles if page.styles else {})
+            custom_logo_alt_text = page.header_logo_alt_text or ""
+
             return TransactionalEmailStyle(
                 is_default_logo=not page.header_logo,
                 logo_url=page.header_logo.url if page.header_logo else HubDefaultEmailStyle.logo_url,
-                logo_alt_text=page.header_logo_alt_text or ""
-                if page.header_logo
-                else HubDefaultEmailStyle.logo_alt_text,
+                logo_alt_text=custom_logo_alt_text if page.header_logo else HubDefaultEmailStyle.logo_alt_text,
                 header_color=_style.colors.cstm_mainHeader or None,
                 header_font=_style.font.heading or None,
                 body_font=_style.font.body or None,

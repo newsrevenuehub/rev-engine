4c3829acb91b485a5e6894a59983232132a36abf
Chris Klimas <chris.klimas@fundjournalism.org>
2023-08-14 10:59:13 -0400

    DEV-1748: Add link to customize Core upgrade prompt

--- spa/src/components/content/customize/CustomizeCoreUpgradePrompt/CustomizeCoreUpgradePrompt.tsx
+++ spa/src/components/content/customize/CustomizeCoreUpgradePrompt/CustomizeCoreUpgradePrompt.tsx
@@ -1,14 +1,34 @@
 import { PRICING_URL } from 'constants/helperUrls';
 import PropTypes, { InferProps } from 'prop-types';
-import { Header, LearnMoreButton, Root, Text, Icon, ButtonWrapper } from './CustomizeCoreUpgradePrompt.styled';
+import { Header, Button, Root, Text, Icon, ButtonWrapper } from './CustomizeCoreUpgradePrompt.styled';
+import { User } from 'hooks/useUser.types';
+import flagIsActiveForUser from 'utilities/flagIsActiveForUser';
+import { SELF_UPGRADE_ACCESS_FLAG_NAME } from 'constants/featureFlagConstants';
+import { Link } from 'react-router-dom';
+import { SETTINGS } from 'routes';
 
 const CustomizeCoreUpgradePromptPropTypes = {
-  onClose: PropTypes.func.isRequired
+  onClose: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired
 };
 
-export type CustomizeCoreUpgradePromptProps = InferProps<typeof CustomizeCoreUpgradePromptPropTypes>;
+export interface CustomizeCoreUpgradePromptProps extends InferProps<typeof CustomizeCoreUpgradePromptPropTypes> {
+  onClose: () => void;
+  user: User;
+}
 
-export function CustomizeCoreUpgradePrompt({ onClose }: CustomizeCoreUpgradePromptProps) {
+export function CustomizeCoreUpgradePrompt({ onClose, user }: CustomizeCoreUpgradePromptProps) {
+  const LooseButton = Button as any;
+  const upgradeButton = flagIsActiveForUser(SELF_UPGRADE_ACCESS_FLAG_NAME, user) ? (
+    <LooseButton component={Link} to={SETTINGS.SUBSCRIPTION}>
+      Upgrade
+    </LooseButton>
+  ) : (
+    <Button href={PRICING_URL} target="_blank">
+      Upgrade
+    </Button>
+  );
+
   return (
     <Root>
       <Icon />
@@ -20,12 +40,10 @@ export function CustomizeCoreUpgradePrompt({ onClose }: CustomizeCoreUpgradeProm
         </Text>
       </div>
       <ButtonWrapper>
-        <LearnMoreButton onClick={onClose} target="_blank" color="text">
+        <Button onClick={onClose} color="text">
           Close
-        </LearnMoreButton>
-        <LearnMoreButton href={PRICING_URL} target="_blank">
-          Upgrade
-        </LearnMoreButton>
+        </Button>
+        {upgradeButton}
       </ButtonWrapper>
     </Root>
   );

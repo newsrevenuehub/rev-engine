ee4da07b481d5651960698e147c9b4a0f2991863
Guilherme Mendonca <9381325+GuiMend@users.noreply.github.com>
2023-09-01 09:15:16 -0300

    add error

--- spa/src/utilities/addMiddleEllipsis.ts
+++ spa/src/utilities/addMiddleEllipsis.ts
@@ -2,13 +2,17 @@ export const MAX_LENGTH = 35;
 export const PRE_ELLIPSIS_SIZE = 20;
 export const POST_ELLIPSIS_SIZE = 10;
 
-// For this function to work properly, the "maxLength" should be greater than the "preEllipsisSize + … + postEllipsisSize"
 function addMiddleEllipsis(
   str: string,
   maxLength = MAX_LENGTH,
   preEllipsisSize = PRE_ELLIPSIS_SIZE,
   postEllipsisSize = POST_ELLIPSIS_SIZE
 ) {
+  // If the maxLength is greater than the MAX_LENGTH, then the preEllipsisSize and postEllipsisSize are ignored.
+  if (maxLength > MAX_LENGTH && maxLength < preEllipsisSize + postEllipsisSize + 1) {
+    throw new Error('The "maxLength" should be greater than the "preEllipsisSize + … + postEllipsisSize"');
+  }
+
   if (str.length > maxLength) {
     const preEllipsis = maxLength < MAX_LENGTH ? maxLength / 2 : preEllipsisSize;
     const postEllipsis = maxLength < MAX_LENGTH ? maxLength / 5 : postEllipsisSize;

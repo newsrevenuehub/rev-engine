a7836c6b6f6ad2c78c643212d35f527deb764eb8
Benjamin White <ben.white@fundjournalism.org>
2023-09-11 11:21:28 -0400

    remove text based locator

--- spa/src/components/common/Button/PublishButton/PublishModal/__mocks__/PublishModal.tsx
+++ spa/src/components/common/Button/PublishButton/PublishModal/__mocks__/PublishModal.tsx
@@ -6,7 +6,7 @@ export const PublishModal = ({ onClose, onPublish, open, slugError }: PublishMod
       <div data-testid="mock-publish-modal">
         <button onClick={() => onPublish({ slug: 'mock-slug' })}>onPublish</button>
         <button onClick={onClose}>PublishModal onClose</button>
-        <div>Slug error:{slugError?.[0]}</div>
+        <div>{slugError?.[0]}</div>
       </div>
     )}
   </>

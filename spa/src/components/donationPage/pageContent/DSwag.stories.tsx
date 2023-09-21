f513d7c6b5de6c0fa36585bfe82b738610f5811c
Chris Klimas <chris.klimas@fundjournalism.org>
2023-09-20 20:41:00 -0400

    DEV-3824: Re-enable swag element

---
+++ spa/src/components/donationPage/pageContent/DSwag.stories.tsx
@@ -0,0 +1,65 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { DonationPageContext } from '../DonationPage';
+import DSwag from './DSwag';
+
+export default {
+  component: DSwag,
+  title: 'Donation Page/DSwag'
+} as ComponentMeta<typeof DSwag>;
+
+const Template: ComponentStory<typeof DSwag> = (props) => (
+  <DonationPageContext.Provider
+    value={
+      {
+        amount: 100,
+        frequency: 'one_time',
+        page: {
+          currency: {
+            code: 'USD',
+            symbol: '$'
+          }
+        }
+      } as any
+    }
+  >
+    <ul style={{ listStyle: 'none', margin: 0, padding: 0 }}>
+      <DSwag {...props} />
+    </ul>
+  </DonationPageContext.Provider>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  element: {
+    content: {
+      swags: [
+        {
+          swagName: 'T-Shirt Size',
+          swagOptions: ['Small', 'Medium', 'Large']
+        }
+      ],
+      swagThreshold: 100
+    },
+    requiredFields: [],
+    type: 'DSwag',
+    uuid: 'mock-uuid'
+  }
+};
+
+export const BelowThreshold = Template.bind({});
+BelowThreshold.args = {
+  element: {
+    content: {
+      swags: [
+        {
+          swagName: 'T-Shirt Size',
+          swagOptions: ['Small', 'Medium', 'Large']
+        }
+      ],
+      swagThreshold: 101
+    },
+    requiredFields: [],
+    type: 'DSwag',
+    uuid: 'mock-uuid'
+  }
+};

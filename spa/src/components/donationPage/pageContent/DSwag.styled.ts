55be94598449aaa9ef3083c91549b0679407450d
Chris Klimas <chris.klimas@fundjournalism.org>
2023-09-20 20:41:00 -0400

    DEV-3824: Re-enable swag element

---
+++ spa/src/components/donationPage/pageContent/DSwag.styled.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { TextField as BaseTextField } from 'components/base';
+
+export const Controls = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const TextField = styled(BaseTextField)`
+  && {
+    .NreTextFieldInputLabelAsterisk {
+      color: #ff476c;
+    }
+
+    .NreTextFieldSelectSelect,
+    .NreTextFieldSelectSelect:focus {
+      background-color: ${({ theme }) => theme.basePalette.greyscale.white};
+    }
+  }
+`;
+
+export const ThresholdDescription = styled.p`
+  font-family: Roboto, sans-serif;
+  font-size: 16px;
+`;

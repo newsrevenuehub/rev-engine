1845314b1fd31a87e7b3d1939b5e9d68b4de6007
Chris Klimas <chris.klimas@fundjournalism.org>
2023-09-29 13:49:41 -0400

    DEV-3350: Remove redundant call info

--- apps/organizations/tasks.py
+++ apps/organizations/tasks.py
@@ -56,9 +56,9 @@ def exchange_mc_oauth_code_for_mc_access_token(oauth_code: str) -> str:
         "code": oauth_code,
     }
 
-    logger.info("exchange_mc_oauth_code_for_mc_access_token making a request to Mailchimp")
+    logger.info("making a request to Mailchimp")
     logger.debug(
-        "exchange_mc_oauth_code_for_mc_access_token making a request to Mailchimp with the following data: %s",
+        "making a request to Mailchimp with the following data: %s",
         request_data | {"code": "REDACTED", "client_secret": "REDACTED"},
     )
     response = requests.post(MAILCHIMP_EXCHANGE_OAUTH_CODE_FOR_ACCESS_TOKEN_URL, data=request_data)

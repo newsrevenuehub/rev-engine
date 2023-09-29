4b1c6759eff8dd66c2dc5d060437eb002c8fd74a
Benjamin White <ben.white@fundjournalism.org>
2023-09-29 16:24:28 -0400

    test stubs

--- apps/users/tests/test_models.py
+++ apps/users/tests/test_models.py
@@ -1,82 +1,122 @@
-from django.contrib.auth import get_user_model
-from django.core import exceptions
-from django.test import TestCase
-
 import pytest
+from waffle import get_waffle_flag_model
 
-from apps.organizations.models import RevenueProgram
+from apps.organizations.models import Organization, RevenueProgram
 from apps.organizations.tests.factories import OrganizationFactory, RevenueProgramFactory
 from apps.users import models
-from apps.users.tests.factories import create_test_user
 
 
-user_model = get_user_model()
+@pytest.fixture(params=["superuser", "hub_admin", "org_user_free_plan", "rp_user", "contributor_user"])
+def expected_user(request):
+    return request.getfixturevalue(request.param)
 
 
-class UserModelTest(TestCase):
-    def setUp(self):
-        self.organization = OrganizationFactory()
-        self.hub_admin_user = create_test_user(role_assignment_data={"role_type": models.Roles.HUB_ADMIN})
-        self.hub_admin_user_role_assignment = self.hub_admin_user.roleassignment
-
-        self.org_admin_user = create_test_user(
-            role_assignment_data={"role_type": models.Roles.ORG_ADMIN, "organization": self.organization}
-        )
-        self.org_admin_user_role_assignment = self.org_admin_user.roleassignment
-
-        rp = RevenueProgramFactory(organization=self.organization)
-        rp_qs = RevenueProgram.objects.filter(pk=rp.pk)
-        self.rp_admin_user = create_test_user(
-            role_assignment_data={
-                "role_type": models.Roles.ORG_ADMIN,
-                "organization": self.organization,
-                "revenue_programs": rp_qs,
-            }
-        )
-        self.rp_admin_user_role_assignment = self.rp_admin_user.roleassignment
-
-    def test_get_role_assignment(self):
-        hub_role_assignment = self.hub_admin_user.get_role_assignment()
-        self.assertEqual(hub_role_assignment, self.hub_admin_user_role_assignment)
-        org_role_assignment = self.org_admin_user.get_role_assignment()
-        self.assertEqual(org_role_assignment, self.org_admin_user_role_assignment)
-        rp_role_assignment = self.rp_admin_user.get_role_assignment()
-        self.assertEqual(rp_role_assignment, self.rp_admin_user_role_assignment)
-
-    def test_get_full_name(self):
-        self.assertIsNotNone(self.hub_admin_user.get_full_name())
-
-    def test_str_method_with_different_user_types(self):
-        str(self.hub_admin_user)
-        str(self.org_admin_user)
-        str(self.rp_admin_user)
-        str(create_test_user())
-
-    def test_validate_unique(self):
-        user_1 = create_test_user()
-        user_2_email = user_1.email.upper()
-        user_2 = user_model(email=user_2_email, password="password")
-        with pytest.raises(exceptions.ValidationError) as _ex:
-            user_2.full_clean()
-        self.assertListEqual(_ex.value.messages, ["User with this Email already exists."])
+@pytest.fixture
+def test_permitted_organizations_setup(rp_user, org_user_free_plan):
+    OrganizationFactory.create_batch(3)
+    assert (
+        Organization.objects.filter(revenue_programs__in=[rp_user.roleassignment.revenue_programs.all()]).count() == 1
+    )
+    assert (
+        Organization.objects.filter(
+            revenue_programs__in=[org_user_free_plan.roleassignment.revenue_programs.all()]
+        ).count()
+        == 1
+    )
 
 
-class RoleAssignmentModelTest(TestCase):
-    def setUp(self):
-        self.organization = OrganizationFactory()
-        self.revenue_program = RevenueProgramFactory(organization=self.organization)
+@pytest.fixture
+def test_permitted_revenue_programs_setup(rp_user, org_user_free_plan):
+    RevenueProgramFactory.create_batch(3)
+    assert RevenueProgram.objects.filter(roleassignement__user=rp_user).count() == 1
+    assert RevenueProgram.objects.filter(roleassignement__user=org_user_free_plan).count() == 1
 
-    def test_role_assignment_str(self):
-        for role_val, role_label in models.Roles.choices:
-            user = user_model.objects.create_user(email=f"{role_val}@test.com", password="testing")
-            role_assignment = models.RoleAssignment.objects.create(
-                user=user, role_type=role_val, organization=self.organization
-            )
-            if role_assignment.role_type == models.Roles.HUB_ADMIN:
-                self.assertEqual(str(role_assignment), role_label)
-            if role_assignment.role_type == models.Roles.ORG_ADMIN:
-                self.assertEqual(str(role_assignment), f"{role_label} for {self.organization.name}")
-            if role_assignment.role_type == models.Roles.RP_ADMIN:
-                role_assignment.revenue_programs.add(self.revenue_program)
-                role_assignment.save()
-                self.assertTrue(f"{role_label} for these revenue programs: " in str(role_assignment))
+
+@pytest.fixture
+def test_active_flags_setup(
+    org_user_free_plan,
+):
+    Flag = get_waffle_flag_model()
+    Flag.objects.delete()
+    Flag(superusers=True, name="flag_superusers")
+    Flag(everyone=True, name="flag_everyone")
+    # etc
+
+
+@pytest.fixture(
+    params=[
+        "superuser",
+        ("superuser", "Superuser"),
+        "hub_admin",
+        ("hub_admin", "Hub Admin"),
+        "rp_user",
+        ("rp_admin", "RP Admin"),
+        "org_user_free_plan",
+        ("org_admin", "Org Admin"),
+        "user_no_role_assignment",
+        None,
+        "contributor_user",
+        None,
+    ]
+)
+def ra_role_type_test_expectation(request):
+    return request.getfixturevalue(request.param[0]), request.param[1]
+
+
+@pytest.mark.django_db
+class TestUser:
+    def test_permitted_organizations(self, expected_user, test_permitted_organizations_setup):
+        orgs = expected_user.permitted_organizations
+        if (
+            expected_user.is_superuser
+            or expected_user.roleassignment
+            and expected_user.roleassignment.role_type == models.Roles.HUB_ADMIN
+        ):
+            assert orgs.count() == Organization.objects.count()
+            assert set(orgs.values_list("id", flat=True)) == set(Organization.objects.values_list("id", flat=True))
+        elif expected_user.roleassignment:
+            assert orgs.count() == 1
+            assert orgs.first() == expected_user.roleassignment.organization
+        else:
+            assert orgs.count() == 0
+
+    def test_permitted_revenue_programs(self, expected_user, test_permitted_revenue_programs_setup):
+        rps = expected_user.permitted_revenue_programs
+        if (
+            expected_user.is_superuser
+            or expected_user.roleassignment
+            and expected_user.roleassignment.role_type == models.Roles.HUB_ADMIN
+        ):
+            assert rps.count() == RevenueProgram.objects.count()
+            assert set(rps.values_list("id", flat=True)) == set(RevenueProgram.objects.values_list("id", flat=True))
+        elif expected_user.roleassignment:
+            assert rps.count() == 1
+            assert rps.first() == expected_user.roleassignment.organization
+        else:
+            assert rps.count() == 0
+
+    def test_active_flags(self, expected_user, test_active_flags_setup):
+        pass
+
+    def test_role_type(self, ra_role_type_test_expectation):
+        user, expected_role_type = ra_role_type_test_expectation
+        assert user.role_type == expected_role_type
+
+    # what this about!?
+
+
+#     def test_validate_unique(self):
+#         user_1 = create_test_user()
+#         user_2_email = user_1.email.upper()
+#         user_2 = user_model(email=user_2_email, password="password")
+#         with pytest.raises(exceptions.ValidationError) as _ex:
+#             user_2.full_clean()
+#         self.assertListEqual(_ex.value.messages, ["User with this Email already exists."])
+
+
+class TestRoleAssignment:
+    def test__str__(self):
+        pass
+
+    def test_can_access_rp(self):
+        pass

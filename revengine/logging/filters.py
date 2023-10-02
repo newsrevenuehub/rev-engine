84cc409541f684150bac12bd10aeed612e83f290
Leo Alves <leo.dcfa@gmail.com>
2023-10-03 08:29:06 +1000

    DEV-3520: simplify filtering

--- revengine/logging/filters.py
+++ revengine/logging/filters.py
@@ -4,16 +4,12 @@ import logging
 
 class ClassNameFilter(logging.Filter):
     def filter(self, record):
-        classname = None
         frames = inspect.stack()
+        record.classname = ""
         for fr in frames:  # pragma: no cover loop is never expected to complete
             if fr.filename == record.pathname and fr.lineno == record.lineno:
                 local_self = fr.frame.f_locals.get("self", None)
                 if local_self:
-                    classname = local_self.__class__.__name__
+                    record.classname = f"[{local_self.__class__.__name__}]"
                 break
-
-        record.classname = ""
-        if classname:
-            record.classname = f"[{classname}] "
         return True

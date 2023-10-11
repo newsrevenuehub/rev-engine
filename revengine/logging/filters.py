5e21ff572ea3c1b7fe6c137016bfc3808003bf22
Leo Alves <leo.dcfa@gmail.com>
2023-10-12 08:09:26 +1000

    DEV-3520: add class name to class mnethods

--- revengine/logging/filters.py
+++ revengine/logging/filters.py
@@ -5,11 +5,20 @@ import logging
 class ClassNameFilter(logging.Filter):
     def filter(self, record):
         frames = inspect.stack()
+        classname = None
         record.classname = ""
         for fr in frames:  # pragma: no cover loop is never expected to complete
             if fr.filename == record.pathname and fr.lineno == record.lineno:
-                local_self = fr.frame.f_locals.get("self", None)
+                local_vars = fr.frame.f_locals
+                local_self = local_vars.get("self", None)
+                local_cls = local_vars.get("cls", None)
+
                 if local_self:
-                    record.classname = f"[{local_self.__class__.__name__}]"
+                    classname = local_self.__class__.__name__
+                elif local_cls:
+                    classname = local_cls.__name__
+
+                if classname:
+                    record.classname = f"[{classname}] "
                 break
         return True

75ee36c879b91fe82a2dc63427bbc6c3bce927d3
Guilherme Mendonca <9381325+GuiMend@users.noreply.github.com>
2023-10-24 17:27:00 -0300

    fix test

--- revengine/tests/test_celery.py
+++ revengine/tests/test_celery.py
@@ -1,6 +1,3 @@
-import unittest
-from unittest.mock import patch
-
 from celery.utils.log import get_task_logger
 
 from revengine.celery import debug_task, request_id, setup_celery_logs
@@ -13,18 +10,20 @@ def test_debug_task():
     debug_task()
 
 
-class TestLoggingConfiguration(unittest.TestCase):
-    @patch("sys.stderr")  # Mock stderr to capture log output
-    def test_log_format(self, mock_stderr):
+class TestLoggingConfiguration:
+    def test_log_format(self, mocker):
+        message = "This is a warning"
+        mock_stderr = mocker.patch("sys.stderr")
+
         # Set up logging with the custom format
         setup_celery_logs()
 
         # Log a warning message
-        logger.warning("This is a warning")
+        logger.warning(message)
 
         # Get the log output captured by sys.stderr
         log_output = mock_stderr.write.call_args[0][0].strip()
 
         # Assert if the log output matches the expected format
-        expected_output = f"WARNING [{request_id.get()}]  test_celery:20 - This is a warning"
+        expected_output = f"WARNING [{request_id.get()}]  test_celery:25 - {message}"
         assert expected_output in log_output

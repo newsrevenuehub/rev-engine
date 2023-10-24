300ca14471cee1ff32e12b9551d5114b32f60f4e
Benjamin White <ben.white@fundjournalism.org>
2023-10-24 15:30:15 -0400

    try not creating googl-sa credentials

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,6 +16,6 @@ if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
     python manage.py migrate --noinput
 fi
 
-/bin/sh ./google_sa.sh
+# /bin/sh ./google_sa.sh
 
 exec uwsgi --ini ./uwsgi.conf --http=0.0.0.0:$PORT

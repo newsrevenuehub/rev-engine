f9e01bdd2f384d75ca20ee7e3f65b58ccf433419
Guilherme Mendonca <9381325+GuiMend@users.noreply.github.com>
2023-10-24 20:00:19 -0300

    DEV-3207: add uncovered test

--- revengine/tests/test_celery.py
+++ revengine/tests/test_celery.py
@@ -21,9 +21,21 @@ class TestLoggingConfiguration:
         # Log a warning message
         logger.warning(message)
 
-        # Get the log output captured by sys.stderr
-        log_output = mock_stderr.write.call_args[0][0].strip()
+        assert (
+            f"WARNING [{request_id.get()}]  test_celery:22 - [test_log_format] {message}"
+            in mock_stderr.write.call_args[0][0]
+        )
 
-        # Assert if the log output matches the expected format
-        expected_output = f"WARNING [{request_id.get()}]  test_celery:22 - [test_log_format] {message}"
-        assert expected_output in log_output
+    def test_undefined_request_id(self, mocker):
+        request_id.set(None)
+
+        message = "This is a warning"
+        mock_stderr = mocker.patch("sys.stderr")
+
+        # Set up logging with the custom format
+        setup_celery_logs()
+
+        # Log a warning message
+        logger.warning(message)
+
+        assert f"WARNING  test_celery:39 - [test_undefined_request_id] {message}" in mock_stderr.write.call_args[0][0]

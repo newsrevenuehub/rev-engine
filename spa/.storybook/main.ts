76718920d5e22fd125db61b0e3ba0fd067d1f66c
Chris Klimas <chris.klimas@fundjournalism.org>
2023-11-07 08:31:49 -0500

    DEV-2801: Enable Turbosnap

--- spa/.storybook/main.ts
+++ spa/.storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import turbosnap from 'vite-plugin-turbosnap';
+import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -9,6 +11,22 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag'
+  },
+  async viteFinal(config, { configType }) {
+    // See https://github.com/IanVS/vite-plugin-turbosnap
+
+    return mergeConfig(config, {
+      plugins:
+        configType === 'PRODUCTION'
+          ? [
+              turbosnap({
+                // This should be the base path of your storybook.  In
+                // monorepos, you may only need process.cwd().
+                rootDir: config.root ?? process.cwd()
+              })
+            ]
+          : []
+    });
   },
   // Needed to avoid problems with how we use spaces in component displayNames.
   // See https://github.com/storybookjs/storybook/issues/18074

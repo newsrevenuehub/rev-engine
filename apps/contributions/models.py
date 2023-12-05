16595158e2eefc69077e165a3f699fb3c4d13e6b
Benjamin White <ben.white@fundjournalism.org>
2023-12-05 10:07:47 -0500

    temp info log

--- apps/contributions/models.py
+++ apps/contributions/models.py
@@ -1204,7 +1204,7 @@ class Payment(IndexedTimeStampedModel):
         contribution, balance_transaction = cls.get_contribution_and_balance_transaction_for_charge_refunded_event(
             event=event
         )
-
+        logger.info(balance_transaction.source)
         return cls._handle_create_payment(
             contribution=contribution,
             balance_transaction=balance_transaction,

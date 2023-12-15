294fceae1c7eabfd226ac3e02f98bb02634b011f
Chris Klimas <chris.klimas@fundjournalism.org>
2023-12-15 13:51:04 -0500

    Suggestions around improvements to Google Maps interations

--- spa/src/hooks/useGoogleMaps.ts
+++ spa/src/hooks/useGoogleMaps.ts
@@ -1,32 +1,39 @@
 import { HUB_GOOGLE_MAPS_API_KEY } from 'appSettings';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Loader } from '@googlemaps/js-api-loader';
 
 const useGoogleMaps = (language?: string) => {
-  const [loading, setLoading] = useState(true);
-  const loader = new Loader({
-    apiKey: HUB_GOOGLE_MAPS_API_KEY,
-    version: 'quarterly',
-    libraries: ['places'],
-    language: language
-  });
+  // Another successful invocation of this hook will create a `google` global.
+  // In this case, we want to avoid loading it a second time.
+
+  const [loading, setLoading] = useState(!window.google);
+  const [error, setError] = useState<Error>();
+  const loader = useMemo(
+    () =>
+      new Loader({
+        apiKey: HUB_GOOGLE_MAPS_API_KEY,
+        version: 'quarterly',
+        libraries: ['places'],
+        language: language
+      }),
+    [language]
+  );
 
   useEffect(() => {
-    loader
-      .importLibrary('places')
-      .then(() => {
-        console.log('Google Maps API is loaded and available');
-      })
-      .catch((err) => {
-        console.error('Error loading Google Maps API', err);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); // only load once
+    if (loading) {
+      loader
+        .importLibrary('places')
+        .catch((err) => {
+          console.error('Error loading Google Maps API', err);
+          setError(err);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [loader, loading]);
 
-  return { isGoogleMapsLoading: loading };
+  return { error, loading };
 };
 
 export default useGoogleMaps;

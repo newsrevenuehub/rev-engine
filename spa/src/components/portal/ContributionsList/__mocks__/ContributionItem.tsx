6f48af594872ee7e4d48e3beec78c2ddba3afb09
Benjamin White <ben.white@fundjournalism.org>
2024-01-03 16:22:43 -0500

    fix mock

--- spa/src/components/portal/ContributionsList/__mocks__/ContributionItem.tsx
+++ spa/src/components/portal/ContributionsList/__mocks__/ContributionItem.tsx
@@ -1,7 +1,7 @@
 import { ContributionItemProps } from '../ContributionItem';
 
 export const ContributionItem = (props: ContributionItemProps) => (
-  <div data-testid={`mock-contribution-item-${props.contribution.payment_provider_id}`} />
+  <div data-testid={`mock-contribution-item-${props.contribution.id}`} />
 );
 
 export default ContributionItem;

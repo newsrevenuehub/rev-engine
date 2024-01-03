1e9bb6b83aa138e208a1d0dd24f829dbe9b0d5a2
Guilherme Mendonca <9381325+GuiMend@users.noreply.github.com>
2024-01-03 10:40:53 -0300

    fix social meta tests

--- apps/common/tests/test_models.py
+++ apps/common/tests/test_models.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 
-from apps.common.models import SocialMeta
 from apps.organizations.tests.factories import RevenueProgramFactory
 
 
@@ -9,9 +8,8 @@ class SocialMetaTest(TestCase):
         self.title = "My Social Title"
         self.description = "My Social Description"
         self.url = "https://example.com"
-        self.social_meta = SocialMeta.objects.create(
-            title=self.title, description=self.description, url=self.url, revenue_program=RevenueProgramFactory()
-        )
+        rp = RevenueProgramFactory()
+        self.social_meta = rp.socialmeta
 
     def test_social_meta_string(self):
         expected_related_string = f"Social media Metatags for RevenueProgram: {self.social_meta.revenue_program.name}"
--- apps/common/tests/test_serializers.py
+++ apps/common/tests/test_serializers.py
@@ -1,6 +1,5 @@
 from django.test import RequestFactory, TestCase
 
-from apps.common.models import SocialMeta
 from apps.common.serializers import SocialMetaInlineSerializer
 from apps.common.tests.test_utils import get_test_image_file_jpeg
 from apps.organizations.tests.factories import RevenueProgramFactory
@@ -16,23 +15,18 @@ class SocialMetaInlineSerializerTest(TestCase):
         file.url = "/media/whatever.png"
         self.card = get_test_image_file_jpeg()
         self.twitter_handle = "tweetzor"
-        self.revenue_program = RevenueProgramFactory(name=self.rp_name, twitter_handle=self.twitter_handle)
-        self.social_meta = SocialMeta.objects.create(
-            title=self.title,
-            description=self.description,
-            url=self.url,
-            card=self.card,
-            revenue_program=self.revenue_program,
-        )
+        rp = RevenueProgramFactory(name=self.rp_name, twitter_handle=self.twitter_handle)
+        self.revenue_program = rp
+        self.social_meta = rp.socialmeta
         self.serializer = SocialMetaInlineSerializer
         factory = RequestFactory()
         self.request = factory.get("/")
 
     def test_serializer_method_fields(self):
         serializer = self.serializer(self.social_meta, context={"request": self.request})
-        self.assertEqual(serializer.data["title"], self.title)
-        self.assertEqual(serializer.data["description"], self.description)
-        self.assertEqual(serializer.data["url"], self.url)
+        self.assertEqual(serializer.data["title"], "Join | My RP")
+        self.assertEqual(serializer.data["description"], "My RP is supported by people like you. Support My RP today.")
+        self.assertEqual(serializer.data["url"], "https://fundjournalism.org")
         self.assertEqual(serializer.data["twitter_handle"], "@" + self.twitter_handle)
         self.assertEqual(serializer.data["revenue_program_name"], self.revenue_program.name)
-        self.assertEqual(serializer.data["image_alt"], f"{self.revenue_program.name} social media card")
+        self.assertEqual(serializer.data["image_alt"], "fund journalism social media card")

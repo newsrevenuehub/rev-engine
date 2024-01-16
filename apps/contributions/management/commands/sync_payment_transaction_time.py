d4f65817deb7d980caac66246769c887315e2e41
Benjamin White <ben.white@fundjournalism.org>
2024-01-16 09:30:56 -0500

    add todo

--- apps/contributions/management/commands/sync_payment_transaction_time.py
+++ apps/contributions/management/commands/sync_payment_transaction_time.py
@@ -10,6 +10,7 @@ from apps.contributions.models import Payment
 class Command(BaseCommand):
     """Try to populate null values for payment.transaction_time in db."""
 
+    # TODO: [DEV-4403] Make sync_payment_transaction_time command resilient vs. Stripe API errors (esp. no longer-connected accounts)
     def handle(self, *args, **options):
         self.stdout.write(self.style.HTTP_INFO("Running `sync_payment_transaction_time`"))
         queryset = Payment.objects.filter(transaction_time__isnull=True)

7d02887292a1ea9c90808c48d2b79629d68c84fe
Benjamin White <ben.white@fundjournalism.org>
2024-01-23 11:58:22 -0500

    remove stale file

--- spa/src/components/portal/ContributionsList/__mocks__/ContributionItem.tsx
+++
@@ -1,7 +0,0 @@
-import { ContributionItemProps } from '../ContributionItem';
-
-export const ContributionItem = (props: ContributionItemProps) => (
-  <div data-testid={`mock-contribution-item-${props.contribution.id}`} />
-);
-
-export default ContributionItem;

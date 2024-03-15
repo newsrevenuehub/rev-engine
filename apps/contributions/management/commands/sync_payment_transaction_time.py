fa6ab7c36b91c98d633dd87e57d09919896c24ea
Benjamin White <ben.white@fundjournalism.org>
2024-03-15 12:38:20 -0400

    refactor command and tests

--- apps/contributions/management/commands/sync_payment_transaction_time.py
+++ apps/contributions/management/commands/sync_payment_transaction_time.py
@@ -1,24 +1,95 @@
 import datetime
+import logging
+from zoneinfo import ZoneInfo
 
 from django.core.management.base import BaseCommand
 
+import reversion
 import stripe
 
 from apps.contributions.models import Payment
+from apps.organizations.models import PaymentProvider
+
+
+# otherwise we get spammed by stripe info logs when running this command
+stripe_logger = logging.getLogger("stripe")
+stripe_logger.setLevel(logging.ERROR)
 
 
 class Command(BaseCommand):
     """Try to populate null values for payment.transaction_time in db."""
 
+    def get_stripe_accounts_and_their_connection_status(self):
+        self.stdout.write(self.style.HTTP_INFO("Retrieving stripe accounts and their connection status"))
+        account_ids = PaymentProvider.objects.filter(stripe_account_id__isnull=False).values_list(
+            "stripe_account_id", flat=True
+        )
+        accounts = {}
+        for account_id in account_ids:
+            try:
+                account = stripe.Account.retrieve(account_id)
+                accounts[account_id] = account.charges_enabled
+            except stripe.error.StripeError as e:
+                self.stdout.write(self.style.ERROR(f"Error while retrieving account {account_id}: {e}"))
+                accounts[account_id] = False
+        return accounts
+
     # TODO: [DEV-4403] Make sync_payment_transaction_time command resilient vs. Stripe API errors (esp. no longer-connected accounts)
     def handle(self, *args, **options):
         self.stdout.write(self.style.HTTP_INFO("Running `sync_payment_transaction_time`"))
-        queryset = Payment.objects.filter(transaction_time__isnull=True)
-        self.stdout.write(self.style.HTTP_INFO(f"Found {queryset.count()} payments to sync"))
-        for payment in queryset.all():
-            bt = stripe.BalanceTransaction.retrieve(
-                payment.stripe_balance_transaction_id, stripe_account=payment.contribution.stripe_account_id
+        if not Payment.objects.filter(transaction_time__isnull=True).exists():
+            self.stdout.write(self.style.HTTP_INFO("No payments with missing transaction time found, exiting"))
+            return
+        accounts = self.get_stripe_accounts_and_their_connection_status()
+        disconnected_accounts = [k for k, v in accounts.items() if not v]
+        connected_accounts = [k for k, v in accounts.items() if v]
+        eligible_payments = Payment.objects.filter(
+            contribution__donation_page__isnull=False,
+            contribution__donation_page__revenue_program__payment_provider__stripe_account_id__in=connected_accounts,
+            transaction_time__isnull=True,
+        )
+        ineligible_because_of_account = Payment.objects.filter(
+            contribution__donation_page__revenue_program__payment_provider__stripe_account_id__in=disconnected_accounts,
+            transaction_time__isnull=True,
+        )
+        ineligible_because_no_donation_page = Payment.objects.filter(
+            contribution__donation_page__isnull=True, transaction_time__isnull=True
+        )
+        self.stdout.write(self.style.HTTP_INFO(f"Found {eligible_payments.count()} eligible payments to sync"))
+        self.stdout.write(
+            self.style.HTTP_INFO(
+                f"Found {ineligible_because_of_account.count()} payment(s) with null value for transaction time that cannot be updated "
+                f"because account is disconnected:{', '.join(str(x) for x in ineligible_because_of_account.values_list('id', flat=True))}"
             )
-            payment.transaction_time = datetime.datetime.fromtimestamp(bt.created)
-            payment.save(update_fields={"modified", "transaction_time"})
+        )
+        self.stdout.write(
+            self.style.HTTP_INFO(
+                f"Found {ineligible_because_no_donation_page.count()} payment(s) with null value for transaction time that cannot be updated "
+                f"because their contribution is not linked to a donation page: "
+                f"{', '.join(str(x) for x in ineligible_because_no_donation_page.values_list('id', flat=True))}"
+            )
+        )
+        updated_ids = []
+        for payment in eligible_payments.all():
+            try:
+                bt = stripe.BalanceTransaction.retrieve(
+                    payment.stripe_balance_transaction_id, stripe_account=payment.contribution.stripe_account_id
+                )
+            except stripe.error.StripeError as e:
+                self.stdout.write(
+                    self.style.ERROR(
+                        f"Error while retrieving balance transaction {payment.stripe_balance_transaction_id}: {e}"
+                    )
+                )
+                continue
+            payment.transaction_time = datetime.datetime.fromtimestamp(bt.created, tz=ZoneInfo("UTC"))
+            with reversion.create_revision():
+                payment.save(update_fields={"modified", "transaction_time"})
+                reversion.set_comment("Updated by sync_payment_transaction_time command")
+            updated_ids.append(payment.id)
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Updated {len(updated_ids)} payment(s) out of {eligible_payments.count()} eligible payments. The following payments were updated: {', '.join(map(str, updated_ids))}"
+            )
+        )
         self.stdout.write(self.style.SUCCESS("`sync_payment_transaction_time` is done"))
--- apps/contributions/tests/test_management_commands.py
+++ apps/contributions/tests/test_management_commands.py
@@ -1,9 +1,12 @@
 import datetime
+from zoneinfo import ZoneInfo
 
 from django.core.management import call_command
 
 import pytest
+import stripe
 
+from apps.contributions.models import Payment
 from apps.contributions.tests.factories import PaymentFactory
 
 
@@ -35,14 +38,85 @@ def test_sync_missing_contribution_data_from_stripe(dry_run, monkeypatch, mocker
 
 
 @pytest.mark.django_db
-def test_sync_payment_transaction_time(mocker, balance_transaction_for_one_time_charge):
-    payment = PaymentFactory(transaction_time=None)
-    mocker.patch(
-        "stripe.BalanceTransaction.retrieve",
-        return_value=balance_transaction_for_one_time_charge,
-    )
-    call_command("sync_payment_transaction_time")
-    payment.refresh_from_db()
-    assert payment.transaction_time == datetime.datetime.fromtimestamp(
-        balance_transaction_for_one_time_charge.created, tz=datetime.timezone.utc
-    )
+class Test_sync_payment_transaction_time:
+
+    DISCONNECTED_ACCOUNT_ID = "acct_disconnected"
+    BALANCE_TRANSACTION_ID_WITH_ERROR = "txn_with_error"
+    BT_CREATED_TIMESTAMP = 1610000000
+    ACCOUNT_ID_WITH_ERROR = "acct_with_error"
+    ACCOUNT_ID = "acct_1"
+
+    @pytest.fixture(autouse=True)
+    def payment_with_transaction_time(self):
+        return PaymentFactory(transaction_time=datetime.datetime.utcnow())
+
+    @pytest.fixture(autouse=True)
+    def payment_no_transaction_time_eligible(self):
+        return PaymentFactory(
+            transaction_time=None,
+            contribution__donation_page__revenue_program__payment_provider__stripe_account_id=self.ACCOUNT_ID,
+        )
+
+    @pytest.fixture(autouse=True)
+    def payment_no_transaction_time_eligible_fail_with_bt_retrieval(self):
+        return PaymentFactory(
+            transaction_time=None,
+            stripe_balance_transaction_id=self.BALANCE_TRANSACTION_ID_WITH_ERROR,
+            contribution__donation_page__revenue_program__payment_provider__stripe_account_id=self.ACCOUNT_ID,
+        )
+
+    @pytest.fixture(autouse=True)
+    def payment_no_transaction_time_eligible_fail_with_account_retrieval(self):
+        return PaymentFactory(
+            transaction_time=None,
+            contribution__donation_page__revenue_program__payment_provider__stripe_account_id=self.ACCOUNT_ID_WITH_ERROR,
+        )
+
+    @pytest.fixture
+    def payment_no_transaction_time_ineligible_because_no_page(self):
+        return PaymentFactory(transaction_time=None, contribution__donation_page=None)
+
+    @pytest.fixture
+    def payment_no_transaction_time_ineligible_because_of_no_account(self):
+        return PaymentFactory(
+            transaction_time=None,
+            contribution__donation_page__revenue_program__payment_provider__stripe_account_id=None,
+        )
+
+    @pytest.fixture
+    def payment_no_transaction_time_ineligible_because_of_disconnected_account(self):
+        return PaymentFactory(
+            transaction_time=None,
+            contribution__donation_page__revenue_program__payment_provider__stripe_account_id=self.DISCONNECTED_ACCOUNT_ID,
+        )
+
+    @pytest.fixture(autouse=True)
+    def stripe_account_retrieve(self, mocker):
+        def side_effect(*args, **kwargs):
+            account_id = args[0]
+            if account_id == self.ACCOUNT_ID_WITH_ERROR:
+                raise stripe.error.StripeError("Some error")
+            return mocker.Mock(charges_enabled=(False if account_id == self.DISCONNECTED_ACCOUNT_ID else True))
+
+        mocker.patch("stripe.Account.retrieve", side_effect=side_effect)
+
+    @pytest.fixture(autouse=True)
+    def balance_transaction_retrieve(self, mocker):
+        def side_effect(*args, **kwargs):
+            if args[0] == self.BALANCE_TRANSACTION_ID_WITH_ERROR:
+                raise stripe.error.StripeError("Some error")
+            return mocker.Mock(created=self.BT_CREATED_TIMESTAMP)
+
+        mocker.patch("stripe.BalanceTransaction.retrieve", side_effect=side_effect)
+
+    def test_sync_payment_transaction_time(self, payment_no_transaction_time_eligible):
+        call_command("sync_payment_transaction_time")
+        payment_no_transaction_time_eligible.refresh_from_db()
+        assert payment_no_transaction_time_eligible.transaction_time == datetime.datetime.fromtimestamp(
+            self.BT_CREATED_TIMESTAMP, tz=ZoneInfo("UTC")
+        )
+
+    def test_sync_payment_transaction_time_when_no_eligible_payments(self, mocker):
+        Payment.objects.all().delete()
+        PaymentFactory(transaction_time=datetime.datetime.utcnow())
+        call_command("sync_payment_transaction_time")

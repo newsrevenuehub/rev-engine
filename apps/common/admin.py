ec4624cf579a09ff3c6cad282f5f207a7bb8f50b
Benjamin White <ben.white@fundjournalism.org>
2024-04-03 10:39:39 -0400

    checking in changes

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -9,10 +9,12 @@ from django_json_widget.widgets import JSONEditorWidget
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
 from pygments.lexers import JsonLexer
-from reversion.models import Revision, Version
+from reversion.admin import VersionAdmin
+from reversion.models import Revision
+from reversion_compare.admin import CompareVersionAdmin
 
 
-class RevEngineBaseAdmin(ModelAdmin):
+class RevEngineBaseAdmin(CompareVersionAdmin, VersionAdmin):
     formfield_overrides = {
         models.JSONField: {"widget": JSONEditorWidget},
     }
@@ -30,19 +32,6 @@ class RevisionAdmin(ModelAdmin):
     list_filter = ("user", "comment")
     search_fields = ("user__email", "comment")
     raw_id_fields = ("user",)
-
-
-@register_admin(Version)
-class VersionAdmin(ModelAdmin):
-    def comment(self, obj):
-        return obj.revision.comment
-
-    list_display = ("object_repr", "comment", "object_id", "content_type", "format")
-    list_display_links = ("object_repr", "object_id")
-    list_filter = ("content_type", "format")
-    list_select_related = ("revision", "content_type")
-    search_fields = ("object_repr", "serialized_data")
-    raw_id_fields = ("revision", "content_type")
 
 
 def prettify_json_field(raw_value, indent=2):  # pragma: no cover

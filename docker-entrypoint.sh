f3fdadda025a9a7946ae80ea18e448f77c06463f
Chris Klimas <chris.klimas@fundjournalism.org>
2024-04-30 09:16:14 -0400

    DEV-4110: Try reverting static file related changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,8 +16,4 @@ if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
     python manage.py migrate --noinput
 fi
 
-
-python manage.py collectstatic --noinput
-
-
 exec uwsgi --ini ./uwsgi.conf --http=0.0.0.0:$PORT

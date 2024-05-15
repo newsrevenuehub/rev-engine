4583659130d29b7c9213c7b1ee1aeabd4e9c8509
Benjamin White <ben.white@fundjournalism.org>
2024-05-15 15:08:49 -0400

    remove Contribution.fix_missing_provider_payment_method_details

--- apps/contributions/management/commands/sync_missing_contribution_data_from_stripe.py
+++ apps/contributions/management/commands/sync_missing_contribution_data_from_stripe.py
@@ -11,6 +11,5 @@ class Command(BaseCommand):
         self.stdout.write(self.style.HTTP_INFO("Running `sync_missing_contribution_data_from_stripe`"))
         Contribution.fix_contributions_stuck_in_processing(dry_run=options["dry_run"])
         Contribution.fix_missing_provider_payment_method_id(dry_run=options["dry_run"])
-        Contribution.fix_missing_payment_method_details_data(dry_run=options["dry_run"])
         Contribution.fix_missing_contribution_metadata(dry_run=options["dry_run"])
         self.stdout.write(self.style.SUCCESS("`sync_missing_contribution_data_from_stripe` is done"))

1ddef2d2ee8e7945d3f4c03db7bb6f14cceedc75
Chris Klimas <chris.klimas@fundjournalism.org>
2024-05-17 13:36:52 -0400

    Temp add debug logging around deploy

--- scripts/heroku-postdeploy.sh
+++ scripts/heroku-postdeploy.sh
@@ -20,6 +20,10 @@ CREATE SCHEMA public;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO public;"
 
+# debug what collations are left after above
+
+psql -d ${DATABASE_URL} -c "SELECT collname, collowner FROM pg_collation ORDER BY collname;"
+
 pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} | pg_restore --clean --no-owner --no-acl --format=custom --if-exists -d ${DATABASE_URL} || true
 # the feature branch's migrations initially get applied as part of the normal build CI process, but here in review app post deploy step,
 # we drop all the tables before restoring from staging (see reason above), which means that any new migrations get whomped. So we run migrations again here.

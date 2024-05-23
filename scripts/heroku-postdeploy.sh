a3add1393ac2ea03156f28a1c33b9b9ce8243cc2
Benjamin White <ben.white@fundjournalism.org>
2024-05-23 10:05:27 -0400

    fix exclude option

--- scripts/heroku-postdeploy.sh
+++ scripts/heroku-postdeploy.sh
@@ -5,5 +5,5 @@ set -o xtrace
 
 # We seed the database with the data from the source database. Note that we exclude the django_migrations table because
 # the source database will not have any migrations that are unique to the feature branch.
-pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} | pg_restore --clean --no-owner --no-acl --format=custom --if-exists -d ${DATABASE_URL} --exclude-table='django_migrations' || true
+pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} --exclude-table='django_migrations' | pg_restore --clean --no-owner --no-acl --format=custom --if-exists -d ${DATABASE_URL} || true
 python manage.py bootstrap-review-app

99b4dd34f9408d79f9b55362d6cf0983592d7059
Benjamin White <ben.white@fundjournalism.org>
2024-05-23 16:12:17 -0400

    squash migrations

--- apps/config/models.py
+++ apps/config/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class DenyListWord(models.Model):
     # NB: db_collation here is to make the word case-insensitive
     # see https://docs.djangoproject.com/en/4.2/releases/4.2/#id1 note on CICharField
-    word = models.CharField(max_length=255, unique=True, db_collation="case_insensitive")
+    word = models.TextField(max_length=255, unique=True, db_collation="case_insensitive")
 
     class Meta:
         verbose_name = "Deny-list word"

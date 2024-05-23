9ca7c25d19eb6721cedb86d553aba238639bde21
Benjamin White <ben.white@fundjournalism.org>
2024-05-23 10:26:02 -0400

    also exclude django_ migrations_id_seq

--- scripts/heroku-postdeploy.sh
+++ scripts/heroku-postdeploy.sh
@@ -3,7 +3,13 @@
 set -o nounset
 set -o xtrace
 
-# We seed the database with the data from the source database. Note that we exclude the django_migrations table because
-# the source database will not have any migrations that are unique to the feature branch.
-pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} --exclude-table='django_migrations' | pg_restore --clean --no-owner --no-acl --format=custom --if-exists -d ${DATABASE_URL} || true
+pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} \
+    # Note that we exclude the django_migrations table because the source database will not have any
+    # migrations that are unique to the feature branch.
+  --exclude-table='public.django_migrations' \
+  --exclude-table='public.django_migrations_id_seq' | \
+# We use the --data-only flag so that we only restore data from source that
+# has counterparts in the target database tables.
+pg_restore --data-only --clean --no-owner --no-acl \
+  --format=custom --if-exists -d ${DATABASE_URL} || true
 python manage.py bootstrap-review-app

dd308c7be3c5b85fb62b22407bca8035244d81f5
Benjamin White <ben.white@fundjournalism.org>
2024-05-23 16:27:59 -0400

    only essential unit tests

--- apps/config/tests/test_models.py
+++ apps/config/tests/test_models.py
@@ -3,30 +3,21 @@ from django.db.utils import IntegrityError
 import pytest
 
 from apps.config.models import DenyListWord
+from apps.config.tests.factories import DenyListWordFactory
 
 
 @pytest.mark.django_db
 class TestDenyListWord:
-    text = (token := "z0mg") + token.upper()
+    @pytest.fixture()
+    def word(self):
+        return DenyListWordFactory()
 
-    def test_case_insensitive_uniqueness(self):
-        # Create a DenyListWord instance
-        DenyListWord.objects.create(word=self.text.lower())
-        # Attempt to create another DenyListWord with different casing
+    @pytest.mark.parametrize("case", ["lower", "upper"])
+    def test_case_insensitive_uniqueness(self, word, case):
         with pytest.raises(IntegrityError):
-            DenyListWord.objects.create(word=self.text.upper())
+            DenyListWord.objects.create(word=getattr(word.text, case)())
 
-    def test_retrieval_case_insensitivity(self):
-        # Create a DenyListWord instance
-        DenyListWord.objects.create(word=self.text)
-        # Retrieve using different casing
-        word_lower = DenyListWord.objects.get(word=self.text.lower())
-        word_upper = DenyListWord.objects.get(word=self.text.upper())
+    def test_retrieval_case_insensitivity(self, word):
+        word_lower = DenyListWord.objects.get(word=word.text.lower())
+        word_upper = DenyListWord.objects.get(word=word.text.upper())
         assert word_lower == word_upper
-
-    def test_insertion_case_insensitivity(self):
-        # Insert a word
-        word = DenyListWord.objects.create(word=self.text.lower())
-        # Ensure the word can be retrieved with different casing
-        inserted_word = DenyListWord.objects.get(word=self.text.upper())
-        assert inserted_word.word == word.word

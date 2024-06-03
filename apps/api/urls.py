913ede70fb8bf19d95ba277e0054714a37e5e49a
Benjamin White <ben.white@fundjournalism.org>
2024-06-03 15:20:04 -0400

    checking in in transit changes

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -12,13 +12,14 @@ from apps.organizations.urls import urlpatterns as organizations_urlpatterns
 from apps.pages.urls import urlpatterns as pages_urlpatterns
 from apps.public.urls import urlpatterns as public_urlpatterns
 from apps.users.urls import api_urlpatterns as users_urlpatterns
-
+from apps.e2e.urls import urlpatterns as e2e_urlpatterns
 
 urlpatterns = [
     path("v1/", include(users_urlpatterns)),
     path("v1/", include(organizations_urlpatterns)),
     path("v1/", include(pages_urlpatterns)),
     path("v1/", include(contributions_urlpatterns)),
+    path("v1/", include(e2e_urlpatterns)),
     path("v1/healthcheck", include("health_check.urls")),
     path("v1/public/", include(public_urlpatterns)),
     path("v1/token/", TokenObtainPairCookieView.as_view(), name="token-obtain-pair"),

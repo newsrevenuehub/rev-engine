ffc0b0a2a7cb94b170337ca09b497a7aff0ec91b
Benjamin White <ben.white@fundjournalism.org>
2024-06-03 18:04:41 -0400

    try to solve for double log

--- apps/contributions/stripe_import.py
+++ apps/contributions/stripe_import.py
@@ -84,23 +84,14 @@ TTL_WARNING_THRESHOLD_PERCENT = 0.75
 # We set up some custom logging for this module so we get timestamps, which are helpful
 # in running down timing/rate limiting issues we're facing when this code runs.
 logger = logging.getLogger(f"{settings.DEFAULT_LOGGER}.{__name__}")
-
-console_handler = logging.StreamHandler()
-
-new_formatter = logging.Formatter(
-    "%(asctime)s %(levelname)s %(name)s:%(lineno)d - [%(funcName)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
-)
-
-# Add the formatter to the handler
-console_handler.setFormatter(new_formatter)
-
-# Remove any existing handlers from the logger
 if logger.handlers:
     for handler in logger.handlers:
-        logger.removeHandler(handler)
-
-# Add the handler to the logger
-logger.addHandler(console_handler)
+        handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s %(levelname)s %(name)s:%(lineno)d - [%(funcName)s] %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S",
+            )
+        )
 
 
 class OnBackoffDetails(BaseModel):

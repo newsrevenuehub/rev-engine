7292d77f7fb4e0486947df0a47dac51337bc9219
Benjamin White <ben.white@fundjournalism.org>
2024-06-20 15:17:14 -0400

    update postdeploy

--- scripts/heroku-postdeploy.sh
+++ scripts/heroku-postdeploy.sh
@@ -3,25 +3,20 @@
 set -o nounset
 set -o xtrace
 
+echo "Resetting database"
+heroku pg:reset $DATABASE_URL --confirm $HEROKU_APP_NAME
 
-# Initially we delete the database. This will allow us to restore from dump,
-# then run branch specific migrations on to of that restore.
-# First ensure we have a database url to work with.
-if [ -z "$DATABASE_URL" ]; then
-  echo "DATABASE_URL is not set."
-  exit 1
-fi
-# Extract the database name from DATABASE_URL
-DESTINATION_DB_NAME=$(echo $DATABASE_URL | grep -oE '[^/]+$')
-echo "Destination database name: $DESTINATION_DB_NAME"
-echo "Dropping database $DESTINATION_DB_NAME"
-psql ${DATABASE_URL} -c "DROP DATABASE $DESTINATION_DB_NAME;"
-echo "Creating database $DESTINATION_DB_NAME"
-psql -d postgres -c "CREATE DATABASE $DESTINATION_DB_NAME;"
+# Dump the source database to a file
+echo "Dumping source database..."
+pg_dump --format=custom $REVIEW_APP_SOURCE_DATABASE_URL -f dump_file.dump
 
-echo "Restoring database from $REVIEW_APP_SOURCE_DATABASE_URL"
-pg_dump --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} | \
-pg_restore --no-owner --no-acl-d ${DATABASE_URL}
+# Restore the dump file to the new database
+echo "Restoring dump to Heroku database..."
+pg_restore --no-owner --no-acl --dbname=$DATABASE_URL dump_file.dump
+
+# Clean up the dump file
+echo "Cleaning up dump file..."
+rm dump_file.dump
 
 echo "Running migrations on top of restored database"
 python manage.py migrate

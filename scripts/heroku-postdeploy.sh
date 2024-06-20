334dc88bc41a576d2b7121717f16cde0d8444912
Benjamin White <ben.white@fundjournalism.org>
2024-06-20 16:11:50 -0400

    try deleting all tables

--- scripts/heroku-postdeploy.sh
+++ scripts/heroku-postdeploy.sh
@@ -3,14 +3,22 @@
 set -o nounset
 set -o xtrace
 
+
+# Parse the DATABASE_URL to extract components
+export PGHOST=$(echo $DATABASE_URL | awk -F[@/:] '{print $4}')
+export PGPORT=$(echo $DATABASE_URL | awk -F[@/:] '{print $7}' | awk -F[?] '{print $1}')
+export PGUSER=$(echo $DATABASE_URL | awk -F[@/:] '{print $3}' | awk -F[:] '{print $1}')
+export PGPASSWORD=$(echo $DATABASE_URL | awk -F[@/:] '{print $3}' | awk -F[:] '{print $2}')
+
+
 # First we zero out the database.
 # We want to be able to restore from backup without any conflicts.
-echo "Resetting database"
-psql -d ${DATABASE_URL} -c "DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO public;"
-
+echo "Drop all tables in db"
+# Drop all tables in the database
+TABLES=$(PGPASSWORD=$PGPASSWORD psql -h $PGHOST -U $PGUSER -d $PGDATABASE -t -c "SELECT tablename FROM pg_tables WHERE schemaname = 'public';")
+for TABLE in $TABLES; do
+    PGPASSWORD=$PGPASSWORD psql -h $PGHOST -U $PGUSER -d $PGDATABASE -c "DROP TABLE IF EXISTS $TABLE CASCADE;"
+done
 
 # Dump the source database to a file
 echo "Dumping source database..."

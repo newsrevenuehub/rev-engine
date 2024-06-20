f85a659d1c46c96155619ce0f6190931fabc5ec7
Benjamin White <ben.white@fundjournalism.org>
2024-06-20 12:45:10 -0400

    update post-deploy

--- scripts/heroku-postdeploy.sh
+++ scripts/heroku-postdeploy.sh
@@ -7,14 +7,38 @@ set -o xtrace
 # We dump the target database excluding the django_migrations table and id sequence
 # because in release step, migrations are run, and they won't be reflected in source
 # db's django_migrations table.
+pg_dump
+    --schema-only \
+    --no-owner \
+    --no-acl \
+    --format=custom \
+    --file=schema_dump.sql ${REVIEW_APP_SOURCE_DATABASE_URL}
+
+pg_restore
+    --schema-only
+    --no-owner
+    --no-acl
+    --format=custom -d ${DATABASE_URL} schema_dump.sql
+
+
+
+python manage.py migrate
+
+
 pg_dump \
-    --format=custom ${REVIEW_APP_SOURCE_DATABASE_URL} \
+    --data-only \
+    --no-owner \
+    --no-acl \
     --exclude-table='public.django_migrations' \
-    --exclude-table='public.django_migrations_id_seq' | \
+    --exclude-table='public.django_migrations_id_seq' \
+    --format=custom \
+    ${REVIEW_APP_SOURCE_DATABASE_URL} | \
+
 pg_restore \
     --data-only \
     --no-owner \
     --no-acl \
     --format=custom \
-    -d ${DATABASE_URL} || true
+    -d ${DATABASE_URL}
+
 python manage.py bootstrap-review-app

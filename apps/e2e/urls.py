a07647ee33842647a0fd38748f7744bd55ac6281
Benjamin White <ben.white@fundjournalism.org>
2024-07-09 17:02:05 -0400

    fix formatting

--- apps/e2e/urls.py
+++ apps/e2e/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from apps.e2e.views import commit_status_detail
 
+
 urlpatterns = [
     # make this also match on the commit_sha so securer and can't just guess monotonic ids
     path("e2e/commit_status/<str:id>/", commit_status_detail, name="e2e-create"),

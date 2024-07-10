3d95ee390e852c45bc3ce9d325bda3acf1ddc2b3
Benjamin White <ben.white@fundjournalism.org>
2024-07-10 09:02:40 -0400

    fix detail view

--- apps/e2e/tests/test_views.py
+++ apps/e2e/tests/test_views.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 @pytest.mark.django_db()
 class Test_commit_status_detail:
     def test_happy_path(self, client, commit_status):
-        response = client.get(reverse("e2e-detail", args=(commit_status.commit_sha, commit_status.github_id)))
+        response = client.get(reverse("e2e-detail", args=(commit_status.commit_sha, commit_status.id)))
         assert response.status_code == 200
         soup = BeautifulSoup(response.content, "html.parser")
         assert soup.find("title").text == f"Commit Status {commit_status.id}"

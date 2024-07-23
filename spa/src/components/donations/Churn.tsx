8765eda982f2f550d82280d50eb26295149e1add
Chris Klimas <chris.klimas@fundjournalism.org>
2024-07-23 16:28:45 -0400

    DEV-5048: Fetch data from endpoint

--- spa/src/components/donations/Churn.tsx
+++ spa/src/components/donations/Churn.tsx
@@ -12,8 +12,8 @@ export function Churn() {
       return undefined;
     }
 
-    const maxChurn = Object.keys(churn['2001']).reduce<number>((result, month) => {
-      const { churn_rate } = churn['2001'][month];
+    const maxChurn = Object.keys(churn['2023']).reduce<number>((result, month) => {
+      const { churn_rate } = churn['2023'][month];
 
       return churn_rate > result ? churn_rate : result;
     }, 0);
@@ -21,24 +21,19 @@ export function Churn() {
     return [
       {
         id: 'Label',
-        data: Object.keys(churn['2001']).map((month) => ({
-          value: churn['2001'][month].churn_rate,
+        data: Object.keys(churn['2023']).map((month) => ({
+          value: churn['2023'][month].churn_rate,
           x: parseInt(month),
-          y: maxChurn - churn['2001'][month].churn_rate
+          y: maxChurn - churn['2023'][month].churn_rate
         }))
       }
     ];
   }, [churn]);
 
-  console.log(churn?.['2001'], chartData);
-
   return (
     <>
       <Heading>Churn</Heading>
-      <Legend>
-        Lorem ipsum dolor sit amet consectetur. Est in nisl diam ipsum sapien. Facilisis at pulvinar feugiat risus sit
-        sit eget iaculis.
-      </Legend>
+      <Legend>This is hardcoded to show 2023 data only.</Legend>
       {chartData ? <ResponsiveScatterPlot nodeComponent={ChurnDataPoint} data={chartData} /> : <CircularProgress />}
     </>
   );
--- spa/src/hooks/useRevenueProgramChurn.ts
+++ spa/src/hooks/useRevenueProgramChurn.ts
@@ -1,4 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
+import axios from 'ajax/axios';
+import { CONTRIBUTIONS_CHURN } from 'ajax/endpoints';
 
 /**
  * Record keys here are year, then month (1-12).
@@ -24,73 +26,17 @@ export type ChurnResponse = Record<
   >
 >;
 
-const fakeData: ChurnResponse = {
-  '2001': {
-    '1': {
-      churn_rate: 0.11764705882352941,
-      num_active: 51,
-      num_canceled: 6
-    },
-    '2': {
-      churn_rate: 0.12307692307692308,
-      num_active: 65,
-      num_canceled: 8
-    },
-    '3': {
-      churn_rate: 0.2235294117647059,
-      num_active: 85,
-      num_canceled: 19
-    },
-    '4': {
-      churn_rate: 0.09345794392523364,
-      num_active: 107,
-      num_canceled: 10
-    },
-    '5': {
-      churn_rate: 0.09090909090909091,
-      num_active: 121,
-      num_canceled: 11
-    },
-    '6': {
-      churn_rate: 0.09696969696969697,
-      num_active: 165,
-      num_canceled: 16
-    },
-    '7': {
-      churn_rate: 0.07575757575757576,
-      num_active: 198,
-      num_canceled: 15
-    },
-    '8': {
-      churn_rate: 0.020833333333333332,
-      num_active: 1056,
-      num_canceled: 22
-    },
-    '9': {
-      churn_rate: 0.04143646408839779,
-      num_active: 1086,
-      num_canceled: 45
-    },
-    '10': {
-      churn_rate: 0.032403240324032405,
-      num_active: 1111,
-      num_canceled: 36
-    },
-    '11': {
-      churn_rate: 0.023376623376623377,
-      num_active: 1155,
-      num_canceled: 27
-    },
-    '12': {
-      churn_rate: 0.024493243243243243,
-      num_active: 1184,
-      num_canceled: 29
-    }
-  }
-};
-
 async function fetchChurn(rpSlug: string): Promise<ChurnResponse> {
-  return Promise.resolve(fakeData);
+  try {
+    const { data } = await axios.get(CONTRIBUTIONS_CHURN);
+
+    return data;
+  } catch (error) {
+    // Log it for Sentry and rethrow, which should cause the generic error
+    // message to appear.
+    console.error(error);
+    throw error;
+  }
 }
 
 export function useRevenueProgramChurn(rpSlug: string) {

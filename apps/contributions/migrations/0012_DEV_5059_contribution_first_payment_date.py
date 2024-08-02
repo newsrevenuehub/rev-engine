70ee13f31bc78757badb7a04411cd4227b7d527b
Chris Klimas <chris.klimas@fundjournalism.org>
2024-08-02 13:57:56 -0400

    Revert "DEV-5059: Add constraint on first_payment_date"
    This reverts commit c812529b717a136594b097d83ea9b1174f1c212a.

--- apps/contributions/migrations/0012_DEV_5059_contribution_first_payment_date.py
+++ apps/contributions/migrations/0012_DEV_5059_contribution_first_payment_date.py
@@ -33,10 +33,4 @@ class Migration(migrations.Migration):
             name="first_payment_date",
             field=models.DateTimeField(null=False),
         ),
-        migrations.AddConstraint(
-            model_name="contribution",
-            constraint=models.CheckConstraint(
-                name="first_payment_date_after_created", check=models.Q(first_payment_date__gte=models.F("created"))
-            ),
-        ),
     ]

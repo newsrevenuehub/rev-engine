4eb57d23d5a9ae38240dc9f0d1dc8d34832df1a9
Chris Klimas <chris.klimas@fundjournalism.org>
2024-08-23 11:23:11 -0400

    DEV-5160: Sort BillingHistory by effective date descending

--- spa/src/components/portal/ContributionsList/ContributionDetail/BillingHistory/BillingHistory.tsx
+++ spa/src/components/portal/ContributionsList/ContributionDetail/BillingHistory/BillingHistory.tsx
@@ -1,12 +1,13 @@
-import { Link, Table, TableBody } from 'components/base';
-import { PortalContributionPayment } from 'hooks/usePortalContribution';
+import { Skeleton } from '@material-ui/lab';
 import PropTypes, { InferProps } from 'prop-types';
+import { useMemo } from 'react';
+import { Link, Table, TableBody } from 'components/base';
+import usePortal from 'hooks/usePortal';
+import { PortalContributionPayment } from 'hooks/usePortalContribution';
 import formatCurrencyAmount from 'utilities/formatCurrencyAmount';
 import { DetailSection } from '../DetailSection';
 import { EmptyBillingHistory, TableCell, TableHead, TableRow } from './BillingHistory.styled';
 import { SectionControlButton } from '../common.styled';
-import usePortal from 'hooks/usePortal';
-import { Skeleton } from '@material-ui/lab';
 
 const BillingHistoryPropTypes = {
   disabled: PropTypes.bool,
@@ -30,6 +31,18 @@ export function BillingHistory({ disabled, payments, onSendEmailReceipt }: Billi
   const { page } = usePortal();
   const sendNreEmail = page?.revenue_program?.organization?.send_receipt_email_via_nre;
   const hasPayments = payments.length > 0;
+  const sortedPayments = useMemo(
+    () =>
+      payments.sort((a, b) => {
+        // Sort by effective date descending (e.g. most recent first).
+
+        const aDate = new Date(a.transaction_time ?? b.created);
+        const bDate = new Date(b.transaction_time ?? b.created);
+
+        return bDate.getTime() - aDate.getTime();
+      }),
+    []
+  );
 
   const renderEmptyBillingHistory = () =>
     page?.revenue_program.name ? (
@@ -71,7 +84,7 @@ export function BillingHistory({ disabled, payments, onSendEmailReceipt }: Billi
         </TableHead>
         <TableBody>
           {hasPayments
-            ? payments.map((payment, index) => (
+            ? sortedPayments.map((payment, index) => (
                 <TableRow key={index}>
                   <TableCell>{dateFormatter.format(new Date(payment.transaction_time ?? payment.created))}</TableCell>
                   <TableCell>{formatCurrencyAmount(payment.gross_amount_paid)}</TableCell>

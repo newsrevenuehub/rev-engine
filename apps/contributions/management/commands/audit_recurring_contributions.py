b80238fbe6c090c7aa6b8d9ddfd39d943e3baafc
Benjamin White <ben.white@fundjournalism.org>
2024-11-15 10:35:21 -0500

    initial implementation, no tests yet

--- apps/contributions/management/commands/audit_recurring_contributions.py
+++ apps/contributions/management/commands/audit_recurring_contributions.py
@@ -2,16 +2,16 @@ import logging
 from pathlib import Path
 
 from django.core.management.base import BaseCommand, CommandParser
-from django.db.models import Q, QuerySet
+from django.db.models import Case, F, Q, QuerySet, Value, When
 from django.utils import timezone
 
-import pandas as pd
 import stripe
 
 from apps.common.utils import get_stripe_accounts_and_their_connection_status
 from apps.contributions.choices import ContributionInterval
 from apps.contributions.models import Contribution
 from apps.contributions.stripe_import import StripeTransactionsImporter
+from apps.contributions.types import STRIPE_PAYMENT_METADATA_SCHEMA_VERSIONS
 
 
 # otherwise we get spammed by stripe info logs when running this command
@@ -37,42 +37,63 @@ class Command(BaseCommand):
         )
 
     def get_stripe_subscriptions_for_account(self, stripe_account_id: str) -> list[stripe.Subscription]:
+        """Get all stripe subscriptions with conforming metadata schema versions for a given stripe account."""
         self.stdout.write(
             self.style.HTTP_INFO(f"Getting stripe subscriptions for stripe account `{stripe_account_id}`")
         )
-        return list(stripe.Subscription.list(stripe_account=stripe_account_id, status="all").auto_paging_iter())
-
-    def _get_link_for_subscription(self, subscription: stripe.Subscription, stripe_account_id: str) -> str:
-        return f"https://dashboard.stripe.com/accounts/{stripe_account_id}/subscriptions/{subscription.id}"
+        query = " OR ".join(
+            f'metadata["schema_version"]:"{version}"' for version in STRIPE_PAYMENT_METADATA_SCHEMA_VERSIONS
+        )
+        return list(
+            stripe.Subscription.search(
+                stripe_account=stripe_account_id,
+                query=query,
+            ).auto_paging_iter()
+        )
 
     def get_contributions_with_unexpected_status(
         self, contributions: QuerySet[Contribution], subscriptions: list[stripe.Subscription], stripe_account_id: str
-    ) -> list[dict]:
+    ) -> QuerySet[dict]:
         self.stdout.write(
             self.style.HTTP_INFO(
                 f"Getting contributions with unexpected status for stripe account `{stripe_account_id}`"
             )
         )
-        problems = []
-        subs_map = {sub.id: sub for sub in subscriptions}
-        for contribution in contributions:
-            sub = subs_map[contribution.provider_subscription_id]
-            if contribution.status != StripeTransactionsImporter.get_status_for_subscription(sub.status):
-                problems.append(
-                    {
-                        "contribution_id": contribution.id,
-                        "contribution_status": contribution.status,
-                        "subscription_status": sub.status,
-                        "subscription_id": sub.id,
-                        "subscription_link": self._get_link_for_subscription(sub, stripe_account_id),
-                    }
-                )
-        return problems
+        expected_status_map = {
+            sub.id: StripeTransactionsImporter.get_status_for_subscription(sub.status) for sub in subscriptions
+        }
+        subscription_status_map = {sub.id: sub.status for sub in subscriptions}
+        return (
+            contributions.annotate(
+                expected_status=Case(
+                    *[
+                        When(provider_subscription_id=sub_id, then=Value(status))
+                        for sub_id, status in expected_status_map.items()
+                    ],
+                    default=Value(None),
+                ),
+                subscription_status=Case(
+                    *[
+                        When(provider_subscription_id=sub_id, then=Value(status))
+                        for sub_id, status in subscription_status_map.items()
+                    ],
+                    default=Value(None),
+                ),
+            )
+            .filter(~Q(status=F("expected_status")))
+            .values(
+                "id",
+                "provider_subscription_id",
+                "expected_status",
+                "status",
+                "subscription_status",
+            )
+        )
 
     def do_audit(self, stripe_account_id: str, timestamp_str) -> None:
         stripe_subscriptions = self.get_stripe_subscriptions_for_account(stripe_account_id)
         self.stdout.write(
-            self.style.HTTP_INFO("Found {len(stripe_subscriptions)} subscriptions for account {stripe_account_id}")
+            self.style.HTTP_INFO(f"Found {len(stripe_subscriptions)} subscriptions for account {stripe_account_id}")
         )
         existing_contributions = self.get_recurring_contributions_for_account(stripe_account_id)
         self.stdout.write(
@@ -81,34 +102,31 @@ class Command(BaseCommand):
             )
         )
         orphaned_contributions = existing_contributions.filter(
-            Q(provider_subscription_id__isnull=True)
-            | Q(provider_subscription_id__in=[sub.id for sub in stripe_subscriptions])
+            Q(
+                Q(provider_subscription_id__isnull=False)
+                & ~Q(provider_subscription_id__in=[sub.id for sub in stripe_subscriptions])
+            )
         )
         self.stdout.write(self.style.HTTP_INFO(f"Found {orphaned_contributions.count()} orphaned contributions"))
-        if orphaned_contributions.exists():
-            pd.DataFrame(
-                [
-                    {"id": x.id, "status": x.status, "interval": x.interval, "problem": "stripe subscription not found"}
-                    for x in orphaned_contributions
-                ]
-            ).to_csv(f"orphaned_contributions_{stripe_account_id}-{timestamp_str}.csv", index=False)
+        for x in orphaned_contributions:
+            self.stdout.write(
+                self.style.HTTP_INFO(
+                    f"ORPHANED CONTRIBUTION: Contribution {x.id} has reference to an unretrievable Stripe "
+                    f"subscription {x.provider_subscription_id}"
+                )
+            )
         orphaned_subscriptions = [
             sub
             for sub in stripe_subscriptions
             if sub.id not in existing_contributions.values_list("provider_subscription_id", flat=True)
         ]
         self.stdout.write(self.style.HTTP_INFO(f"Found {len(orphaned_subscriptions)} orphaned subscriptions"))
-        if orphaned_subscriptions:
-            pd.DataFrame(
-                [
-                    {
-                        "id": sub.id,
-                        "status": sub.status,
-                        "link": self._get_link_for_subscription(sub, stripe_account_id),
-                    }
-                    for sub in orphaned_subscriptions
-                ]
-            ).to_csv(f"orphaned_subscriptions_{stripe_account_id}-{timestamp_str}.csv", index=False)
+        for x in orphaned_subscriptions:
+            self.stdout.write(
+                self.style.HTTP_INFO(
+                    f"ORPHANED SUBSCRIPTION: Stripe subscription {x.id} with status {x.status} has no corresponding contribution"
+                )
+            )
         contributions_with_unexpected_status = self.get_contributions_with_unexpected_status(
             existing_contributions.exclude(id__in=orphaned_contributions.values_list("id", flat=True)),
             stripe_subscriptions,
@@ -119,10 +137,12 @@ class Command(BaseCommand):
                 f"Found {len(contributions_with_unexpected_status)} contributions with unexpected status"
             )
         )
-
-        if contributions_with_unexpected_status:
-            pd.DataFrame(contributions_with_unexpected_status).to_csv(
-                f"contributions_with_unexpected_status_{stripe_account_id}-{timestamp_str}.csv", index=False
+        for x in contributions_with_unexpected_status:
+            self.stdout.write(
+                self.style.HTTP_INFO(
+                    f"UNEXPECTED STATUS: Contribution {x['id']} has status {x['status']} but expected "
+                    f"{x['expected_status']}. Subscription {x['provider_subscription_id']} has status {x['subscription_status']}"
+                )
             )
 
     def add_arguments(self, parser: CommandParser) -> None:

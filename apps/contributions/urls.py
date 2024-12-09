38fc1376a9bedca92e8914a59c17a933b32aa109
Benjamin White <ben.white@fundjournalism.org>
2024-12-09 10:29:13 -0500

    handle imports a bit differently

--- apps/contributions/urls.py
+++ apps/contributions/urls.py
@@ -3,41 +3,27 @@ from django.urls import path, re_path
 
 from rest_framework import routers
 
-from apps.contributions.views import (
-    checkout as checkout_views,
-)
-from apps.contributions.views import (
-    orgs as orgs_views,
-)
-from apps.contributions.views import (
-    portal as portal_views,
-)
-from apps.contributions.views import (
-    switchboard as switchboard_views,
-)
-from apps.contributions.views import (
-    webhooks as webhooks_views,
-)
+from apps.contributions.views import checkout, orgs, portal, switchboard, webhooks
 
 
 router = routers.DefaultRouter()
-router.register(r"contributions", orgs_views.ContributionsViewSet, basename="contribution")
-router.register(r"payments", checkout_views.PaymentViewset, basename="payment")
-router.register(r"contributors", portal_views.PortalContributorsViewSet, basename="portal-contributor")
+router.register(r"contributions", orgs.ContributionsViewSet, basename="contribution")
+router.register(r"payments", checkout.PaymentViewset, basename="payment")
+router.register(r"contributors", portal.PortalContributorsViewSet, basename="portal-contributor")
 # TODO @BW: Consolidate all switchboard endpoints under /switchboard/ URL
 # DEV-5534
 router.register(
-    r"switchboard-contributions", switchboard_views.SwitchboardContributionsViewSet, basename="switchboard-contribution"
+    r"switchboard-contributions", switchboard.SwitchboardContributionsViewSet, basename="switchboard-contribution"
 )
 router.register(
-    r"switchboard-contributors", switchboard_views.SwitchboardContributorsViewSet, basename="switchboard-contributor"
+    r"switchboard-contributors", switchboard.SwitchboardContributorsViewSet, basename="switchboard-contributor"
 )
 
 urlpatterns = [
-    path("stripe/oauth/", orgs_views.stripe_oauth, name="stripe-oauth"),
+    path("stripe/oauth/", orgs.stripe_oauth, name="stripe-oauth"),
     re_path(
         settings.WEBHOOK_URL,
-        webhooks_views.process_stripe_webhook,
+        webhooks.process_stripe_webhook,
         name="stripe-webhooks-contributions",
     ),
 ]

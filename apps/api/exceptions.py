c7528090fa974cc94f467fc612232756a16322b7
Benjamin White <ben.white@fundjournalism.org>
2025-01-22 17:19:41 -0500

    remove unused exception

--- apps/api/exceptions.py
+++ apps/api/exceptions.py
@@ -6,9 +6,3 @@ class ApiConfigurationError(APIException):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     default_detail = "There was a problem with the API"
     default_code = "api_configuration_error"
-
-
-class UniquenessConflictException(APIException):
-    status_code = status.HTTP_409_CONFLICT
-    default_detail = "A uniqueness constraint was violated"
-    default_code = "uniqueness_conflict"

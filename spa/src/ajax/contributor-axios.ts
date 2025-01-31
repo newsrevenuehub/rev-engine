7ac57a5a678e296caa1b51f7c6521bf5f152a5cc
Chris Klimas <chris.klimas@fundjournalism.org>
2025-01-31 13:22:41 -0500

    DEV-5682: Add documentation, increase timeout

--- spa/src/ajax/contributor-axios.ts
+++ spa/src/ajax/contributor-axios.ts
@@ -7,10 +7,11 @@ export const apiBaseUrl = `/api/${REVENGINE_API_VERSION}/`;
  * An Axios instance configured for use on contribution pages. There is no
  * authentication involved here, but we need to send the CSRF token present in
  * cookies as a request header.
+ * https://docs.djangoproject.com/en/4.2/howto/csrf/#setting-the-token-on-the-ajax-request
  */
 export const contributorAxios = axios.create({
   baseURL: apiBaseUrl,
-  timeout: 7000,
+  timeout: 15000,
   withCredentials: true
 });
 

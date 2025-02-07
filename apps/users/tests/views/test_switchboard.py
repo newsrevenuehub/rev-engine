8ed5ac167694580fb79f4b3a8845adf5dd4be1a5
Chris Klimas <chris.klimas@fundjournalism.org>
2025-02-07 15:48:04 -0500

    DEV-5669: Move token fixtures to conftest

--- apps/users/tests/views/test_switchboard.py
+++ apps/users/tests/views/test_switchboard.py
@@ -1,9 +1,4 @@
-from datetime import timedelta
-
-from django.utils.timezone import now
-
 import pytest
-from knox.models import AuthToken
 from rest_framework import status
 from rest_framework.reverse import reverse
 
@@ -12,25 +7,14 @@ from apps.users.models import User
 
 @pytest.mark.django_db
 class TestSwitchboardUsersViews:
-    @pytest.fixture
-    def token(self, switchboard_user):
-        return AuthToken.objects.create(switchboard_user)[1]
-
-    @pytest.fixture
-    def expired_token(self, api_client, switchboard_user, default_password, monkeypatch, settings):
-        token, token_string = AuthToken.objects.create(switchboard_user)
-        token.expiry = now() - timedelta(days=1)
-        token.save()
-        return token_string
-
     @pytest.mark.parametrize("exists", [True, False])
-    def test_retrieve_by_id(self, api_client, token, org_user_multiple_rps: User, exists):
+    def test_retrieve_by_id(self, api_client, switchboard_api_token, org_user_multiple_rps: User, exists):
         pk = org_user_multiple_rps.id
         if not exists:
             org_user_multiple_rps.delete()
         response = api_client.get(
             reverse("switchboard-user-detail", args=(pk,)),
-            headers={"Authorization": f"Token {token}"},
+            headers={"Authorization": f"Token {switchboard_api_token}"},
         )
         if exists:
             assert response.status_code == status.HTTP_200_OK
@@ -50,13 +34,13 @@ class TestSwitchboardUsersViews:
             assert response.status_code == status.HTTP_404_NOT_FOUND
 
     @pytest.mark.parametrize("exists", [True, False])
-    def test_retrieve_by_email(self, api_client, token, org_user_multiple_rps, exists):
+    def test_retrieve_by_email(self, api_client, switchboard_api_token, org_user_multiple_rps, exists):
         email = org_user_multiple_rps.email
         if not exists:
             org_user_multiple_rps.delete()
         response = api_client.get(
             reverse("switchboard-user-get-by-email", args=(email,)),
-            headers={"Authorization": f"Token {token}"},
+            headers={"Authorization": f"Token {switchboard_api_token}"},
         )
         if exists:
             assert response.status_code == status.HTTP_200_OK
@@ -76,7 +60,9 @@ class TestSwitchboardUsersViews:
             assert response.status_code == status.HTTP_404_NOT_FOUND
 
     @pytest.mark.parametrize("url_name", ["switchboard-user-get-by-email", "switchboard-user-detail"])
-    @pytest.mark.parametrize(("token_fixture", "expect_success"), [("token", True), ("expired_token", False)])
+    @pytest.mark.parametrize(
+        ("token_fixture", "expect_success"), [("switchboard_api_token", True), ("switchboard_api_expired_token", False)]
+    )
     def test_only_works_with_valid_token(
         self, url_name, token_fixture, expect_success, api_client, request, org_user_multiple_rps
     ):

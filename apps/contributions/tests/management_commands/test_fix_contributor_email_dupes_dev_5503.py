ed57e40f37e06334a6e4511670ee64b6828feb4f
Benjamin White <ben.white@fundjournalism.org>
2025-02-10 15:45:07 -0500

    more thorough testing

--- apps/contributions/tests/management_commands/test_fix_contributor_email_dupes_dev_5503.py
+++ apps/contributions/tests/management_commands/test_fix_contributor_email_dupes_dev_5503.py
@@ -10,28 +10,52 @@ from apps.contributions.tests.factories import ContributionFactory, ContributorF
 class TestCommand:
 
     @pytest.fixture
-    def dupes_with_contributions(self, faker):
-        canonical = ContributorFactory(email=faker.email().lower())
-        duplicate = ContributorFactory(email=canonical.email.upper())
+    def email_lower(self):
+        return "foo@bar.com"
+
+    @pytest.fixture
+    def email_upper(self, email_lower):
+        return email_lower.upper()
+
+    @pytest.fixture
+    def email_strange(self, email_lower):
+        return email_lower.replace("com", "COM")
+
+    @pytest.fixture
+    def dupes_with_contributions(self, email_lower, email_upper):
+        canonical = ContributorFactory(email=email_lower)
+        duplicate = ContributorFactory(email=email_upper)
         ContributionFactory(contributor=canonical)
         ContributionFactory(contributor=duplicate)
         return {"canonical": canonical, "duplicates": [duplicate]}
 
     @pytest.fixture
-    def dupes_without_contributions(self, faker):
+    def dupes_without_contributions(self, email_lower, email_upper):
         return {
-            "canonical": ContributorFactory(email=(email := faker.email().lower())),
-            "duplicates": [ContributorFactory(email=email.upper())],
+            "canonical": ContributorFactory(email=email_lower),
+            "duplicates": [ContributorFactory(email=email_upper)],
         }
 
-    @pytest.mark.parametrize("dupes_fixture", ["dupes_with_contributions", "dupes_without_contributions"])
+    @pytest.fixture
+    def dupes_with_gt_one_contribution(self, email_lower, email_upper, email_strange):
+        canonical = ContributorFactory(email=email_lower)
+        dupe1 = ContributorFactory(email=email_upper)
+        dupe2 = ContributorFactory(email=email_strange)
+        ContributionFactory.create_batch(size=2, contributor=canonical)
+        ContributionFactory.create_batch(size=2, contributor=dupe1)
+        ContributionFactory.create_batch(size=2, contributor=dupe2)
+        return {"canonical": canonical, "duplicates": [dupe1, dupe2]}
+
+    @pytest.mark.parametrize(
+        "dupes_fixture", ["dupes_with_contributions", "dupes_without_contributions", "dupes_with_gt_one_contribution"]
+    )
     def test_get_duplicate_emails_by_contributors(self, dupes_fixture, request):
         dupes = request.getfixturevalue(dupes_fixture)
         result = Command().get_duplicate_emails_by_contributors()
         assert result.count() == 1
-        assert (item := result[0])["lower_email"] == dupes["canonical"].email
-        assert item["contributor_count"] == 2
-        assert set(item["contributors"]) == {dupes["canonical"].id, dupes["duplicates"][0].id}
+        assert (item := result[0])["lower_email"] == dupes["canonical"].email.lower()
+        assert item["contributor_count"] == len(dupes["duplicates"]) + 1
+        assert set(item["contributors"]) == {dupes["canonical"].id, *(x.id for x in dupes["duplicates"])}
 
     @pytest.mark.usefixtures("dupes_with_contributions")
     def test_get_canonical_and_duplicate_contributors(self):

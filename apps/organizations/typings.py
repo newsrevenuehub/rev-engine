abf27903f0f3749296a4fa6f07430839e2b53644
Benjamin White <960351+benjaminEwhite@users.noreply.github.com>
2025-03-05 20:13:41 -0500

    remove comments

--- apps/organizations/typings.py
+++ apps/organizations/typings.py
@@ -9,9 +9,7 @@ class MailchimpProductType(StrEnum):
 
 class MailchimpSegmentType(StrEnum):
     ALL_CONTRIBUTORS = "all_contributors"
-    # question is it okay for this to change?
     ONE_TIME_CONTRIBUTORS = "one_time_contributor"
-    # can this change to plural?
     RECURRING_CONTRIBUTORS = "recurring_contributor"
     MONTHLY_CONTRIBUTORS = "monthly_contributor"
     YEARLY_CONTRIBUTORS = "yearly_contributor"

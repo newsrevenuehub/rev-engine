0305cd911337ae36d0ffcf9adaeb865e7194a612
Benjamin White <960351+benjaminEwhite@users.noreply.github.com>
2025-05-30 14:57:14 -0400

    create test fixture

--- apps/emails/tests/test_models.py
+++ apps/emails/tests/test_models.py
@@ -1,25 +1,33 @@
 import pytest
 
 from apps.emails.models import EmailCustomization
+from apps.organizations.models import RevenueProgram
 
 
+@pytest.mark.django_db
 class TestEmailCustomization:
-    @pytest.mark.django_db
-    def test_save_removes_unexpected_html_tags(self):
-        customization = EmailCustomization()
+
+    @pytest.fixture
+    def customization(self, revenue_program: RevenueProgram) -> EmailCustomization:
+        """Fixture to create a default EmailCustomization instance."""
+        return EmailCustomization.objects.create(
+            content_html="<p>Test content</p>",
+            email_type="test_email",
+            email_block="test_block",
+            revenue_program=revenue_program,
+        )
+
+    def test_save_removes_unexpected_html_tags(self, customization: EmailCustomization):
         customization.content_html = "<script>not allowed</script><b>OK</b>"
         customization.save()
         assert customization.content_html == "<b>OK</b>"
 
-    @pytest.mark.django_db
-    def test_save_removes_unexpected_html_attributes(self):
-        customization = EmailCustomization()
+    def test_save_removes_unexpected_html_attributes(self, customization: EmailCustomization):
         customization.content_html = '<b onclick="doEvil()">should be pruned</b>'
         customization.save()
         assert customization.content_html == "<b>should be pruned</b>"
 
-    def test_plain_text_conversion(self):
-        customization = EmailCustomization()
+    def test_plain_text_conversion(self, customization: EmailCustomization):
         customization.content_html = """
           <p><b>Bold</b> <i>Italic</i> <s>Strike</s> <u>Underline</u></p>
           <ol>

535c323c33d1f0fceaebb10586942dd60dbf735c
Benjamin White <960351+benjaminEwhite@users.noreply.github.com>
2025-06-04 16:49:45 -0400

    add urls entry

--- apps/emails/urls.py
+++ apps/emails/urls.py
@@ -1,10 +1,19 @@
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework import routers
 
 from apps.emails import views
+from apps.emails.views import EmailCustomizationViewSet
 
 
 # These allow viewing templated emails in isolation.
-
 debug_urlpatterns = [
     path("contribution/<str:template_name>", views.preview_contribution_email_template),
+]
+
+router = routers.DefaultRouter()
+router.register(r"customizations", EmailCustomizationViewSet, basename="customization")
+
+url_patterns = [
+    path("", include(router.urls)),
 ]

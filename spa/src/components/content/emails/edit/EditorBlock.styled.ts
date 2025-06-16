55edb6c4e25eb2d34c3431e002d064f7dcbba757
Chris Klimas <112435785+nrh-cklimas@users.noreply.github.com>
2025-06-16 16:10:14 -0400

    DEV-6142: Tweak editor hover style

--- spa/src/components/content/emails/edit/EditorBlock.styled.ts
+++ spa/src/components/content/emails/edit/EditorBlock.styled.ts
@@ -5,6 +5,10 @@ export const EditorContent = styled(BaseEditorContent)`
   & .ProseMirror {
     outline: 1px solid ${({ theme }) => theme.basePalette.primary.engineBlue};
     padding: 12px 20px;
+
+    &:hover {
+      outline: 2px solid ${({ theme }) => theme.basePalette.primary.engineBlue};
+    }
   }
 
   & .ProseMirror-focused {

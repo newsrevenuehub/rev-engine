659f9b6726ac45aeaf048a293462442e9a5ed4f1
Benjamin White <960351+benjaminEwhite@users.noreply.github.com>
2025-07-17 12:03:36 -0400

    correctly implemented, and tested

--- spa/src/components/content/emails/EmailsRoute.tsx
+++ spa/src/components/content/emails/EmailsRoute.tsx
@@ -4,7 +4,7 @@ import EmailBlock, { EmailBlockProps } from './EmailBlock';
 import { Blocks } from './EmailsRoute.styled';
 import PageTitle from 'elements/PageTitle';
 import { TestEmailName, useTestEmails } from 'hooks/useTestEmails';
-import { EMAIL_KB_URL } from 'constants/helperUrls';
+import { EMAIL_CANCELATION_KB_URL, EMAIL_KB_URL, EMAIL_PAYMENT_CHANGE_KB_URL } from 'constants/helperUrls';
 import useUser from 'hooks/useUser';
 import { useMemo } from 'react';
 import { PLAN_NAMES } from 'constants/orgPlanConstants';
@@ -17,36 +17,57 @@ interface EmailBlock extends Omit<EmailBlockProps, 'onSendTest'> {
 export const blocks: EmailBlock[] = [
   {
     editable: true,
+    emailType: 'contribution_receipt',
     description:
       'Receipts are sent for all contributions after a successful payment. The receipts thank the contributor for their contribution, confirm payment, and provide tax information. Contributors can also resend receipts to themselves in the contributor portal.',
-    name: 'Receipts',
+    name: 'Receipt',
     testEmailName: 'receipt'
   },
   {
+    disabled: true,
     editable: true,
-    description:
-      'A canceled contribution email is sent when a contribution is canceled for any reason; this includes the contributor or the organization canceling, or RevEngine’s payment processor canceling due to an expired or failed payment method.',
-    name: 'Canceled'
+    description: (
+      <>
+        These emails are sent when a contribution is canceled for any reason—by the contributor, by the org, or by the
+        payment processor (when a payment method fails).{' '}
+        <Link href={EMAIL_CANCELATION_KB_URL} target="_blank">
+          Show Preview
+        </Link>
+      </>
+    ),
+    hideActions: true,
+    name: 'Cancelation'
   },
   {
+    disabled: true,
     editable: true,
     description:
       'For contributors with recurring annual contributions, a payment reminder is sent 7 days before their contribution renews. This time period can be configured within your organization’s Stripe account.',
-    name: 'Payment Reminders',
+    name: 'Payment Reminder',
     testEmailName: 'reminder'
   },
   {
+    disabled: true,
     editable: false,
     description:
       "A failed payment notification email is sent when RevEngine's payment processor cannot charge the payment method of a contribution, which could be due to incorrect payment information or expired cards.",
-    name: 'Failed'
+    name: 'Payment Failed'
   },
   {
-    description:
-      'A payment change confirmation email is sent when a contributor successfully makes changes to an active recurring contribution, like changing the payment method, amount, and/or frequency.',
-    name: 'Payment Changes Confirmation'
+    description: (
+      <>
+        A payment change confirmation email is sent when a contributor successfully makes changes to an active recurring
+        contribution, like changing the payment method, amount, and/or frequency.{' '}
+        <Link href={EMAIL_PAYMENT_CHANGE_KB_URL} target="_blank">
+          Show Preview
+        </Link>
+      </>
+    ),
+    hideActions: true,
+    name: 'Payment Change Confirmation'
   },
   {
+    disabled: true,
     description:
       'Contributors can access the contributor portal to manage their contributions to your organization, and are granted access through a secure magic link sent via email.',
     name: 'Magic Link',
@@ -57,12 +78,15 @@ export const blocks: EmailBlock[] = [
 export function EmailsRoute() {
   const { user } = useUser();
   const isFreeOrg = user?.organizations?.[0]?.plan?.name === PLAN_NAMES.FREE;
+
   const { sendTestEmail } = useTestEmails();
   const firstRevenueProgramId = useMemo(() => {
     if (user?.revenue_programs) {
       return user.revenue_programs[0].id;
     }
   }, [user?.revenue_programs]);
+  const orgPlan = useMemo(() => user?.organizations[0].plan.name, [user?.organizations]);
+  const prompt = orgPlan === 'FREE' ? '' : 'View and edit capabilities coming soon.';
 
   function handleSendTest(emailName: TestEmailName) {
     if (!firstRevenueProgramId) {
@@ -84,13 +108,16 @@ export function EmailsRoute() {
           </Link>
         }
         title="Emails"
-        subtitle="RevEngine sends emails to your contributors to confirm transactions and keep them informed about their contributions. View, edit, or customize emails sent to your organization's contributors."
+        subtitle={`RevEngine sends emails to your contributors to confirm transactions and keep them informed about their contributions. ${prompt}`}
       />
       <Blocks>
         {blocks.map((block) => (
           <EmailBlock
             description={block.description}
-            editable={block.editable && !isFreeOrg}
+            disabled={block.disabled || isFreeOrg}
+            editable={block.editable}
+            emailType={block.emailType}
+            hideActions={block.hideActions}
             key={block.name}
             name={block.name}
             onSendTest={
